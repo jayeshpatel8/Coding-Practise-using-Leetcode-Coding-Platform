a14252abe1bc8205a444857da4000d5abce164c9
jayeshpatel8 <jnpatel81@gmail.com>
2020-05-16 16:26:38 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (cpp)

---
+++ problems/binary_tree_inorder_traversal/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     vector<int> o;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (0==root) return o;
+        inorderTraversal(root->left);
+        o.push_back(root->val);
+        inorderTraversal(root->right);
+        return o;
+    }
+};
