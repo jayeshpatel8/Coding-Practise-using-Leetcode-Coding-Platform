5d2e99eb83c40f59f7587ad3112fc68b5b78100c
jayeshpatel8 <jnpatel81@gmail.com>
2020-12-16 00:10:48 +0000

    Sync LeetCode submission - Course Schedule (cpp)

---
+++ problems/course_schedule/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        if (pre.size() < 1) return true;
+        vector<int> graph[n] , indegree(n,0);
+        
+  
+        for ( int i=0; i< pre.size(); i++){
+            graph[pre[i][0]].push_back(pre[i][1]);
+            indegree[pre[i][1]]++;
+        }
+        queue<int> q;
+        for ( int i=0; i< n; i++)
+            if (indegree[i]==0) q.push(i);
+        int count=0;
+        while(!q.empty()){
+            int qs = q.size();
+            count +=qs;
+            
+            for (int i=0; i<qs; i++ ){
+                int j = q.front();
+                q.pop();
+                for (auto u : graph[j])
+                    if (--indegree[u] == 0) q.push(u);
+            }
+        }
+                
+        return count==n;
+    }
+    bool Cycle( vector<int> *g,vector<int> &vis, int v){
+        if (vis[v]==1) return true;
+        if (vis[v]==2) return false;
+        
+        vis[v]=1;
+        for (auto i : g[v]){
+            if (Cycle(g,vis, i)) { return true;}
+        }
+        
+        vis[v]=2;
+        return false;
+    }
+};
