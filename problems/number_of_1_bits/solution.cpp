f7a186b4a0dedb9e524418aafc8ac2c74d0335fb
jayeshpatel8 <jnpatel81@gmail.com>
2021-02-02 19:53:59 +0000

    Sync LeetCode submission - Number of 1 Bits (cpp)

--- problems/number_of_1_bits/solution.cpp
+++ problems/number_of_1_bits/solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int i=0;
-        while (n){
-            i++;
-            n &= n - 1;
+        int c=0;
+        while(n){
+            c++;
+            n &= n-1;
         }
-        return i;
-        
+        return c;
     }
 };
