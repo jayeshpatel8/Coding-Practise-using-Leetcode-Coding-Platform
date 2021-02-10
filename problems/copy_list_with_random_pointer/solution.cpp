6d5e6a605732dd817de8177cd5e6a0a5ec5c3d09
jayeshpatel8 <jnpatel81@gmail.com>
2021-02-10 15:37:03 +0000

    Sync LeetCode submission - Copy List with Random Pointer (cpp)

--- problems/copy_list_with_random_pointer/solution.cpp
+++ problems/copy_list_with_random_pointer/solution.cpp
@@ -17,19 +17,32 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node * t = head;
-        unordered_map<Node *, Node *> m;
-        while (t != NULL){
-            m[t] = new Node(t->val);
-            t=t->next;
+        if (!head) return head;
+        Node * h = head, *nh;
+        //Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+        //Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+        while (h){
+            Node *t = new Node(0);
+            *t = *h;
+            h->next = t;
+            h = t->next;
         }
-        t=head;
-
-        while (t != NULL){
-            m[t]->random = m[t->random];
-            m[t]->next=m[t->next];
-            t=t->next;
+        h = head;
+        nh = head->next;
+        while(h){
+            Node *n = h->next;
+            n->random = n->random? n->random->next:0;
+            h = n->next;
         }
-        return m[head];
+        h = head;
+        while(h)
+        {
+            Node *n = h->next;
+            h->next = n->next;
+            n->next = h->next?h->next->next:0;
+            
+            h = h->next;
+        }
+        return nh;
     }
 };
