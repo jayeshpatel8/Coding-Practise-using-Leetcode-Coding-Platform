334c8f8083b7f5d366ed7555039b7e0742f14d3d
jayeshpatel8 <jnpatel81@gmail.com>
2021-04-29 21:06:07 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- problems/unique_paths_ii/solution.cpp
+++ problems/unique_paths_ii/solution.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& o) {
-    int r = o.size(),c=o[0].size();
-    vector<vector<long>> dp(r+1,vector<long>(c+1,0));
-    dp[0][1]=1;
-    for (int i=1; i<=r; i++)
-        for (int j=1; j<=c; j++)        
-        {
-            if (o[i-1][j-1] ) {continue;}
-            dp[i][j]= dp[i][j-1] + dp[i-1][j];
+    int uniquePathsWithObstacles(vector<vector<int>>& og) {
+        int m = og.size(),n=og[0].size();
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        dp[0][1]=1;
+        for (int i=1; i<=m; i++){
+            for (int j=1; j<=n; j++){
+                if (og[i-1][j-1]){
+                    dp[i][j] = 0;
+                }
+                else{ 
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];                
+                }
+            }
         }
-        return dp[r][c];
+        return dp[m][n];
     }
 };
