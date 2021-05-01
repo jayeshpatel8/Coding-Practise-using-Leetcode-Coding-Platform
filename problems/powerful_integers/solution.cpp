36c34785b350b111f298694645b62ea0b7f247ea
jayeshpatel8 <jnpatel81@gmail.com>
2021-05-01 09:04:39 +0000

    Sync LeetCode submission - Powerful Integers (cpp)

---
+++ problems/powerful_integers/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {        
+        set<int> n;        
+        for (int i=1; i<=bound; i*=x){
+            for (int j=1; i+j<=bound; j*=y){
+                n.insert(i+j);
+                if (y==1) break;
+            }
+            if (x==1) break;
+        }
+        return vector<int>(n.begin(),n.end());
+    }
+};
