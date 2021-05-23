b293ecb2a3a9fdc77f46774d7efc09f42a240f55
jayeshpatel8 <jnpatel81@gmail.com>
2021-05-23 12:29:26 +0000

    Sync LeetCode submission - N-Queens (cpp)

--- problems/n-queens/solution.cpp
+++ problems/n-queens/solution.cpp
@@ -1,4 +1,36 @@
-bool checkValidMove(vector<string> &s,int r,int c){
+class Solution {
+public:
+    vector<vector<string>> res;
+    int N;
+    vector<vector<string>> solveNQueens(int n) {
+        N=n;
+        vector<bool> visited(n,0);
+        vector<string> board(n,string(n, '.'));
+        solveNQueens(n,0,visited,board);
+        
+        return res;
+    }
+    void solveNQueens(int n, int r, vector<bool>& visited, vector<string>& board){
+        if (r == n ) {
+            res.push_back(board);
+            return;
+        }
+       //for (; c < n; c++){
+           for (int c=0; c < n; c++){
+               if ( allowed(board,r,c)){
+                   visited[r]=1;
+                   board[r][c]='Q';
+                   
+                   solveNQueens(n,r+1,visited,board);
+                   
+                   board[r][c]='.';
+                   visited[r]=0;
+               }
+           }
+           return ;
+       //}   
+    }
+    bool allowed(vector<string>& s, int r , int c){
     int n = s.size();
     // check for col
     for (int i =0; i<n; i++)
@@ -9,30 +41,5 @@ bool checkValidMove(vector<string> &s,int r,int c){
     for (int i =r-1,j=c+1;  j<n && i>=0 ; i--,j++)
         if (s[i][j]=='Q') return false;
     return true;
-}
-void Queens(vector<string> &s ,vector<vector<string>> & sl ,int r){
-    int n = s.size();
-    if (r == n) {
-        sl.push_back(s);
-        return ;
-    }
-    for (int c=0; c<n; c++){
-
-        if (checkValidMove(s,r,c))
-        {
-            s[r][c]='Q';
-            Queens(s,sl,r+1);
-                s[r][c]='.';   
-        }             
-    }
-}
-class Solution {
-public:
-    vector<vector<string>> solveNQueens(int n) {
-         
-        vector<string> s(n,string(n,'.'));
-        vector<vector<string>> sl;
-        Queens(s,sl,0);
-        return sl;
     }
 };
