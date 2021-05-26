a4878bae0c8e8b86d07b2204a59ba0580e795481
jayeshpatel8 <jnpatel81@gmail.com>
2021-05-26 20:04:23 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (cpp)

--- problems/evaluate_reverse_polish_notation/solution.cpp
+++ problems/evaluate_reverse_polish_notation/solution.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int mystoi(string s){
-        int r=0;
-        int i=0;
-        int m=1;
-
-        if (s[0]=='-' && (s.length() >1)) { m=-1;i=1; }
-        for (; i<s.length(); i++)
-                r = s[i] - '0' + (r * 10);
-        return r * m;
-    }
     int evalRPN(vector<string>& tokens) {
-       stack<int> st;
-        long int r=0;
-       for (auto s : tokens){
-           if (s.length() > 1 || s[0] >= '0' && s[0] <='9'){
-               st.push(stoi(s));
-           }
-           else{
-               int b = st.top(); st.pop();
-               int a = st.top(); st.pop();
-               
-               if(s[0]=='+')         st.push(a + b); 
-               else if(s[0] == '-')  st.push(a - b);
-               else if(s[0] == '*')  st.push(a * b);
-               else                  st.push(a / b);
-           }
-               
-       }
-        return st.top();
+        stack<int64_t> num;
+        
+        for (auto s : tokens){
+            if (s.size() ==1 && (s[0] =='+' || s[0] =='-' || s[0] =='*' || s[0] == '/')){
+                int64_t r, n1,n2;
+                n2 = num.top(); num.pop();
+                n1 = num.top(); num.pop();
+                switch (s[0] ){
+                    case '+':
+                        r  = n1 + n2;
+                        break;
+                    case '-':
+                        r  = n1 - n2;
+                        break;
+                    case '*':
+                        r  = n1 * n2;
+                        break;
+                    case '/':
+                        r  = n1 / n2;
+                        break;
+                }
+                num.push((int64_t)r);
+            }
+            else{
+                num.push(stoi(s));
+            }
+        }
+        return num.top();
     }
 };
