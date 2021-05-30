18fb49868f53296fc5c566eb12342d3c14308865
jayeshpatel8 <jnpatel81@gmail.com>
2021-05-30 06:21:36 +0000

    Sync LeetCode submission - N-Queens II (cpp)

--- problems/n-queens_ii/solution.cpp
+++ problems/n-queens_ii/solution.cpp
@@ -1,29 +1,37 @@
-bool valid(vector<int> &s, int r ,int c){
-    int n=s.size();
-    for (int i=0;i<r; i++){
-        if ((s[i]==c)||((r-i)==abs(c-s[i])))
-            return false;
-    }
-    
-    return true;
-}
-void Queens(vector<int> &s, int r ,int &i){
-    int n = s.size();
-    if (r==n) {i++; return;}
-    for (int c=0; c<n; c++){
-        if(valid(s,r,c)){
-            s[r]=c;
-            Queens(s,r+1,i);
-            s[r]=-1;
-        }
-    }
-}
 class Solution {
 public:
+    int N;
     int totalNQueens(int n) {
-        vector<int> r(n,0);
-        int i=0;
-         Queens(r,0,i);
-        return i;
+        vector<vector<bool>> board(n,vector<bool>(n,0));
+        int cnt=0;
+        N=n;
+        nq(board, 0,cnt);
+        
+        return cnt;
     }
+    void nq(vector<vector<bool>>& B, int r, int & cnt){
+        if ( r == N ) { cnt++; return; }
+        
+        for (int c=0; c<N; c++){
+            if (isAllowed(B,r,c)){
+                B[r][c]=1;
+                nq(B, r+1,cnt);
+                B[r][c]=0;
+            }
+        }
+    }
+    bool isAllowed(vector<vector<bool>>& B, int r, int c){
+        
+        for (int i=0; i<r; i++){
+            if (B[i][c]) return false;
+        }        
+        for (int i=r-1,j=c-1; i>=0 && j >=0; i--,j--){
+            if (B[i][j]) return false;
+        }
+        for (int i=r-1,j=c+1; i>=0 && j<N; i--,j++){
+            if (B[i][j]) return false;
+        }        
+        return true;
+    }
+   
 };
