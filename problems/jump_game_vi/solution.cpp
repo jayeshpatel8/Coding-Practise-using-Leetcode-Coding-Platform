f84db304b7d3ac4dd0f15f65bb396c4fce15b168
jayeshpatel8 <jnpatel81@gmail.com>
2021-06-10 03:44:26 +0000

    Sync LeetCode submission - Jump Game VI (cpp)

--- problems/jump_game_vi/solution.cpp
+++ problems/jump_game_vi/solution.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        
-        long  j=0;
-        if (k==1 || nums.size()<=2)
+        int n = nums.size();
+        int dp[n];
+        deque<int> dq;
+        dq.push_back(0);
+        dp[0]=nums[0];
+        for (int i=1; i<nums.size();i++)
         {
-            for (auto i : nums) j +=i;
-            return j;
+            if (dq.front() < i-k) dq.pop_front();
+            dp[i] = dp[dq.front()] + nums[i];
+            int cur = dp[i];
+            while (!dq.empty() && cur > dp[dq.back()]) dq.pop_back();
+            dq.push_back(i);
         }
-        int i=0,N=nums.size();
-        //j=nums[i];
-        for (; i<N; ){
-            j+=nums[i];
-            
-            int next = i+1,l=i+1;
-            // [1,-1,-2,4,-7,3], k = 2
-            while (l<=i+k && l<N && nums[l]<0) {                            
-              if(nums[next] < nums[l])
-                next = l;                
-              l++;
-            }
-            
-            if (l>i+k){
-                if(l>=N || next == N-1) { j+=nums[N-1];break;}
-                i =next;                
-            }
-            else if (l >=N-1){
-                j +=nums[N-1]; break;
-            }else{
-                i=l;
-            }            
-        }
-        return j;
+        return dp[n-1];
     }
 };
