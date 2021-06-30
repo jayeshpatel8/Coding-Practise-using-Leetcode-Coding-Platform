7fec5ac6eeea904e23d693fbc9bc5697030a2ec3
jayeshpatel8 <jnpatel81@gmail.com>
2021-06-30 06:00:47 +0000

    Sync LeetCode submission - Max Consecutive Ones III (cpp)

--- problems/max_consecutive_ones_iii/solution.cpp
+++ problems/max_consecutive_ones_iii/solution.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& A, int K) {
-        int i=0,j=0;
-        for (; i<A.size(); i++){
-            if(A[i]==0) K--;
-            if(K<0 && A[j++]==0){
-                K++;
+    int longestOnes(vector<int>& nums, int k) {
+        int ans=0,cnt=0;
+        //  [1,1,0,0,0,1,1,0]  k=2
+        for (int i=0,j=0; j<nums.size();j++){
+            if (nums[j]==0) cnt++;
+            while (cnt > k) {
+               if (nums[i++] == 0) cnt--;
             }
+            ans = max(ans,j-i+1);
         }
-        return i-j;
+        return ans;
+        
     }
 };
