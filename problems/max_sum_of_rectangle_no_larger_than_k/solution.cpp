6b5e3d92e3823d1555b24b38bbaf84b982f20374
jayeshpatel8 <jnpatel81@gmail.com>
2021-07-04 07:43:22 +0000

    Sync LeetCode submission - Max Sum of Rectangle No Larger Than K (cpp)

--- problems/max_sum_of_rectangle_no_larger_than_k/solution.cpp
+++ problems/max_sum_of_rectangle_no_larger_than_k/solution.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
+    
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-     
-        int M = matrix.size(), N = matrix[0].size();;
-        
-        
-        int ans=INT_MIN;
-        for (int i = 0; i<N; i++){
-            vector<int> prefix_sum(M,0);
-            for (int c = i; c<N; c++){                
-                int cur_sum=0, max_sum=INT_MIN;
-
-                for (int r = 0; r<M; r++){                    
-                    prefix_sum[r] += matrix[r][c];
-                    int v = prefix_sum[r];
-                    cur_sum = max(cur_sum + v , v);
-                    max_sum = max(max_sum, cur_sum);                                       
+        int m = matrix.size() , n  = matrix[0].size();
+        int ans = INT_MIN;
+        for (int c=0; c<n; c++){
+            vector<int> sum(m);
+            
+            for (int j=c; j<n; j++){
+                int psum=0, msum=INT_MIN;
+                for (int i=0; i<m; i++){
+                    sum[i] +=matrix[i][j];
+                    psum = max(psum +sum[i], sum[i]);
+                    msum = max(msum,psum);
                 }
-                    if (max_sum  <= k){
-                        ans = max(ans,max_sum);
-                        continue;
-                    }                
-                    set<int> st{0};
-                    int ps=0;
-                    for (auto sum : prefix_sum ){
-                        ps +=sum;
-                        int diff = ps - k;
-                        auto it  = st.lower_bound(diff);
-                        if (it != st.end()) ans = max(ans,(ps-*it));
-                        st.insert(ps);
+                if (msum <= k) {ans = max(ans,msum); continue;}
+                set<int> s={0};
+                psum=0;
+                for (auto i : sum){
+                    psum +=i;
+                    auto it  = s.lower_bound(psum - k);
+                    if (it != end(s)){
+                        ans = max(ans,psum-*it);
                     }
-                if (ans == k) return ans;
+                    s.insert(psum);
+                }
             }
-        }      
+        }
         return ans;
     }
 };
