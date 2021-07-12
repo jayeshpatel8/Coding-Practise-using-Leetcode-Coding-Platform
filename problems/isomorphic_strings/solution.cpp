ceea54d461acd69a401cd6e3f477d6a5af102836
jayeshpatel8 <jnpatel81@gmail.com>
2021-07-12 09:06:41 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- problems/isomorphic_strings/solution.cpp
+++ problems/isomorphic_strings/solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int cnt[256]={};
-        int cnt2[256]={};
-        for(int i=0;  i< s.length(); i++) {
-            int a = s[i], b= t[i];
-            if (cnt[a] != cnt2[b]) return false;
-             cnt[a]=cnt2[b]=i+1;
+       vector<int> pos1(128,-1),pos2(pos1);
+        for (auto i=0 ; i<s.length(); i++){
+
+            if ( pos1[s[i]] != pos2[t[i]]) return false;
+            pos1[s[i]] = pos2[t[i]] = i;
         }
         return true;
     }
