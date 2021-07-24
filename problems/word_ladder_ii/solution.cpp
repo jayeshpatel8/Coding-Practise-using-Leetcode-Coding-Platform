ccd99b8a86d64e7925bc0dc505bfc937d9b714ad
jayeshpatel8 <jnpatel81@gmail.com>
2021-07-24 20:12:39 +0000

    Sync LeetCode submission - Word Ladder II (cpp)

--- problems/word_ladder_ii/solution.cpp
+++ problems/word_ladder_ii/solution.cpp
@@ -1,101 +1,59 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-    
+    unordered_set<string> words;
+    unordered_map<string, vector<string>> graph;
+    unordered_map<string,int> visited;
+    vector<vector<string>> ans;
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        for (auto s : wordList) words.insert(s);
+        if (words.count(endWord) ==0) return {};
         
-        int n = wordList.size();
-        unordered_set<string> wset(wordList.begin(),wordList.end());
-        // wordList len =0 or endWord not found
-        if (wset.count(endWord)==0) return    vector<vector<string>>();     
-        
-        unordered_set<string> firstset{beginWord},endset{endWord};
-        bool flag=false, end=false;
-        unordered_map<string,vector<string>> dic;
-        int len=1;
-        
-        while(!firstset.empty() && !endset.empty() && !flag)
-        {
-            unordered_set<string> temp;            
-            if(firstset.size()>endset.size()){
-                firstset.swap(endset);
-                end = !end;
-            }
-            
-            for (const auto & s : firstset ) wset.erase(s);
-            for (const auto & s : endset ) wset.erase(s);
-            
-            for( auto s : firstset){                      
-                
-                 //cout << "[" <<s<<"]=";
-                string cur=s;
-                for (int i = 0; i<s.length(); i++)
-                {
-                    char old = s[i];
-                    for (char c = 'a'; c<='z'; c++)
-                    {
-                        s[i] = c;                    
-                        //if(wset.count(s)>0)
+        BFS(beginWord, endWord);
+        DFS(beginWord ,endWord);
+        return ans ; 
+    }
+    void BFS(string& b, string& e){
+        queue<string> q;
+        q.push(b);
+        visited[b]=1;
+        while(!q.empty()){
+            int sz = q.size();
+            bool end=false;
+            while (sz--){
+                string s = q.front(); q.pop();
+                string ts= s;
+                for ( auto & ch : s){
+                    char tc = ch;
+                    for (int c ='a' ; c <='z'; c++){
                         {
-                            
-                            if (endset.count(s)){
-                            const string* parent = &cur;
-                            const string* child = &s;
-
-                            if(end) swap(parent, child);                                
-                                flag=true;
-                                dic[*parent].push_back(*child);
-                                //cout << "END - " << cur << ":" << old << "-"<< s << ", ";
-                            }                            
-                            else if (wset.count(s) && !flag)
-                            {
-                                
-                                temp.insert(s);
-                            const string* parent = &cur;
-                            const string* child = &s;
-
-                            if(end) swap(parent, child);
-									 dic[*parent].push_back(*child);
-                                //cout <<  s << ", ";
-                            }                            
-                        }                                        
+                            ch = c;
+                            if (words.count(s) ){
+                                if (visited.count(s)==0){
+                                    visited[s]=visited[ts]+1;
+                                    graph[ts].push_back(s);
+                                    q.push(s);                                     
+                                }
+                                else if (visited[s] == visited[ts]+1){                                
+                                    graph[ts].push_back(s);
+                                }
+                                if (s==e) end= true;
+                            }
+                        }
                     }
-                    s[i]=old;                
+                    ch = tc;
                 }
             }
-            
-            firstset.swap(temp);
-            len++;
-        }
-        /*
-        for (auto s: dic){
-            cout << "dic[" << s.first<< "] = "<< s.second.size() <<"[ ";
-            for (auto t : s.second)
-                cout << t << " ";
-            cout << "] ";
-        }*/
-        if (flag){
-           vector<string> s{beginWord};
-            dfs(dic,beginWord,endWord,s);
-            
-        }
-        return res;                    
-    }
-void dfs(const unordered_map<string,vector<string>>& dic, const string & beginWord,const string & endWord, vector<string>& s){
-         if (beginWord==endWord )
-        {
-           
-                res.push_back(s); 
-            return;
-        }
-        
-        const auto it = dic.find(beginWord);
-        if(it == dic.cend()) return;
-         for(const string& child : it->second){
-            s.push_back(child);
-            dfs(dic,child,endWord,s);
-            s.pop_back();
+            if (end) break;
         }
     }
-   
+    void DFS(string& s,string& e, vector<string> st = {}){
+        st.push_back(s);
+        if (s == e) {
+            ans.push_back(st);
+        }
+        for (auto i : graph[s]){
+            DFS(i,e,st);
+        }
+        st.pop_back();
+    }
 };
