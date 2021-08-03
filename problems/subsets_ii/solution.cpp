0a6c423af12b1c18215b462c0a7d82ca837a0c96
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-03 10:06:08 +0000

    Sync LeetCode submission - Subsets II (cpp)

--- problems/subsets_ii/solution.cpp
+++ problems/subsets_ii/solution.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-         vector<vector<int>>res;
-        sort(nums.begin(),nums.end());
-        vector<int> t;   
-        subsetsWithDup(nums,res,t,0);
-        return res;
+        sort(begin(nums), end(nums));
+        vector<int> temp;
+        bt(nums,0,{temp});
+        return ans;
     }
-    private:
-    void subsetsWithDup(vector<int>& nums ,vector<vector<int>> & res, vector<int>&t, int pos) {
-        if (pos > nums.size()) {        
-            res.push_back(t);
-            return ;
-        }
-        res.push_back(t);   
-        
-        for (int i =pos; i < nums.size(); i++)
-        {
-            if (i-1>=pos && nums[i-1] == nums[i]) continue;
-            t.push_back(nums[i]);    
-            subsetsWithDup(nums,res,t,i+1);
-            t.pop_back();    
+    void bt(vector<int>& nums, int i, vector<int>& temp){
+        ans.push_back(temp);
+        for (int j=i ; j<nums.size(); j++){
+            if (j>i && nums[j]==nums[j-1]) continue;
+            temp.push_back(nums[j]);
+            bt(nums,j+1,temp);
+            temp.pop_back();
         }
     }
+    
 };
