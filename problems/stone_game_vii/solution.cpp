34716c28a73934757f52a93eba751fdfcfb6d805
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-05 20:26:12 +0000

    Sync LeetCode submission - Stone Game VII (cpp)

--- problems/stone_game_vii/solution.cpp
+++ problems/stone_game_vii/solution.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    int sum , N;
-    int dp1[1002][1002];
+    int dp[1002][1002]={} , psum[1002];
     int stoneGameVII(vector<int>& stones) {
-        N = stones.size();
-        //sum = accumulate(begin(stones),end(stones),0);
-        int dp[N+2][N+2];
-        memset(dp,0,sizeof(dp));
-        int pre[N+1];
-        pre[0] =0;
-        for (int i=1; i<=N; i++) pre[i] = pre[i-1] + stones[i-1];
-        for (int l=N; l>=1; l--){
-            for (int r=l; r<=N; r++)
-                dp[l][r] = max ( pre[r]-pre[l] - dp[l+1][r], pre[r-1]-pre[l-1]-dp[l][r-1]);
-        }
-        return dp[1][N];
-        //return dfs(stones,0,N-1,sum);;
-    }
-    int dfs(vector<int>& stones, int s, int e,  int sum){
-        if (s == e){
-            return dp1[s][e] = 0;
-            
-        }
-        if (dp1[s][e] != -1) return dp1[s][e];
+        psum[0]=0;
+        for (int i=0; i<stones.size(); i++) 
+            psum[i+1] = stones[i]+psum[i];
         
-        
-        return dp1[s][e] = sum - min (stones[s] + dfs(stones,s+1,e,sum-stones[s]),
-                                      stones[e] + dfs(stones,s,e-1,sum-stones[e])
-                              );
+        for (int l=1; l<stones.size(); l++){
+            for (int i=0; i<stones.size()-l; i++){
+                int  j = i+l;
+                dp[i][j] = max( psum[j+1] - psum[i+1] - dp[i+1][j],
+                                psum[j] - psum[i] - dp[i][j-1]);
+            }
+        }
+        return dp[0][stones.size()-1];
     }
 };
