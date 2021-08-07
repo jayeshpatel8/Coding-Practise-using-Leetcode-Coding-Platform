7f495f7cebe928e216ee314d186ed369a3137ad8
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-07 14:26:28 +0000

    Sync LeetCode submission - Palindrome Partitioning II (cpp)

--- problems/palindrome_partitioning_ii/solution.cpp
+++ problems/palindrome_partitioning_ii/solution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    
     int minCut(string s) {
-        int n = s.length();
-        int pal[n+1];
-        for (int i=0; i<=n; i++) pal[i]=i-1;
+        int N = s.size();
+
+        vector<vector<bool>> isPal(N+1,vector<bool>(N+1,false));
+        int cut[N+1];
+        for (int i=0; i<=N; i++) cut[i]=i-1;        
         
-        //aabba
-        for (int L=0; L<n; L++){
-            
-            bool pal1 = true,pal2=true;
-            for (int i=L, j =L,k=L+1; i>=0 && j<n && (pal1 || pal2); i--,j++){
-                if (pal1 && s[i] == s[j])
-                    pal[j+1] = min(pal[j+1], 1 + pal[i]);
-                else pal1=false;
-                if (pal2 && k<n && s[i] == s[k])
-                    pal[k++ +1] = min(pal[k+1], 1 + pal[i]);                
-                else pal2=false;
+        for ( int j=1; j<N; j++){
+            for (int i=j; i>=0; i--){                
+                if (s[i]==s[j] && (j-i < 3 || isPal[i+1][j-1]))
+                    isPal[i][j]=true, cut[j+1] = min(cut[j+1],1 + cut[i]);
             }
         }
-          return pal[n];      
+        
+        return  cut[N];
     }
 };
