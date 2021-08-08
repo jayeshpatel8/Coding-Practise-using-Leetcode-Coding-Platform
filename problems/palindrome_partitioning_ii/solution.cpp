32dd6b60f7afd41f0070299a6a47be1a59cc1a2b
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-08 14:59:50 +0000

    Sync LeetCode submission - Palindrome Partitioning II (cpp)

--- problems/palindrome_partitioning_ii/solution.cpp
+++ problems/palindrome_partitioning_ii/solution.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    
     int minCut(string s) {
-        int N = s.size();
+        int n = s.length();
+        int pal[n+1];
+        for (int i=0; i<=n; i++) pal[i]=i-1;
+        vector<int> d1(n),d2(n);
 
-        vector<vector<bool>> isPal(N+1,vector<bool>(N+1,false));
-        int cut[N+1];
-        for (int i=0; i<=N; i++) cut[i]=i-1;        
-        
-        for ( int j=1; j<N; j++){
-            for (int i=j; i>=0; i--){                
-                if (s[i]==s[j] && (j-i < 3 || isPal[i+1][j-1]))
-                    isPal[i][j]=true, cut[j+1] = min(cut[j+1],1 + cut[i]);
+        for (int i = 0, l1 = 0,l2=0, r1 = -1, r2= -1; i < n; i++) {
+            int k = (i > r1) ? 1 : min(d1[l1 + r1 - i], r1 - i + 1);
+            pal[i+1] = min(pal[i+1],pal[i]+1);
+            while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) {
+                pal[i+k+1] = min(pal[i+k+1],pal[i-k]+1);
+                k++;
             }
-        }
-        
-        return  cut[N];
+            d1[i] = k--;
+            if (i + k > r1) {
+                l1 = i - k;
+                r1 = i + k;
+            }
+            
+            if (i-1>=0  && s[i]==s[i-1])
+                pal[i+1] = min(pal[i+1],pal[i-1]+1);
+            k = (i > r2) ? 0 : min(d2[l2 + r2 - i + 1], r2 - i + 1);
+            while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) {
+                pal[i+k+1] = min(pal[i+k+1],pal[i-k-1]+1);
+                k++;
+                
+            }
+            d2[i] = k--;
+            if (i + k > r2) {
+                l2 = i - k - 1;
+                r2 = i + k ;
+                
+            }   
+			
+            if(i==n-1 && r2==i){
+			int j = i-1;
+				while( j >= l2)
+					pal[r2+1] = min (pal[r2+1], 1 + pal[j--]);
+			}            
+        }            
+        return pal[n];      
     }
+    
 };
