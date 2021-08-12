aba62858cc926b6530c4e7021e2ccaac0984c95f
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-12 09:50:40 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- problems/group_anagrams/solution.cpp
+++ problems/group_anagrams/solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-    vector<vector<string>> v;
-    unordered_map<string,int> map;
-    int idx=0;     
-        
-        for (auto s : strs)
-        {            
-            string os=s;
-            sort(s.begin(),s.end());
-            
-            if (map[s]==0)
-            {
-                map[s]=++idx;
-                vector<string> vs(1,os);                    
-                v.push_back(vs);
+        unordered_map<string,int> M;
+        vector<vector<string>> ans;
+        for (auto &s : strs){
+            string s1 = s;
+            sort(begin(s1),end(s1));            
+            if (M.count(s1))
+                ans[M[s1]].push_back(s);
+            else{
+                M[s1]=ans.size();
+                ans.push_back({s});
             }
-            else
-                v[map[s]-1].push_back(os);
         }
-        return v;
+        return ans;        
+        /*
+        unordered_map<string,vector<string>> M;
+        for (auto &s : strs){
+            string s1 = s;
+            sort(begin(s1),end(s1));            
+            M[s1].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto &  [k, v] :  M)
+            ans.push_back(v);
+        return ans;
+        */
     }
 };
