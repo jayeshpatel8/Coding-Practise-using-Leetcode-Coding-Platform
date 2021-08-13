bd49f55eece7477903f158faba13fab1612ecf08
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-13 10:24:36 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- problems/set_matrix_zeroes/solution.cpp
+++ problems/set_matrix_zeroes/solution.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-     int m= matrix.size(),n=matrix[0].size();
-        bool col0=0;
-        for (int i=0; i<m; i++){
-            if (0==matrix[i][0])  col0=1;
-            for (int j=1; j<n; j++)
-                if (0==matrix[i][j]){
-                    matrix[i][0]=matrix[0][j] =0;}
+        int  M = matrix.size(), N = matrix[0].size(), col0=0;
+        
+        for (int r = 0; r < M; r++){
+            if(!matrix[r][0]) col0=1;
+            for (int c = 1; c <N; c++){
+                if(!matrix[r][c])
+                    matrix[r][0] = matrix[0][c] = 0; 
+            }
         }
-
-        for (int i=m-1; i>=0; i--){
-            for (int j=n-1; j>=1; j--){
-                if( 0==matrix[i][0]  || ( 0==matrix[0][j]))
-                   matrix[i][j]=0;                                       
-            }          
-            if(col0)matrix[i][0]=0;      
-        }
+        for (int r = M-1; r >=0 ; r--){            
+            for (int c = N-1; c >=1; c--){
+                if(!matrix[r][0] || !matrix[0][c])
+                    matrix[r][c] = 0; 
+            }
+            if(col0) matrix[r][0]=0;
+        }        
     }
 };
