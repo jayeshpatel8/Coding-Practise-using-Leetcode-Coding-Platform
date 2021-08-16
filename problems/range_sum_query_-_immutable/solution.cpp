cb526cc21f1cbeb013f31c26f434382ca019273d
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-16 10:04:55 +0000

    Sync LeetCode submission - Range Sum Query - Immutable (cpp)

--- problems/range_sum_query_-_immutable/solution.cpp
+++ problems/range_sum_query_-_immutable/solution.cpp
@@ -1,16 +1,16 @@
 class NumArray {
+    vector<int> psum;
 public:
-    vector<int> prefixSum;
     NumArray(vector<int>& nums) {
-        prefixSum.resize(nums.size());
-        prefixSum[0]=nums[0];
-        for (int i=1; i < nums.size(); i++){
-            prefixSum[i] =prefixSum[i-1] +  nums[i];
+        psum = nums;
+        for (int i=1; i< psum.size(); i++){
+            psum[i] += psum[i-1];
         }
+        
     }
     
     int sumRange(int left, int right) {
-        return left ?(prefixSum[right] -  prefixSum[left-1]):prefixSum[right];        
+        return psum[right] - (left==0 ? 0 : psum[left-1]);
     }
 };
 
