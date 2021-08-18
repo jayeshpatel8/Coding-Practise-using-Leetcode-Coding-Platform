79c8983897cf865589cf892d62cc0bfdd98e7de8
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-18 08:48:47 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- problems/decode_ways/solution.cpp
+++ problems/decode_ways/solution.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int N;
-    int dp[100];
     int numDecodings(string s) {
-        N=s.length();
-        for ( int i=0; i<N;i++) dp[i]=-1;
-        return numDecodings_(s,0);
-    }
-    int numDecodings_(string s, int i) {
-        if (i>=N) return 1;
+        int  N = s.size();
+        int dp[N][2];
+        dp[0][0] = s[0] != '0'; dp[0][1]=0;
         
-        if (dp[i]!=-1) return dp[i];
-        int sum=0;
-        if (s[i]>'0' && s[i]<='9') {
-            sum += numDecodings_(s,i+1);
-        
-            if (s[i]<'3' && (1+i)<N && 
-             (   (s[i] == '2' && s[i+1]>='0' && s[i+1]<='6') || 
-              (s[i] == '1' && s[i+1]>='0' && s[i+1]<='9'))
-            )
-                sum +=numDecodings_(s,i+2);
+        for(int i=1; i<N; i++){
+            dp[i][0]=dp[i][1]=0;
+            if(s[i] != '0')    
+                dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            if (s[i-1] == '1' || ( s[i-1] == '2' && s[i]<='6' ))
+                dp[i][1] = dp[i-1][0];
         }
-        return dp[i]=sum;
+        return dp[N-1][0] + dp[N-1][1];
     }
 };
