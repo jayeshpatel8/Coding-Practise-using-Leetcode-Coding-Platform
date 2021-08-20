affddcb31e7d45a3c6021f854594daa72388b70f
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-20 08:52:24 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- problems/valid_sudoku/solution.cpp
+++ problems/valid_sudoku/solution.cpp
@@ -1,21 +1,16 @@
-
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-    short row[9]={0};
-    short col[9]={0};
-    short box[9]={0};
-        for (int r =0; r < 9; r++)
-        {
-            for (int c=0; c<9; c++)
-            {
-                if (board[r][c] == '.') continue;
-                short id = 1 << (board[r][c] -'0');
-                if (row[r] & id || col[c] & id || box[r/3*3+c/3]&id)
-                    return false;
-                row[r] |= id ; col[c] |= id ; box[r/3*3+c/3]|=id;
-            }
-        }
+        int row[9][10]={}, col[9][10]={}, box[3][3][10]={};
+        
+        for( int r=0; r< 9; r++)
+            for( int c = 0; c < 9; c++)
+                if (board[r][c] != '.'){
+                    int n = board[r][c] - '0';
+                    if (row[r][n]++ > 0 || col[c][n]++ > 0 || box[r/3] [c/3][n]++ > 0)                   
+                        return false;
+                }
+        
         return true;
     }
 };
