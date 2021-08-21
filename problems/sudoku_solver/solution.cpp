ab34189d7ded9b3634621eb272484dabde1f3a6d
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-21 09:36:53 +0000

    Sync LeetCode submission - Sudoku Solver (cpp)

--- problems/sudoku_solver/solution.cpp
+++ problems/sudoku_solver/solution.cpp
@@ -1,57 +1,30 @@
-int empty(vector<vector<char>>& board , int *r, int* c)
-{
-    for(*r=0;*r<9;(*r)++)
-    {
-        for(*c=0;*c<9;(*c)++)
-        {
-            if(board[*r][*c]=='.')
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;    
-}
-int backtrack(vector<vector<char>>& board , short *row, short *col, short *box)
-{
-    int r=0, c=0;
-    if(!empty(board,&r,&c))
-        return 1;
-    for (int k=1; k<=9; k++)
-    {
-        #if 1
-        short id = 1 << k;
-        if (row[r] & id || col[c] & id || box[r/3*3+c/3]&id)  
-            continue;
-        else
-        {
-            row[r] |= id ; col[c] |= id ; box[r/3*3+c/3]|=id;
-            board[r][c]='0'+k;
-             if (1 == backtrack(board ,row, col, box))
-                 return 1; 
-            board[r][c]='.';
-            row[r] &= ~id ; col[c] &= ~id ; box[r/3*3+c/3] &=~id;
-        }    
-        #endif
-    }
-    //cout << "r,c:"<< r << "," << c<< "|";
-    return 0;
-}
 class Solution {
 public:
+    int row[9][10]={},col[9][10]={},box[3][3][10]={};
     void solveSudoku(vector<vector<char>>& board) {
-    short row[9]={0};
-    short col[9]={0};
-    short box[9]={0};
-        for (int r =0; r < 9; r++)
-        {
-            for (int c=0; c<9; c++)
-            {
-                if (board[r][c] == '.') continue;
-                short id = 1 << (board[r][c] -'0');
-                row[r] |= id ; col[c] |= id ; box[r/3*3+c/3]|=id;
+        
+        for (int r = 0; r<9; r++){
+            for (int c=0; c<9; c++){
+                if (board[r][c] != '.'){
+                    int n = board[r][c] - '0';
+                    row[r][n] = col[c][n] = box[r/3][c/3][n]=1; 
+                }
             }
-        }   
-        cout << backtrack(board ,row, col, box); 
+        }
+        bt(board,0);
+    }
+    bool bt(vector<vector<char>>& board, int n){
+        int r = n/10, c = n%10;
+        if (r==9) return true;
+        
+        if (board[r][c]!='.') return bt(board,n+1);
+        for ( int i=1; i<=9; i++){
+            if (row[r][i] || col[c][i]|| box[r/3][c/3][i]) continue;
+            board[r][c] = i+'0'; row[r][i] = col[c][i]= box[r/3][c/3][i] = 1;
+            if (bt(board,n+1)) return true;
+            row[r][i] = col[c][i]= box[r/3][c/3][i] = 0;
+        }
+        board[r][c] = '.';
+        return false;
     }
 };
