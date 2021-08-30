0d66d79bda389fea44fad7d4b34d67d636cdecff
jayeshpatel8 <jnpatel81@gmail.com>
2021-08-30 05:45:53 +0000

    Sync LeetCode submission - Patching Array (cpp)

---
+++ problems/patching_array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        size_t patches = 0, sum = 1, i = 0, N = nums.size();
+        while (sum <= n){
+            if ( i < N && nums[i] <= sum){
+                sum += nums[i++];
+            }
+            else{
+                sum *=2;
+                patches++;
+            }
+        }
+        return patches;
+    }
+};
