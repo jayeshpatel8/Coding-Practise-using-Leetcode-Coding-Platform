48d44d7925069ad4f49d4952734eadae5ebb325d
jayeshpatel8 <jnpatel81@gmail.com>
2021-09-15 12:24:48 +0000

    Sync LeetCode submission - Longest Turbulent Subarray (cpp)

--- problems/longest_turbulent_subarray/solution.cpp
+++ problems/longest_turbulent_subarray/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-
     int maxTurbulenceSize(vector<int>& arr) {
         int n = arr.size();
-        if (n==1) return 1;
-        vector<vector<int>> dp(n,vector<int>(2,1));
-
-        int ans = 1;
+        int endless = 1, endgreat=1, ans= 1;        
         for (int i=1; i<n; i++){
-            if (arr[i-1]>arr[i]){
-                dp[i][0] +=dp[i-1][1];
-                ans = max(ans, dp[i][0]);
+            if (arr[i-1] == arr[i]) {
+                endless = endgreat = 1;
             }
             else if (arr[i-1] < arr[i]){
-                dp[i][1] +=dp[i-1][0];
-                ans = max(ans, dp[i][1]);                
+                endgreat = endless + 1;
+                endless = 1;
             }
-            // else 0 , 0
+            else{
+                endless = 1 + endgreat;
+                endgreat = 1;
+            }
+            ans = max(ans, max(endless, endgreat));
+
         }
         return ans;
     }
