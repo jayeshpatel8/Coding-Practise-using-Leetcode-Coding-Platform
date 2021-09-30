d6e978202a3aefa845cce1140a0800060e8ee348
jayeshpatel8 <jnpatel81@gmail.com>
2021-09-30 10:44:29 +0000

    Sync LeetCode submission - Partition to K Equal Sum Subsets (cpp)

--- problems/partition_to_k_equal_sum_subsets/solution.cpp
+++ problems/partition_to_k_equal_sum_subsets/solution.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int ksum = 0, N;
-    int dp[(1<<16)];
-    bool canPartitionKSubsets(vector<int>& nums, int k) {     
-        for (auto i : nums) ksum +=i;
-        if (ksum % k) return false;
-        ksum /=k;
-        N=nums.size();
+    int N, tsum;
+    int dp[1<<16];
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        if(k==1) return true;
+        int sum = 0;
+        for (auto i : nums) sum +=i;
+        
+        if (sum % k) return false;
+        N  = nums.size();
+        tsum = sum/k;
         memset(dp,-1,sizeof(dp));
-        return partsum(nums,0,k,0);
+        return dfs(nums,k-1,0);
     }
-    bool partsum(vector<int>& nums,int mask, int k, int sum = 0){
-        if (k == 1) return dp[mask] = true;
-        if(sum == ksum) return dp[mask] = partsum(nums,mask,k-1,0);
-        if (sum > ksum ) return dp[mask] = false;
-        if (dp[mask] != -1) return dp[mask];
-        for (int i=0; i<N; i++){
-            if ((mask & (1<<i)) == 0){
-                if (partsum(nums, mask | (1<<i) ,k, sum + nums[i])) return dp[mask] = true;
+    int dfs(vector<int>& nums,  int k, int sum, int mask = 0){
+        //if (mask == (1 <<N)-1) return false;
+        if (dp[mask] !=  -1) return dp[mask];
+        if(sum == tsum){            
+            if (k == 1) return true;
+            return dfs(nums,k-1,0,mask);
+        }
+        
+        for ( int j = 0; j<N; j++){
+            if ((mask & (1<<j)) == 0 && sum + nums[j] <= tsum){
+                if (dp[mask| (1 <<j)] = dfs(nums,k,sum + nums[j],mask | (1 <<j)))
+                    return  true;
             }
         }
         return dp[mask] = false;
