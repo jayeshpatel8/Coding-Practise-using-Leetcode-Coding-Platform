77204e24002adee5f13e26def3d6a580c42289ff
jayeshpatel8 <jnpatel81@gmail.com>
2021-10-02 13:04:46 +0000

    Sync LeetCode submission - Dungeon Game (cpp)

--- problems/dungeon_game/solution.cpp
+++ problems/dungeon_game/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int m = dungeon.size(),n = dungeon[0].size();
-        vector<vector<int>> cost(m+1,vector<int>(n+1,INT_MAX));
-        cost[m][n-1]=cost[m-1][n]=1;
-        for (int i=m-1; i>=0; i--){
-            for (int j=n-1; j>=0; j--){
-                    cost[i][j] = min(cost[i+1][j],  cost[i][j+1]) - dungeon[i][j];
-                    if (cost[i][j] < 1) cost[i][j] = 1;    
-            }
+    int calculateMinimumHP(vector<vector<int>>& dun) {
+        int M= dun.size(), N = dun[0].size();        
+         vector<vector<int>> dp(M+1,vector<int>(N+1,INT_MAX));
+        dp[M - 1][N] = 1;
+        dp[M][N - 1] = 1;
+        for ( int i = M-1; i>=0; i--)
+        {
+            for ( int j = N-1; j>=0; j--)
+                    dp[i][j] = max(min(dp[i][j+1],dp[i+1][j])-dun[i][j],1);
         }
-        return cost[0][0];
+        return dp[0][0];
     }
 };
