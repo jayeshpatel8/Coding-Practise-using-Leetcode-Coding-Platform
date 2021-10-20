143dd2de5a87d50c784dd44eb134cecd125cb4c0
jayeshpatel8 <jnpatel81@gmail.com>
2021-10-20 22:40:22 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

--- problems/reverse_words_in_a_string/solution.cpp
+++ problems/reverse_words_in_a_string/solution.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-
     string reverseWords(string s) {
-        int n = s.length();
-        // remove the space and reverse each word
-        int start=0,j=-1,i=0;
-        for (; i<n; i++){
-            if (s[i]==' ' && j==-1 || s[i] != ' ' && j!=-1) continue;
-            if (j==-1) j=i;
+        string s1;
+        for (int i=0; i<s.size(); ){
+            if (s[i] == ' ') { i++; continue;}
             else{
-                
-                //rev word
-                int st=j, en=i-1;
-                while(st<en){ char c = s[st]; s[st++]=s[en];s[en--]=c; }        
-                
-                if (start!=0) s[start++]=' ';
-                //copy to start
-                int len=i-j;
-                while(len--) s[start++]=s[j++];
-                j=-1;
+                int j=i;
+                string s2;
+                while(j<s.size() && s[j] != ' ') s2.push_back(s[j++]);
+                reverse(s2.begin(), s2.end());
+                s1 += s2 + " ";
+                i=j;
             }
         }
-        
-        if (j!=-1){
-                int st=j, en=i-1;
-                while(st<en){ char c = s[st]; s[st++]=s[en];s[en--]=c; }        
-                if (start!=0) s[start++]=' ';
-                //copy to start
-                int len=i-j;
-                while(len--) s[start++]=s[j++];
-        }
-        
-        // str rev
-        i=0, j=start-1;
-        while (i<j){
-            char c  = s[j];
-            s[j--]=s[i];
-            s[i++]=c;
-        }
-        s.resize(start);
-        return s;
+        s1.pop_back();
+        reverse(s1.begin(),s1.end());
+        return s1;
     }
 };
