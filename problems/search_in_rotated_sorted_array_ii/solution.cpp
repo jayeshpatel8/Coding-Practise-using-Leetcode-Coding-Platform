a77016c0a0c7bc15ac48b2fd2dbc81b59ec2f136
jayeshpatel8 <jnpatel81@gmail.com>
2021-10-23 12:48:19 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-     
-        int l=0,r=nums.size()-1;
-        while (l<=r)
-        {
-            int m = l+(r-l)/2;
-
-            if (nums[m]==target) return true;
-            
-            if (nums[l] == nums[m] && nums[m]==nums[r]){
-                 l++;r--; continue;
-            }
-
-            if (nums[l] <= nums[m] )
-                if ( target >= nums[l] &&  target< nums[m]) 
-                    r=m-1;
-                else
-                    l=m+1;
-            else
-                if ( target > nums[m] &&  target<= nums[r])
-                    l=m+1;
-                else
-                    r=m-1;
-
-        }
-
-        return false;
-        
+    bool search(vector<int>& nums, int t) {
+      int l = 0, r = nums.size()-1;
+      while(l<=r){
+          int m = (l+r)/2;
+          if (nums[m] == t) return true;
+          if (nums[l] < nums[m]){
+              
+              if (t >= nums[l] && t < nums[m])
+                  r = m-1;
+              else 
+                  l= m+1;
+          }
+          else if (nums[m] < nums[r]){
+              
+              if (t > nums[m] && t <= nums[r])
+                  l = m+1;
+              else 
+                  r= m-1;              
+          }
+          else if (nums[r] != t)
+              r--;
+          else
+              return true;
+      }
+      return false;
     }
 };
