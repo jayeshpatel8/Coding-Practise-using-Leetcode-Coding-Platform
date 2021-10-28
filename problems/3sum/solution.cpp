07db27ffebb8212ad92f68500ed2a84b7274633f
jayeshpatel8 <jnpatel81@gmail.com>
2021-10-28 14:19:09 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- problems/3sum/solution.cpp
+++ problems/3sum/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // -1 -1  -0 1 2 4
-        int n = nums.size();
-        if (n<3) return {};
+        sort(begin(nums), end(nums));
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        
-        for (int i=0; i<n-2 ; i++){            
+        for (int i=0; i<nums.size(); i++){
             if (i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1, k=n-1;
-            while(j<k){
-                long sum3 = nums[i] + nums[j] + nums[k];
-                if ( sum3 < 0 )  j++;
-                else if( sum3 > 0 )  k--;
+            int l  =i+1, r = nums.size()-1;
+            while (l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if ( sum > 0) r--;
+                else if ( sum < 0) l++;
                 else{
-                    int n1=nums[i],n2=nums[j],n3=nums[k];;
-                    while(j<=k && n2 == nums[j])
-                        j++;
-                    
-                    while(j<=k && n3 == nums[k])
-                        k--;
-                    ans.push_back({n1,n2,n3});
+                    ans.push_back({nums[i] , nums[l] , nums[r]});
+                    while( l+1<r && nums[l] ==nums[l+1]) l++;
+                    while( r-1>l && nums[r] ==nums[r-1]) r--;
+                    l++,r--;
                 }
-            }            
+            }
         }
         return ans;
     }
 };
+
+/*
+    -4 -1 -1 0 1 2  
+*/
