f054c99429f109d73a94202d04c28805a2bdb70a
jayeshpatel8 <jnpatel81@gmail.com>
2021-11-01 15:20:27 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (cpp)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    int dp[100001][2][2];
     int maxProfit(vector<int>& prices) {
-        int len = prices.size();
-        if (len < 2) return 0;
-        int dp[len+1], min=prices[0];
-        dp[0]=0;
-        for (int i = 1; i<= len; i++)
-        {
-            if (prices[i-1]< min) min= prices[i-1];
-            dp[i] = max(prices[i-1]-min, dp[i-1]);
-        }
-        int max=prices[len-1];
-        int res=-1;
-        for (int i = len; i>0; i--)
-        {
-            if (prices[i-1]> max) max= prices[i-1];
-            dp[i] = std::max(max-prices[i-1] + dp[i-1], dp[i]);
-            if (dp[i]>res) res=dp[i];
-        }        
-        return res;
+        memset(dp,-1,sizeof(dp));
+        return dfs(prices,0,1,0);
+    }
+    int dfs(vector<int>& prices, int i, bool buy,int cnt){
+        if (i>= prices.size() || cnt >= 2) return 0;
+        if (dp[i][buy][cnt] != -1) return dp[i][buy][cnt];
+        int ans = dfs(prices,i+1,buy,cnt);
+        if (buy)
+            ans = max(ans, -prices[i] + dfs(prices,i+1,0,cnt) );
+        else
+            ans = max(ans, prices[i] + dfs(prices,i+1,1,cnt+1) );
+        
+        return dp[i][buy][cnt] = ans;
+        
     }
 };
