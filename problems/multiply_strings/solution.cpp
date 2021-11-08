fcc8808437c3fde2405f685c5b55c273ff9d90cf
jayeshpatel8 <jnpatel81@gmail.com>
2021-11-08 02:15:30 +0000

    Sync LeetCode submission - Multiply Strings (cpp)

--- problems/multiply_strings/solution.cpp
+++ problems/multiply_strings/solution.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1.size() < 1|| num2.size() <1) return "0";
-
-        int s1 =num1.size(),s2=num2.size();
-        vector<int> arr(s1+s2,0);
-        for (int i=s1-1; i>=0;i--)
-        {
-            for (int j=s2-1; j>=0 ;j--)
-            {                   
-                int p = s1 -i-1 +s2 -j-1;
-                int sum  = (num2[j]-'0')*(num1[i]-'0')+arr[p];  
-                arr[p+1] += sum/10;
-                arr[p] = sum%10;                  
-            }            
+        int l1 = num1.size(), l2 = num2.size();
+        vector<int> sum(l1 + l2);
+        for (int i=l1-1; i>=0; i--){
+            for (int j=l2-1; j>=0; j--){
+                int idx = l1-1-i + l2-1-j;
+                int s = sum[idx] + (num1[i]-'0') * (num2[j]-'0');
+                sum[idx+1] += s/10;
+                sum[idx] = s%10;
+            }
         }
-
-        int i=s1+s2-1;        
-        while (i && arr[i]==0)
-            i--;
-        string r("");
-        while (i>=0){
-            r +=(arr[i--]+'0');            
-        }
-        return r;
+        int i = l1 + l2 - 1;
+        for (; i>=1 && sum[i]==0; i--) ;
+        string ans;
+        for (; i>=0 ; i--) 
+            ans.push_back(sum[i]+'0');
+        return ans;
     }
 };
