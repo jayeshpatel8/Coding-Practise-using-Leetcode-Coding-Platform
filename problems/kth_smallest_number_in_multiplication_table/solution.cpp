da938d5870520716e3d7cbd330652c03090291ef
jayeshpatel8 <jnpatel81@gmail.com>
2021-11-16 11:20:02 +0000

    Sync LeetCode submission - Kth Smallest Number in Multiplication Table (cpp)

---
+++ problems/kth_smallest_number_in_multiplication_table/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int lo = 1, hi = m * n;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (!enough(mi, m, n, k)) lo = mi + 1;
+            else hi = mi;
+        }
+        
+        return lo;        
+    }
+   bool  enough(int x, int m, int n, int k) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += min(x / i, n);
+        }
+        return count >= k;
+    }
+};
