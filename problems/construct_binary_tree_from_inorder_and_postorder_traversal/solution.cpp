51a09fe6b8f99ae89cdc35825b713e028ca0af31
jayeshpatel8 <jnpatel81@gmail.com>
2021-11-21 11:20:32 +0000

    Sync LeetCode submission - Construct Binary Tree from Inorder and Postorder Traversal (cpp)

--- problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.cpp
+++ problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.cpp
@@ -11,24 +11,21 @@
  */
 class Solution {
 public:
-    int idx=0;
+    unordered_map<int,int> ino_idx;
+    int i ;
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if (inorder.size()<1) NULL;
-        unordered_map<int,int> map;
-        for (int i=0; i<inorder.size(); i++)
-            map[inorder[i]]=i;
-        idx=inorder.size()-1;
-        return buildTree(inorder, postorder ,  map, 0, inorder.size()-1);
+        for (int i=0; i<inorder.size(); i++) 
+            ino_idx[inorder[i]] = i;
+        i = postorder.size()-1;
+        return rec(0,postorder.size()-1, postorder);
     }
-   TreeNode* buildTree(vector<int>& in, vector<int>& po , unordered_map<int,int> & map, int st, int en)   {
-       if(st>en) return NULL;
-       int val =po[idx--];
-       TreeNode*root = new TreeNode(val);
-       if (st==en) return root;
-       int i = map[val];
-       root->right = buildTree(in, po,  map, i+1,en);
-       root->left = buildTree(in, po,  map, st,i-1);
-       return root;
-       
-   }
+    TreeNode * rec(int st, int en,  vector<int>& po){
+        if (st > en) return NULL;
+        if (st == en) return new TreeNode (po[i--]);
+        int val = po[i--] , idx = ino_idx[val];
+        TreeNode * root = new TreeNode(val);
+        root -> right = rec(idx+1,en,po);
+        root -> left = rec(st,idx-1,po);
+        return root;
+    }
 };
