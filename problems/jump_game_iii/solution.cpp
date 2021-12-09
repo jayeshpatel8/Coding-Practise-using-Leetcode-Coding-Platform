bb024727dcbd2abc0f25e1106f028b0824e74464
jayeshpatel8 <jnpatel81@gmail.com>
2021-12-09 06:01:45 +0000

    Sync LeetCode submission - Jump Game III (cpp)

--- problems/jump_game_iii/solution.cpp
+++ problems/jump_game_iii/solution.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    unordered_map<int,bool> M;
     bool canReach(vector<int>& arr, int start) {
+        if (!arr[start]) return true;
+        queue<int> q;
+        q.push(start);
+        int N = arr.size();
+        vector<bool> vis(N,false);
         
-        if (arr[start]==0) return M[start]=true;
-        if (M.count(start)) return M[start];
-        M[start] = false;
-        if (((start+arr[start]) < arr.size() && canReach(arr,start+arr[start])) ||
-           ((start-arr[start]) >=0 && canReach(arr,start-arr[start])))
-           M[start] = true;
-        return M[start];
+        while(!q.empty()){
+            int i = q.front(); q.pop();
+            if (arr[i] == 0) return true;
+            
+            vis[i]=true;
+            int j = i + arr[i];
+            if (j <N && !vis[j]) 
+                q.push(j);
+            j = i - arr[i];
+            if (j >=0 && !vis[j]) 
+                q.push(j);
+        }
+        return false;
     }
 };
