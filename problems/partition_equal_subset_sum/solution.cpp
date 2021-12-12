8282f239c7f10555a8f1cf008b8c420fbdff923a
jayeshpatel8 <jnpatel81@gmail.com>
2021-12-12 10:02:33 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (cpp)

--- problems/partition_equal_subset_sum/solution.cpp
+++ problems/partition_equal_subset_sum/solution.cpp
@@ -1,20 +1,32 @@
 class Solution {
-public:    
+public:
+    //int dp[200][10001];
+    
     bool canPartition(vector<int>& nums) {
-        // total sum has to be even
-        int total = 0;
-        for (auto i : nums) total +=i;
-        if (total%2) return false;
-        // find subset sum of total/2
-        vector<bool> dp(total/2+1);
-        dp[0]=true;
+        int sum  = 0;
+        
+        for (auto i : nums) sum += i ;
+        if (sum%2) return false;
+        
+        vector<bool> dp(sum/2+1);
+        dp[0]=1;
         for (auto i : nums){
-            for (int sum=total/2-i; sum>=0; sum--){
-                if (dp[sum] ){
-                    dp[sum+i]=true;
-                }
+            for (int s = sum/2 - i; s>=0; s--){
+                if (dp[s])
+                    dp[s+i] = true;
             }
         }
-        return dp[total/2];
+        return dp[sum/2];
+        //memset(dp, -1, sizeof(dp));     
+        //return dfs(nums,0,sum/2);
     }
+    /*
+    int dfs(vector<int>& nums, int i,int sum){
+        if (i >= nums.size() || sum < 0) return sum==0;
+        if (sum == 0) return true;
+        if (dp[i][sum] != -1) return false;
+        
+        return dp[i][sum] = dfs(nums,i+1,sum) || dfs(nums,i+1,sum - nums[i]);
+    }
+    */
 };
