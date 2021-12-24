0fdf73c99e3389e7c5e939b43622f314a205ec14
jayeshpatel8 <jnpatel81@gmail.com>
2021-12-24 20:24:36 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- problems/merge_intervals/solution.cpp
+++ problems/merge_intervals/solution.cpp
@@ -1,29 +1,19 @@
-bool comp(vector<int>& a, vector<int>& b){
-    return a[0]<b[0];
-}
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> res;
-        int n = intervals.size();
-        if (n)
-        {
-            sort(intervals.begin(),intervals.end(),comp);
-            res.push_back(vector<int>({intervals[0][0],intervals[0][1]}));
-        }
-        int j=0;    
-        for (int i=0; i<n;i++)
-        {
-            if (res[j][1]<intervals[i][0]){
-                res.push_back(vector<int>({intervals[i][0],intervals[i][1]}));
-                j++;
-            }
-            else
-            {
-                if (res[j][1]<intervals[i][1])
-                res[j][1]=intervals[i][1];
+    vector<vector<int>> merge(vector<vector<int>>& intv) {
+        vector<vector<int>> ans;
+        sort(begin(intv), end(intv), [](auto &i, auto &j){ return i[0] < j[0];});
+        
+        int s = intv[0][0], e = intv[0][1];
+        for (int i=1; i<intv.size(); i++){
+            if (intv[i][0] > e){
+                ans.push_back({s,e});
+                s = intv[i][0] , e = intv[i][1];
+            }else{
+                e = max(e,intv[i][1]); 
             }
         }
-        return res;
+        ans.push_back({s,e});
+        return ans;
     }
 };
