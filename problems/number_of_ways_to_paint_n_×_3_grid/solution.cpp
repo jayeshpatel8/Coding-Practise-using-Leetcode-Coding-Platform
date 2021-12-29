1ebfe2c3ba4e4340fc58a83bee8fb41345d74314
jayeshpatel8 <jnpatel81@gmail.com>
2021-12-29 15:20:35 +0000

    Sync LeetCode submission - Number of Ways to Paint N × 3 Grid (cpp)

--- problems/number_of_ways_to_paint_n_×_3_grid/solution.cpp
+++ problems/number_of_ways_to_paint_n_×_3_grid/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int dp[5001][4][4][4]={}, mod = 1e9 + 7;
+int dp[5001][4][4][4]={}, mod = 1e9 + 7;
     int numOfWays(int n, int r=0,int c=0, int c1=0, int c2=0, int c3=0) {
         if (r == n) return 1;
         if (c == 3) return numOfWays(n,r+1,0,c1,c2,c3);
         if(c==0 && dp[r][c1][c2][c3]!=0) return dp[r][c1][c2][c3];
-        int ans=0;
+        int ans = 0;
         for (int col=1; col<=3; col++){
             if (c==0){
                 if (col !=c1) ans = (ans + numOfWays(n,r,c+1,col,c2,c3))%mod;                
@@ -17,7 +17,7 @@ public:
                 if (col !=c3 && col !=c2) ans = (ans + numOfWays(n,r,c+1,c1,c2,col))%mod;               
             }
         }
-        if (c==0) dp[r][c1][c2][c3]=ans;
-        return ans;
+        if (c==0) dp[r][c1][c2][c3] = ans;
+        return  ans;
     }
 };
