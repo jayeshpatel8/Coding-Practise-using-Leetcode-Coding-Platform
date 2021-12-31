76264b70127554f3c4d2c1680e1e507b74fde3d4
jayeshpatel8 <jnpatel81@gmail.com>
2021-12-31 11:14:59 +0000

    Sync LeetCode submission - Stone Game VII (cpp)

--- problems/stone_game_vii/solution.cpp
+++ problems/stone_game_vii/solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int dp[1002][1002]={} , psum[1002];
+    int dp[1001][1001];
     int stoneGameVII(vector<int>& stones) {
-        psum[0]=0;
-        for (int i=0; i<stones.size(); i++) 
-            psum[i+1] = stones[i]+psum[i];
-        
-        for (int l=1; l<stones.size(); l++){
-            for (int i=0; i<stones.size()-l; i++){
-                int  j = i+l;
-                dp[i][j] = max( psum[j+1] - psum[i+1] - dp[i+1][j],
-                                psum[j] - psum[i] - dp[i][j-1]);
-            }
-        }
-        return dp[0][stones.size()-1];
+        memset(dp,-1,sizeof(dp));
+        return dfs(stones,0,stones.size()-1, accumulate(begin(stones),end(stones),0));
+    }
+    int dfs(vector<int>& stones, int i, int j, int sum) {
+        if (i==j) return dp[i][j] = 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        int ans = max(sum - stones[i] - dfs(stones, i+1,j,sum-stones[i]) ,sum - stones[j] - dfs(stones, i,j-1,sum-stones[j]));
+        return dp[i][j]=ans;
     }
 };
