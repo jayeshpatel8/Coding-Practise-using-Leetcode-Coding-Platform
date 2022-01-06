9c8e335348db6d86f948572d588f09dcee04ca19
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-06 20:55:15 +0000

    Sync LeetCode submission - Distinct Echo Substrings (cpp)

--- problems/distinct_echo_substrings/solution.cpp
+++ problems/distinct_echo_substrings/solution.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int distinctEchoSubstrings(string text) {
-        unordered_set<string> s;
+        unordered_set<int> s;
         int n  = text.size();
-        /*
-        for (int i=2;i<=n; i++){
-            for (int j=0; j<=n-i;j++){
-                s.insert(text.substr(j,i));
-            }
-        }*/
-        int ans=0;
-        for ( int k=0; k<n; k++){
-            string  j ;
-            for (int i=k; i<n; i++){
-                j  +=text[i] ;  
-                int len=i-k+1;
-                for (int q=k, p=i+1; p<n && len >0; len-- ){
-                  if (text[p++]!=text[q++]) break;
-                }
-                if (len == 0){
-                    if (s.count(j)==0){
-                        ans++; s.insert(j);
-                    }
-                }
+
+        int mod=1e9 + 7;
+        for ( int k=1; k<n; k++){            
+             long long h_r=0, h_l=0, base=1;
+            for (int r=k, l=k-1; l >= 0 && r < n; --l,r++){
+                h_r = (h_r * 29 + text[r]) % mod;
+                h_l = (h_l + text[l] * base) % mod;
+                base= (base * 29)%mod;
+                if ( h_r == h_l)
+                    s.insert(h_l);
             }
         }
-        return ans;
-    }
+        return s.size();
+    }  
 };
+
