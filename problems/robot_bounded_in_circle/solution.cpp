2a98a17e82f8635a888e4c2f8b2bfe51d853d8f1
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-09 23:35:30 +0000

    Sync LeetCode submission - Robot Bounded In Circle (cpp)

---
+++ problems/robot_bounded_in_circle/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isRobotBounded(string ins) {
+        int dir[4][2] = {{0,1},{-1,0},{0,-1},{1,0}}; // N, L, S, R
+        int x =0, y = 0, i = 0;
+        for ( auto c : ins){
+            if (c  == 'L')
+                i = (i + 1)%4;
+            else if (c  == 'R')
+                i = (i+3)%4;
+            else                
+                x += dir[i][0], y += dir[i][1];
+        }
+        return (x==0 && y==0)  || i!=0;
+    }
+};
