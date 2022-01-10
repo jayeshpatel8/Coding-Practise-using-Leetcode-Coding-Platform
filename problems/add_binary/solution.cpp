ee6b589688c38b59c089dda5222be4b3564f3909
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-10 10:46:06 +0000

    Sync LeetCode submission - Add Binary (cpp)

--- problems/add_binary/solution.cpp
+++ problems/add_binary/solution.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int al=a.size(),bl=b.size();
-        int l = (al>bl?bl:al);
-        int c=0;
-        string s;
-        while (l--){
-            int sum = a[--al]+b[--bl]+c;            
-            s.push_back(sum%2+'0');
-            c=(sum >97);
-        }
-        while(al)
-        {            
-            int sum = a[--al]+c;
-            s.push_back(sum%2+'0');
-            c=(sum>49);
-        }            
-        while(bl)
-        {
-            int sum = b[--bl]+c;
-            s.push_back(sum%2+'0');
-            c=(sum>49);
-        }  
-        
-        if(c) s.push_back('1');
-        reverse(s.begin(),s.end());
-        return s;
+        int c = 0,i =a.size()-1, j=b.size()-1;
+        string ans;
+        while (i >=0 || j>=0 || c){
+            c += (i>=0? a[i--]-'0' :  0 )+ (j>=0 ? b[j--]-'0' : 0 );
+            ans += (c& 1)+'0';
+            c = c>1;        
+        }    
+
+        reverse(begin(ans), end(ans));
+        return ans;
     }
 };
