7a5f0ecf9b992dd5eff48b9085927bbc3ba8fcd9
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-12 15:12:18 +0000

    Sync LeetCode submission - Goal Parser Interpretation (cpp)

---
+++ problems/goal_parser_interpretation/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string interpret(string c) {
+        string ans;
+        for (int i=0; i<c.size(); i++){
+            if (c[i] == 'G') ans+='G';
+            else if ('('==c[i] &&  c[i+1] == ')') ans+='o',i++;
+            else ans+="al", i+=3;
+        }
+        return ans;
+    }
+};
