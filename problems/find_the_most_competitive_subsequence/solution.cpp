af8ea560a7f7475e6e5b56e548157e5326e2f9f9
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-13 15:17:10 +0000

    Sync LeetCode submission - Find the Most Competitive Subsequence (cpp)

--- problems/find_the_most_competitive_subsequence/solution.cpp
+++ problems/find_the_most_competitive_subsequence/solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
-public :
+public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
-        //nums = [2,4,3,3,5,4,9,6], k = 4
-        int max_nums_to_delete = nums.size()-k;
-        vector<int> s;
-         
-        for (int i= 0; i< nums.size(); i++){
-           while(!s.empty() && nums[i] < s.back()   && max_nums_to_delete){
-               max_nums_to_delete--;
-               s.pop_back();
-           }
-            s.push_back(nums[i]);
+        
+        int n = nums.size();
+        vector<int> ans(n);
+        
+        ans[0]=nums[0];
+        for (int i=1,j=1; i<n; i++){
+            
+            while( j> 0 && (ans[j-1] > nums[i]) && (n - i - 1 + j)>=k){                
+                j--;
+                
+            }
+            ans[j++]=nums[i];
         }
-        s.resize(k);
-        return s;
+        vector<int> a(begin(ans),begin(ans)+k);
+        
+        return a;
     }
 };
