538bf3af47b99978f870a28ab0f25ff948757d8a
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 10:32:37 +0000

    Sync LeetCode submission - Check If Two String Arrays are Equivalent (cpp)

--- problems/check_if_two_string_arrays_are_equivalent/solution.cpp
+++ problems/check_if_two_string_arrays_are_equivalent/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-         string w1 ,w2;
-        for (auto c : word1) w1+=c;
-        for (auto c : word2) w2+=c;
-        return w1==w2;
+        string s1 ,s2;
+        for (auto s : word1) s1+=s;
+        for (auto s : word2) s2+=s;
+        return s1==s2;
     }
 };
