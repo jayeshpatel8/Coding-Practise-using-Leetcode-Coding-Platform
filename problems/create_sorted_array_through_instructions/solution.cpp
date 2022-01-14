e8d2a10d8120f2f6834327ae2dd37cc8e60e9554
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 15:22:47 +0000

    Sync LeetCode submission - Create Sorted Array through Instructions (cpp)

--- problems/create_sorted_array_through_instructions/solution.cpp
+++ problems/create_sorted_array_through_instructions/solution.cpp
@@ -1,42 +1,23 @@
-typedef struct BT{
-    BT * left,*right;
-    int v;
-    BT(int val):v(val),left(0),right(0){}
-}BT;
 class Solution {
 public:
-        const int BITree_size = 100002;
-        vector<uint32_t> BITree;
-    
-    int createSortedArray(vector<int>& inst) {
-        
-        uint32_t cost=0, Mod=1e9+7;
-        BITree.resize(BITree_size);
-        //[1,5,6,2]
-        for (int i=0; i< inst.size(); i++){
-            
-            uint32_t left=QueryToBITree(inst[i]-1),right=i-QueryToBITree(inst[i]);
-            
-            
-            cost +=min(left,right);
-            cost%=Mod;
-            
-            addToBITree(inst[i]);
-        }
-        return cost;
+    int arr[100001]={};
+    void add(int i ){
+        for (; i < 100001; i += i & -i)
+            arr[i]++;
     }
-    void addToBITree(int idx){
-        
-        for (; idx < BITree_size; idx+= idx & -idx){
-            BITree[idx]++;
-        }
+    int get (int i){
+        int ans = 0;
+        for (; i> 0; i -= i & -i)
+            ans += arr[i];        
+        return ans;
     }
-    uint32_t QueryToBITree(int idx){
-        uint32_t cost =0;
-        for (; idx > 0; idx-= idx & -idx){
-            cost +=BITree[idx];
+    int createSortedArray(vector<int>& ins) {
+        int ans = 0 ,mod = 1e9 + 7;
+        add(ins[0]);
+        for (int i=1; i<ins.size(); i++){
+            ans = (ans + min(get(ins[i]-1), i - get(ins[i])))%mod; 
+            add(ins[i]);
         }
-        return cost;
-    }  
-
+        return ans;
+    }
 };
