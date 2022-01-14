fdea44003d57467ada3f90dec2bce6eae3aaa674
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 11:21:42 +0000

    Sync LeetCode submission - Minimum Initial Energy to Finish Tasks (cpp)

--- problems/minimum_initial_energy_to_finish_tasks/solution.cpp
+++ problems/minimum_initial_energy_to_finish_tasks/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        sort(begin(tasks), end(tasks),[](auto&a, auto &b){ return a[1]-a[0] > b[1]-b[0];});
-        int ans = 0, bal=0;
-        for (auto &i : tasks){
-            if (bal < i[1])
-                ans +=  i[1]-bal,bal=i[1];
-            
-                bal -= i[0];
+        sort(begin(tasks),end(tasks),[](auto &a, auto &b){ return a[1]-a[0] > b[1]-b[0] ;});
+        
+        int e = 0, s=0;
+        for (auto &t : tasks){
+            if (s >= t[1])
+                s -= t[1];
+            else
+                e += t[1]-s, s= 0;
+            s += t[1] - t[0];
         }
-        return ans;
+        return e;
+        
     }
 };
