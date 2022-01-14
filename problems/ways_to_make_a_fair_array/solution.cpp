430c41d9e52bee8db5139369c0268272be83ee1b
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 11:07:54 +0000

    Sync LeetCode submission - Ways to Make a Fair Array (cpp)

--- problems/ways_to_make_a_fair_array/solution.cpp
+++ problems/ways_to_make_a_fair_array/solution.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int waysToMakeFair(vector<int>& nums) {
-        
-        int n = nums.size();
-        if (n==1) return  1;
-        if (n==2) return (nums[0] ==0 + nums[1]==0);
-        vector<int> prefix(n), suffix(n);
-        prefix[0]=nums[0];
-        prefix[1]= n > 1 ? nums[1]:0;
-        for (int i=2; i<n; i++)
-            prefix[i] = prefix[i-2] + nums[i];        
-        for (int i=n-1; i>=0; i--){
-            suffix[i]=nums[i];
-            if (i<n-2)
-                suffix[i]+=suffix[i+2];
+        int n = nums.size(), ans=  0;
+        vector<int> odd(n+2), even(n+2);   
+        for (int i=0, o=0,e=0; i<n;i++){
+            odd[i+1] = odd[i];
+            even[i+1] = even[i];            
+            if (i%2)
+                odd[i+1] += nums[i];
+            else
+                even[i+1]  += nums[i];
+
         }
         
-        int ans = suffix[1] == suffix[2];
-        
-        for (int i=1; i<n-1; i++){
-            ans += prefix[i-1]+ suffix[i] == prefix[i]+suffix[i+1];
-                
+        for (int i=0; i<n;i++){
+                ans += (odd[i] + even[n]-even[i+1] == even[i]+  odd[n]-odd[i+1]);
         }
-            
-        ans += prefix[n-2] == prefix[n-3];
         return ans;
     }
 };
-//   1,1,2
