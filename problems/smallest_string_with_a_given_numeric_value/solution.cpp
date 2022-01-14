488a370dbefb253613159dfc4f6dba22287cabde
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 10:48:07 +0000

    Sync LeetCode submission - Smallest String With A Given Numeric Value (cpp)

--- problems/smallest_string_with_a_given_numeric_value/solution.cpp
+++ problems/smallest_string_with_a_given_numeric_value/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
+        int m = 26 * (n-1);
         string s;
-        for( int i=1; i<=n; i++){
-            int rem = k - (n-i) * 26;
-            if (rem <= 0)
-                rem=1;
-            
-                s+=rem+'a'-1;
-            k-=rem;
+        while (n--){
+            int d = k - m;
+            if (d <= 0)
+                 s+='a', k -= 1;
+            else 
+                s+='a' + d - 1, k -= d;
+            m -= 26;
         }
         return s;
     }
