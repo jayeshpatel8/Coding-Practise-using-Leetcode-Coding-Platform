38333855250157b5fefd3904a99fdcd843cff064
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 15:36:35 +0000

    Sync LeetCode submission - Get Maximum in Generated Array (cpp)

--- problems/get_maximum_in_generated_array/solution.cpp
+++ problems/get_maximum_in_generated_array/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        if (n<=0) return 0;
-        if (n<=1) return 1;
-        vector<int> A(n+1);
-        A[0]=0;
-        A[1]=A[2]=1;
-        int m=1;
-        for (int i=3; i<=n; i++){
-            if (i%2)
-                A[i] = A[i/2] + A[i-i/2]; 
-            else
-                A[i] = A[i/2];
-            m = max(m,A[i]);
+        if (n<=2) return n!= 0;
+        int arr[101]={} , ans = 1;
+        arr[1]=1;
+        for (int j=2, f= 1; j<=n;f++ ){
+            arr[j++] = arr[f];
+            if (j<=n)
+                arr[j++] = arr[f]+arr[f+1];
+            ans = max({ans,arr[j-2],arr[j-1]});
         }
-        return m;
+        return ans;
     }
 };
