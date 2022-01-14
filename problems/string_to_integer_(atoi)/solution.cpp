6b4cbc4a20881649abe2e2d3e07b2c146edb75dd
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-14 13:26:08 +0000

    Sync LeetCode submission - String to Integer (atoi) (cpp)

--- problems/string_to_integer_(atoi)/solution.cpp
+++ problems/string_to_integer_(atoi)/solution.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     int myAtoi(string s) {
-     int i=0,len=s.size();
-     
-     while(i < len && (s[i]==' '))
-     {
-         i++;
-     }
-     int sign=1;
-    if (i < len )
-    {
-      if(s[i]=='-'){
-          sign=-1;i++;}
-      else if (s[i]=='+')
-          i++;
-    }
-        int sum=0;
-            if ((i < len && isdigit(s[i]))&& sign ==-1)
-            {
-                sum = sum * 10 + s[i]-'0';
-                sum=~sum + 1 ;
-                i++;
-            }
-        while (i < len && isdigit(s[i]))
-        {
-            if (sign ==-1)
-            {
-              if (sum < INT_MIN/10 || (sum == INT_MIN/10 && (s[i]-'0')>8))
-                  return INT_MIN;
-
-              sum = sum * 10 - (s[i]-'0');  
-            }
-            else
-            {
-              if (sum > INT_MAX/10 || (sum == INT_MAX/10 && (s[i]-'0')>7))
-                  return INT_MAX;
-              sum = sum * 10 + (s[i]-'0');                  
-            }
-            i++;
+        int i=0, n  =s.size(), neg=0;
+        
+        while(i<n && s[i] == ' ') i++;
+        
+        if (s[i]=='-' || s[i]=='+')
+            neg = s[i++]=='-';
+        long num=0;
+        while(i<n && isdigit(s[i])){
+            num = num* 10 + (s[i++]-'0');
+            if (num > INT_MAX) break;            
         }
-        return (sum);
+        return neg ? max<long>(INT_MIN, num*-1 ) : min<long>(INT_MAX,num); 
     }
 };
