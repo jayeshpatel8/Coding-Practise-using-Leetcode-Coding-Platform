ab0316e10db5837464610f26bfb5f9a8bd8240ea
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-15 11:40:13 +0000

    Sync LeetCode submission - Jump Game IV (cpp)

--- problems/jump_game_iv/solution.cpp
+++ problems/jump_game_iv/solution.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> m;
-    
     int minJumps(vector<int>& arr) {
-        const int N = arr.size();
-        if (N<=1) return 0;
-        for (int i=0; i<N; i++ ){
-            m[arr[i]].push_back(i);
+      int n = arr.size();
+        vector<bool> vis(n);
+        unordered_map<int,vector<int>> map;
+        for (int i=0; i<n; i++) {
+            if (i==0 || i==n-1|| !(arr[i-1] ==arr[i] && arr[i] ==arr[i+1]) )
+            map[arr[i]].push_back(i);
         }
-        
-        
-        // BFS
         queue<int> q;
-        vector<bool> vis(N,false);
-        q.push(0);
-        int step=-1;
-        vis[0]=true;
+        q.push(0); vis[0]=1;
+        int step=0;
         while(!q.empty()){
-            int qs = q.size();
-            step++;            
-            
-            while(qs--){
-                int i = q.front();q.pop();
-                if (i + 1 == N) return step;
-                if (i-1>=0 && !vis[i-1]) q.push(i-1),vis[i-1]=true;;
-                if (i+1<N && !vis[i+1]) q.push(i+1),vis[i+1]=true;;
-                for (auto j : m[arr[i]]) 
-                    if (!vis[j])
-                        q.push(j),vis[j]=true;;
-                m[arr[i]].clear();
+            int sz = q.size();
+            while(sz-- > 0){
+                int u = q.front(); q.pop();
+                if (u == n-1) return step;
+                if (u>0 && !vis[u-1] ) q.push(u-1),vis[u-1]=1;
+                if (!vis[u+1] ) q.push(u+1),vis[u+1]=1;
+                for (auto v : map[arr[u]]){
+                    if (u!= v && !vis[v])
+                        q.push(v),vis[v]=1;
+                }
+                map[arr[u]].clear();
             }
+            step++;
         }
-        
-        return step;
-    
+        return 0;
     }
 };
