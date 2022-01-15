c81c81c68cc32597bed2bf227af30a752f2cb4da
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-15 22:27:23 +0000

    Sync LeetCode submission - Furthest Building You Can Reach (cpp)

--- problems/furthest_building_you_can_reach/solution.cpp
+++ problems/furthest_building_you_can_reach/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        
-        for (int i=1; i<heights.size(); i++){
-            int diff = heights[i] - heights[i-1];
-            if (diff <= 0) continue;
-            pq.push(diff);
-            while(pq.size() > ladders){
-                bricks -= pq.top();
-                pq.pop();                
+    int furthestBuilding(vector<int>& h, int bricks, int ladders) {
+        int i=1;
+        priority_queue<int> pq;
+        for (i=1; i<h.size() ; i++)   {
+            int diff = h[i] - h[i-1];
+            if (diff <=0) continue;
+            pq.push(-diff);
+            if (pq.size() > ladders){
+                auto b = -pq.top(); pq.pop();
+                if ( b > bricks) return i-1;
+                bricks -=b;
             }
-            if (bricks < 0) return i - 1;
         }
-        return heights.size()-1;
+        return i-1;
     }
 };
