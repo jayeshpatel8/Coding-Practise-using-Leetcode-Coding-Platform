66d113b4d8a04a25f3942dff0bfa8a80cb5340cb
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-17 13:04:09 +0000

    Sync LeetCode submission - Number of Ways to Form a Target String Given a Dictionary (cpp)

--- problems/number_of_ways_to_form_a_target_string_given_a_dictionary/solution.cpp
+++ problems/number_of_ways_to_form_a_target_string_given_a_dictionary/solution.cpp
@@ -1,52 +1,34 @@
 class Solution {
 public:
-    map<int,int> M[1001];
-    int N, T;
-    int dp[1001][1001]={} , mod = 1e9 +7;
-    int numWays(vector<string>& words, string target) {
-        N  = words[0].size();
-        T = target.size();
-        /*
-        for (int i=0; i<words.size(); i++){
-            for (int j=0; j<N; j++){
-                M[j][words[i][j]-'a']++;
-            }
-        }*/
-        int dp1[1001]={};
-        dp1[0]=1;
-        for ( int i=1; i<=N; i++){
-            int freq[26]={};
-            for (int k=0; k<words.size(); k++) freq[words[k][i-1]-'a']++;
-            for ( int j=T; j>=1; j--){
-                
-                int cnt = freq[target[j-1]-'a'];
-                if (cnt)
-                    dp1[j] = (dp1[j] +dp1[j-1] * (long)cnt )%mod;
-            }
+    int numWays(vector<string>& words, string t) {
+        long freq[26][10002]={};
+        int tn =t.size(), mod = 1e9 +7;        
+        int ml=words[0].size();
+        if (tn >ml) return 0;
+        for (auto &w : words){
+            for (int i=0;i<ml; i++) freq[w[i]-'a'][i+1]++;
         }
-        return dp1[T];
-        /*
-        for ( int i=0; i<=words.size(); i++) dp[i][0]=1;
-        for ( int i=1; i<=N; i++){
-            for ( int j=1; j<=T; j++){
-                int freq = M[i-1][target[j-1]-'a'] , ans = dp[i-1][j];
-                if (freq)
-                    ans = (ans +dp[i-1][j-1] * (long)freq )%mod;
-                dp[i][j] = ans;
-            }
+        
+        for ( int i=0; i<26; i++){
+            for (int j=1; j<=ml; j++){
+                freq[i][j] +=freq[i][j-1];
+            }            
         }
-        return dp[N][T];
-*/
-        //memset(dp,-1, sizeof(dp));
-        //return dfs(target);
-    }
-    long dfs(string& target, int i=0, int j=0){
-        if (j >= T) return 1;
-        if (N-i < T-j) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        int ans =dfs(target, i+1,j), freq = M[i][target[j]-'a'];
-        if (freq)
-            ans = (ans  + freq  * dfs(target, i+1, j+1)) %mod;        
-        return dp[i][j] =  ans;
+        
+        vector<long> dp(ml+1);
+        for (int i=ml-1,c = t[tn-1]-'a'; i>=0; i--){
+            dp[i] = freq[c][ml] - freq[c][i] ;
+        }
+        
+        for (int i=tn-2; i>=0; i--){
+            vector<long> dp2(ml+1);
+            for (int j=ml-2, c = t[i+1]-'a'; j>=0; j--){
+                int c2 = t[i]-'a';
+                dp2[j] = (dp[j+1] * (freq[c2][j+1] - freq[c2][j])  + dp2[j+1] )%mod;
+            }
+            swap(dp,dp2);
+        }
+
+        return dp[0];
     }
 };
