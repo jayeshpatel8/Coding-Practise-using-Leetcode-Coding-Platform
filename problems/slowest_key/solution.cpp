dd3dcad8ec6c794c0cc9ae376e1d15141640d542
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-18 12:13:08 +0000

    Sync LeetCode submission - Slowest Key (cpp)

--- problems/slowest_key/solution.cpp
+++ problems/slowest_key/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
-        int time=releaseTimes[0], ans=keysPressed[0];
-        for (int i=1; i<keysPressed.size(); i++){
-            int diff = releaseTimes[i]-releaseTimes[i-1];
-            if(diff == time){
-                if (ans < keysPressed[i])
-                    ans = keysPressed[i];
-            }
-            else if ((diff > time)){
-                time = diff, ans = keysPressed[i];
-            }
+    char slowestKey(vector<int>& rt, string kp) {
+        int t[26]={0};
+        t[kp[0]-'a']=rt[0];
+        for (int i=1;i<rt.size(); i++){
+            t[kp[i]-'a']=max(t[kp[i]-'a'], rt[i]-rt[i-1]);
         }
-        return ans;
+        int m =0, a;
+        for ( int i=0; i<26; i++){            
+            if (m <= t[i] ) m = t[i], a = i;
+        }
+
+        return 'a'+a;
     }
 };
