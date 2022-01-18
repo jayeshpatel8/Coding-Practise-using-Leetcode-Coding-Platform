668de3c29a80d460a94bd11fbf926ed6e1aca041
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-18 21:27:25 +0000

    Sync LeetCode submission - Rank Transform of a Matrix (cpp)

--- problems/rank_transform_of_a_matrix/solution.cpp
+++ problems/rank_transform_of_a_matrix/solution.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> matrixRankTransform(vector<vector<int>>& matrix) {
-        map<int,vector<pair<int,int>>> map;
         int M = matrix.size(), N = matrix[0].size();
-        for (int i  = 0; i < M; i++){
-            for (int j  = 0; j < N; j++){
-                map[matrix[i][j]].push_back({i,j});
+        vector<int> dp(M + N);
+        map<int, vector<array<int,2>>> p;
+        for (int i=0; i<M; i++){
+            for (int j=0;j<N; j++){
+                p[matrix[i][j]].push_back( {i,j});
             }
         }
-        vector<int> rank(M+N) ;
-        
-        for  (auto &[v, val] : map){
-            vector<int> uf(M+N);
-            iota(begin(uf), end(uf), 0 );
-            for (auto idx : val){
-                // connect all
-                int r = idx.first, c = idx.second;
-                int r1 = find(uf,r), c1= find(uf,M + c);
-                uf[r1] = c1;
-                rank[c1] = max(rank[r1],rank[c1]);
+        vector<int> rank(M+N);
+        for (auto & [k,v] : p){
+            vector<int> uf(M+N,-1);
+            for(auto &e:v){
+                int u1 = find(uf,e[0]), v1 = find(uf,M+e[1]);
+                if (u1!= v1)
+                {
+                    uf[u1] = v1;
+                    rank[v1] = max(rank[v1],rank[u1]);
+                }
             }
-            auto rank2 = rank;
-            for (auto idx : val){
-                int r = idx.first, c = idx.second;
-                int r1 = find(uf,r);
-                rank2[r] = rank2[M + c] = matrix[r][c] =rank[r1]+1;           
-            }
-            swap(rank,rank2);
+            auto rank2=rank;
+            for(auto &e:v){
+                int u1 = find(uf,e[0]);
+                    rank2[e[0]]=rank2[M+e[1]] = matrix[e[0]][e[1]] = 1 + rank[u1];
+            }            
+            swap(rank2, rank);           
         }
         return matrix;
     }
-    int find(vector<int> & root, int n){
-        if (root[n] != n )
-            root[n] = find(root, root[n]);
-        return root[n];
+    int find( vector<int>& uf, int i){
+        return uf[i]==-1? i : uf[i] = find (uf, uf[i]);
     }
 };
