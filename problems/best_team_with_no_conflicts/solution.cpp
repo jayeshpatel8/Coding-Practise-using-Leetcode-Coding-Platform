d361c3e44f4f8a67fad170afe465edf910ba069f
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-19 12:53:11 +0000

    Sync LeetCode submission - Best Team With No Conflicts (cpp)

--- problems/best_team_with_no_conflicts/solution.cpp
+++ problems/best_team_with_no_conflicts/solution.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-     int dp[1000];
+    int dp[1001][1001], N;
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        int N = scores.size();
-        vector<pair<int,int>> P(N);
-         for ( int i =0; i<N; i++)
-             P[i] ={ages[i], scores[i]};
-         sort(begin(P), end(P));
-        
-        int ans = dp[0] = P[0].second;
-        for( int i=1; i<N; i++){
-            dp[i]=P[i].second;
-            for ( int j=0; j<i;j++){
-                if (P[i].second >= P[j].second)
-                    dp[i] = max(dp[i], dp[j] + P[i].second);
-                ans = max(ans, dp[i]);
-            }
-        } 
+        N = scores.size();
+        int ans = 0;
+
+        vector<array<int,2>> s(N);
+        for ( int i=0; i<N; i++)
+            s[i]= {scores[i], ages[i]};
+        sort(rbegin(s),rend(s));
+        vector<int> dp1(N);
+        for (int i=0; i<N; i++){
+            dp1[i] = max(dp1[i], s[i][0]);
+            int age = s[i][1];
+            for (int j=i+1; j<N; j++)
+                if (age >= s[j][1])
+                    dp1[j] = max(dp1[j] , dp1[i] + s[j][0]);
+            ans = max(ans , dp1[i]);
+        }
         return ans;
+        //memset(dp, -1, sizeof (dp));
+        //return dfs(s,0,1000);
     }
-};
+    int dfs (vector<array<int,2>> & s, int i, int age){
+        if (i >= N) return 0;
+        if (dp[i][age] != -1) return dp[i][age];
+        
+        int ans = dfs(s,i+1,age);
+        if (age >= s[i][1])
+            ans = max(ans, s[i][0] + dfs (s,i+1,min(age,s[i][1])));
+        
+        return dp[i][age] = ans;
+    }
+
+}; 
