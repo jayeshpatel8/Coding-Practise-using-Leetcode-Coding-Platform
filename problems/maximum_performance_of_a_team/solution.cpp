db055edc775cb9d1be852996284779838b1b0119
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-21 19:39:00 +0000

    Sync LeetCode submission - Maximum Performance of a Team (cpp)

--- problems/maximum_performance_of_a_team/solution.cpp
+++ problems/maximum_performance_of_a_team/solution.cpp
@@ -1,30 +1,26 @@
-auto speed=[]()
-{
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        vector<pair<int,int>> eff(n);
-        for (int i=0; i< n; i++)
-            eff[i] = {efficiency[i],speed[i]};
-        sort(rbegin(eff), rend(eff));
-        
-        size_t ans=0,sum=0;
-        const int mod = 1e9 + 7;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for (auto & [ef,speed] : eff){
-                sum+= speed;
-                pq.push(speed);            
-                if (pq.size() > k){
-                    sum -=pq.top(); pq.pop();
-                }
-
-                ans = max(ans, (sum * ef)); 
+    int maxPerformance(int n, vector<int>& sp, vector<int>& ef, int k) {
+        vector<array<int,2>> v(n);
+        priority_queue<int,vector<int>,greater<int>> p1;        
+        long sum = 0 ,ans=0;
+        int mod = 1e9 +7;
+        for (int i=0; i<n; i++){
+            v[i] = { ef[i] ,sp[i]};
+        }      
+        sort(rbegin(v),rend(v));
+        for ( int i=0; i<n; i++){                        
+            int e = v[i][0], s = v[i][1];
+            p1.push(s);
+            sum+=s;
+             if( p1.size()>k){
+                 sum -=p1.top();
+                p1.pop();
+            }
+            ans= max(ans , sum * e);
         }
-        return ans%mod;
+
+        
+        return ans % mod;
     }
 };
