325234492706b88d0fa631c62e34583d2daa17a5
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-21 10:54:26 +0000

    Sync LeetCode submission - Gas Station (cpp)

--- problems/gas_station/solution.cpp
+++ problems/gas_station/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-
-        int n = gas.size();
-        int spare=0,last_spare=INT_MAX, idx;    
-        for (int i=0; i<n ; i++)
-        {
-            spare += gas[i] - cost[i];
-            if (spare < last_spare){
-                last_spare = spare;
-                idx = i;
-            }
+        int c = 0, ans = 0, cur = 0;
+        for ( int i= 0; i< gas.size(); i++){
+            int c1 =  gas[i] - cost[i];
+            cur = max (c1, c1+ cur);
+            c += c1;
+            if (cur < 0 ) ans = i+1;
         }
-        return spare<0 ?-1: (idx+1)%n;
+        
+        if (c<0) return -1;
+        return ans ;
     }
 };
-/// -1 +1 +2
- 
