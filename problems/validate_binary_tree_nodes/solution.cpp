a3fb9a6f873b2dd66170395cbbcf6abc0b3d8072
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-27 22:36:08 +0000

    Sync LeetCode submission - Validate Binary Tree Nodes (cpp)

--- problems/validate_binary_tree_nodes/solution.cpp
+++ problems/validate_binary_tree_nodes/solution.cpp
@@ -1,64 +1,34 @@
 class Solution {
 public:
-    bool vis[10001]={};
-    bool validateBinaryTreeNodes(int n, vector<int>& lc, vector<int>& rc){
-        int root = -1;
-        vector<int> indeg(n);
-        for (int i=0; i<n; i++){
-            if (lc[i] != -1) indeg[lc[i]]++;
-            if (rc[i] != -1) indeg[rc[i]]++;
+    bool vis[10001]={0};
+    bool validateBinaryTreeNodes(int n, vector<int>& l, vector<int>& r) {
+        vector<int> v1(n+1);
+        for (int i =0; i<n; i++){    
+            if (l[i] !=  -1)
+                v1[l[i]]++;
+            if (r[i] !=  -1)
+                v1[r[i]]++;
+            
         }
-        for (int i=0; i<n; i++){
-            if (indeg[i]==0){
-                if (root == -1) root = i;
-                else  return false;
+        
+        if (*min_element(begin(v1),end(v1)) >=1 || *max_element(begin(v1),end(v1)) >=2) return false;
+        int m = -1;
+        for (int i =0; i<n; i++){            
+            if (v1[i]==0){
+                if (m !=-1) return false;
+                m = i;
             }
         }
-        if (root == -1) return false;
-        if (! dfs(root,lc, rc)) return false;
-        for (int i=0; i<n; i++)
-            if (vis[i]==0) return false;
+         dfs(l,r,m);
+        for (int i =0; i<n; i++){
+            if (vis[i] == 0) return false;
+        }
         return true;
     }
-    bool dfs(int root, vector<int> &lc , vector<int> & rc){
-        if (vis[root]) return false;
-        vis[root]=1;
-        bool ans = 1;
-        if (lc[root] != -1) ans &= dfs(lc[root],lc, rc);
-        if (ans && rc[root] != -1) ans &= dfs(rc[root],lc, rc);
-        return ans;
-    }
-    vector<int> parent;
-    bool validateBinaryTreeNodes2(int n, vector<int>& lc, vector<int>& rc) {
-        vector<int> uf(n, -1);
-        parent.resize(n);
-        
-        for (int i=0; i<n; i++){
-            int u  = i;
-            if (lc[u] != -1){
-               if (!check(uf,i,lc[u])) return false; 
-            }  
-            if (rc[u] != -1  ){
-                if(!check(uf,i,rc[u])) return false;
-            }
-        }
-        
-        return count_if(begin(parent),end(parent),[](int i){ return i==0;}) ==1  ;
-    }
-    int find(vector<int> &uf, int i){
-        return  uf[i] == -1 ? i : uf[i] = find (uf, uf[i]);
-        
-    }
-    bool check(vector<int> &uf, int i, int j){
-        int p1 =  find (uf,i) , p2 =  find (uf,j);
-        if (p2 ==  p1) return false;
-        uf[p1]=p2;
-        return parent[j]++ < 1;
-    }
-    void connect (vector<int> & uf, int i, int j){
-        int p1 = find (uf, i), p2 =  find (uf , j);
-        if (p1 != p2){
-            uf[p1] = p2;
-        }
+    bool dfs(vector<int>& l, vector<int>& r, int i ){
+        if (i == -1) return true;
+        if (vis[i]) return false;
+        vis[i]=1;
+        return (dfs(l,r,l[i])  && dfs(l,r,r[i]) );
     }
 };
