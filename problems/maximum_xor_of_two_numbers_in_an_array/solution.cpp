e403fb71d6a943265a3ea4cce77a9b36ea9c77cc
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-27 13:20:03 +0000

    Sync LeetCode submission - Maximum XOR of Two Numbers in an Array (cpp)

--- problems/maximum_xor_of_two_numbers_in_an_array/solution.cpp
+++ problems/maximum_xor_of_two_numbers_in_an_array/solution.cpp
@@ -1,48 +1,44 @@
-int Msb=31;
+    int Msb=31;
 int mXor;
-class Trie{
-        Trie* T[2];
-    public:
-        Trie(){T[0]=T[1]=0;}
-        void insert(int num){
-            Trie *t = this;
-            for (int b=Msb; b>=0; b--){
-                int bit = (num>>b) & 1;
-                if (t->T[bit]== NULL) t->T[bit] = new Trie();
-                t = t->T[bit];
-            }
-        }
-    int search(int num){
-        Trie * t = this;int val=0;
-        
-        for (int b=Msb; b>=0; b--){            
-            int bit = num >> b & 1;
-            
-            if (t->T[!bit]) bit = !bit;
-            else if(mXor > (val ^ num)) return 0;
-            t = t->T[bit];
-            if (bit) val |=1<<b;                   
-        }
-        return val ^ num;
-    }
-};
 class Solution {
 public:
-    int findMaximumXOR(vector<int>& nums) {
-        //[3,10,5,25,2,8]
-        //[2,3,5,8,10,25]
-        //if (nums.size()<2) return 0;
-        Trie root;
-        mXor=0;
-        int ored=0;
-        for (auto i : nums) ored|=i;
-        int b=31;
-        for (;b>1; b--) if (ored & 1<<b) break;
-        Msb=b;
-        for (auto i : nums) root.insert(i);
-        for (auto i : nums){
-            mXor = max(mXor,root.search(i));            
+    
+    class Trie{
+        public:
+        Trie *  bits[2]={NULL,NULL};
+    } ;
+    void insert(Trie * root, int n){
+        Trie *r = root;
+        for (int i=31; i>=0; i--){
+            int b = (n >> i) & 1;
+            if (r->bits[b] == NULL){
+                r->bits[b] = new Trie;                
+            }
+            r = r->bits[b];
         }
-        return mXor;
     }
+    int search(Trie *root, int n){
+        Trie *r = root;
+        int ans=0;
+      
+        for (int i=31; i>=0; i--){
+            int b = (n >> i) & 1;
+            if (r->bits[!b])
+                b= !b,ans  |= 1<<i;;
+            
+            r = r->bits[b];
+        }        
+        return ans;
+    }
+    
+    int findMaximumXOR(vector<int>& nums) {
+        Trie root;
+        int ans = 0;
+        for ( int i=1; i<nums.size(); i++){
+            insert(&root, nums[i-1]);
+            ans = max(ans, search(&root,nums[i]));            
+        }
+        return ans;
+    }
+
 };
