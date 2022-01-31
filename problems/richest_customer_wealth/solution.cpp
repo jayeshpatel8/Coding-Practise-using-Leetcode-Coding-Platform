2023acef2a1ab47c42c5532b9c72e1ada7789ec4
jayeshpatel8 <jnpatel81@gmail.com>
2022-01-31 10:46:47 +0000

    Sync LeetCode submission - Richest Customer Wealth (cpp)

--- problems/richest_customer_wealth/solution.cpp
+++ problems/richest_customer_wealth/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-     int ans = 0;
-        for (auto &i : accounts){
-            ans= max(ans, accumulate(begin(i),end(i),0));
+        int ans = 0;
+        for (auto &i  : accounts){
+            ans = max(ans, accumulate(begin(i),end(i),0));
         }
         return ans;
     }
