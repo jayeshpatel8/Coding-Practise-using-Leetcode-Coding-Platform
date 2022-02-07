d74956b97c73c4af5de7c0d3e31bfa2db15d33be
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-07 10:17:32 +0000

    Sync LeetCode submission - Find the Difference (cpp)

---
+++ problems/find_the_difference/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans = 0;
+        for (int i=0; i<s.size(); i++)
+            ans += t[i]-s[i];
+        return ans + t[t.size()-1];
+    }
+};
