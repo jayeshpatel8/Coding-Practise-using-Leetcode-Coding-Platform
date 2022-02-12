f4964846d81976cb00019c8a8205b7cf4dcce730
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-12 12:01:39 +0000

    Sync LeetCode submission - Jump Game IV (cpp)

--- problems/jump_game_iv/solution.cpp
+++ problems/jump_game_iv/solution.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
-      int n = arr.size();
-        vector<bool> vis(n);
-        unordered_map<int,vector<int>> map;
-        for (int i=0; i<n; i++) {
-            if (i==0 || i==n-1|| !(arr[i-1] ==arr[i] && arr[i] ==arr[i+1]) )
+        int  N = arr.size();
+        if (N<2) return 0;
+        if (arr[0]==arr[N-1]) return 1;
+        //BFS
+        queue<int>q; q.push(0);
+        vector<bool> vis(N);
+        unordered_map<int,vector<int>>map;
+        for ( int i=0; i<N; i++)
             map[arr[i]].push_back(i);
-        }
-        queue<int> q;
-        q.push(0); vis[0]=1;
-        int step=0;
+        int end=N-1,best=N+1,ans=0;
+        vis[0]=1;
         while(!q.empty()){
             int sz = q.size();
-            while(sz-- > 0){
-                int u = q.front(); q.pop();
-                if (u == n-1) return step;
-                if (u>0 && !vis[u-1] ) q.push(u-1),vis[u-1]=1;
-                if (!vis[u+1] ) q.push(u+1),vis[u+1]=1;
-                for (auto v : map[arr[u]]){
-                    if (u!= v && !vis[v])
-                        q.push(v),vis[v]=1;
+            while(sz-->0)
+            {
+                auto i = q.front(); q.pop();
+
+                if (map.count(arr[i])){
+                    for (auto j : map[arr[i]]){
+                        if (vis[j]) continue;
+                        vis[j]=1;
+                        q.push(j);
+                    }
+                    map.erase(arr[i]);
                 }
-                map[arr[u]].clear();
+                if (i+1<N && vis[i+1]==0){
+                    vis[i+1]=1; q.push(i+1);
+                }
+                if (i-1>=0 && vis[i-1]==0){
+                    vis[i-1]=1; q.push(i-1);
+                }                
+                if (vis[end]) return ans+1;
             }
-            step++;
+            ans++;
         }
-        return 0;
+        return N;
     }
 };
