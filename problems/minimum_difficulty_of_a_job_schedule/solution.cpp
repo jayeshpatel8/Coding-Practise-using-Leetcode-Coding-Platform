b2758daec6536672507763e487d5c2ee4161c93c
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-14 16:27:02 +0000

    Sync LeetCode submission - Minimum Difficulty of a Job Schedule (cpp)

--- problems/minimum_difficulty_of_a_job_schedule/solution.cpp
+++ problems/minimum_difficulty_of_a_job_schedule/solution.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    int N;
-    int dp[11][301];
-    int minDifficulty(vector<int>& job, int d) {
-        N = job.size();
-        if(N < d) return -1;
-        dp[0][0]=job[0];
-        
-        for(int j = 1; j < N; ++j){
-            dp[0][j] = max(job[j], dp[0][j - 1]);
-        }
-        for (int i=1; i<d; i++){
-            for(int j=i; j<N; j++){
-                int c_max =0;
-                dp[i][j]=1e6;
-                for (int k=j; k>=i; k--){
-                    c_max = max(c_max, job[k]);
-                    dp[i][j] = min (dp[i][j], c_max + dp[i-1][k-1]);                    
-                }
-            }
-        }
-        return dp[d-1][N-1];
-        //memset(dp,-1,sizeof(dp));
-        //int ans = dfs(job, d,N-1);
-       // return ans == 1e6 ? -1 : ans;
+    int dp[1001][11], N;
+    int minDifficulty(vector<int>& jd, int d) {
+        memset(dp,-1,sizeof(dp));
+        N = jd.size();
+        dfs(jd,0,d);
+        return dp[0][d] >=100000 ? -1 :  dp[0][d];
     }
-    /*
-    int dfs(vector<int>& job, int d, int i){
-        if (i== -1 && d == 0) return 0;
-        if ( d == 0 ||i ==-1) return 1e6;
-        
-        if (dp[d][i] != -1) return dp[d][i];
-        int ans = 1e6;
-        int c_max = job[i];
-        for(int j  = i; j>=0; j--){
-            c_max = max(c_max, job[j]);
-            ans = min (ans, c_max + dfs(job,d-1,j-1));
+    int dfs(vector<int>& jd, int i, int d){
+        if (i>=N || d==0){
+            if (d==0 && i==N) return 0;
+            return 100000;
         }
-        return dp[d][i] = ans;
-    }*/
+        if (dp[i][d] != -1) return dp[i][d];
+        int ans = INT_MAX, ma= 0;;
+        for ( int j=i; j<N-d+1; j++){
+            ma= max(ma, jd[j]);
+            ans= min(ans, ma+dfs(jd,j+1,d-1));
+        }
+        return dp[i][d]  =ans;
+    }
 };
