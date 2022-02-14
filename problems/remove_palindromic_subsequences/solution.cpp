99abb54e6529844c3910f4d6695707c88a9fc4ee
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-14 15:39:15 +0000

    Sync LeetCode submission - Remove Palindromic Subsequences (cpp)

--- problems/remove_palindromic_subsequences/solution.cpp
+++ problems/remove_palindromic_subsequences/solution.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-     int sz = s.length();
-        if (!sz  ) return 0;
-        for(int i=0,j=sz-1; i<j; i++) if(s[i]!=s[j--]) return 2;
-        return 1;
+    
+        
+        return 1 + (isPal(s)==false);
+    }
+    bool isPal(string s){
+        for (int i=0, j=s.size()-1; i<j; i++,j--){
+            if (s[i] != s[j]) return false;
+        }
+        return true;
     }
 };
