ba037a57fc3c578ebcfdeb28af202d3e17def9e5
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-15 11:17:22 +0000

    Sync LeetCode submission - Break a Palindrome (cpp)

--- problems/break_a_palindrome/solution.cpp
+++ problems/break_a_palindrome/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string breakPalindrome(string pal) {
-        int i=0, N = pal.size()/2;
-        if (N==0) return "";
-        for (; i< N; i++)
-            if ('a' != pal[i]){ 
-                pal[i] = 'a';
-                return pal;
-            }
-        pal[pal.size()-1]='b';
+        int N = pal.size();
+        if (N==1) return {};
+        //int m = N&1? N/2 : -1;
+        for (int i=0; i<N/2; i++){            
+            if (pal[i]=='a') continue;
+            pal[i]='a';
+            return pal;
+        }
+        pal[N-1]='b';
         return pal;
     }
 };
