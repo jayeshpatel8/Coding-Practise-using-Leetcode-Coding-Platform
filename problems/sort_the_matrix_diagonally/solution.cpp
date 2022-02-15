2a604e89d77c970efe3e796115a87263cfd15357
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-15 11:29:04 +0000

    Sync LeetCode submission - Sort the Matrix Diagonally (cpp)

--- problems/sort_the_matrix_diagonally/solution.cpp
+++ problems/sort_the_matrix_diagonally/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int N=mat.size() , C= mat[0].size() ;
-        unordered_map<int, deque<int>>M(C+N);
-        
-        for (int i=0; i<N ; i++){
-            for (int j=0; j  <C; j++){
-                int d =  C+i-   j  ;
-                {
-                    M[d].push_back(mat[i][j]);                                     
-                }
+        int M = mat.size(), N = mat[0].size();
+        for (int i=0; i<M; i++){
+            vector<int> a;
+            for (int k=i, j=0; k<M && j<N; k++,j++){
+                a.push_back(mat[k][j]);
+            }
+            sort(begin(a),end(a));
+            for (int k=i, j=0,c=0; k<M && j<N; k++,j++){
+                mat[k][j] = a[c++];
             }
         }
-        for (int i=1; i<C+N; i++ ) sort(M[i].begin(), M[i].end());
-        for (int i=0; i<N ; i++){
-            for (int j=0; j  <C; j++){
-                int d =  C+i-   j  ;
-                mat[i][j] = M[d].front(); M[d].pop_front();
+        for (int j=0; j<N; j++){
+            vector<int> a;
+            for (int k=j, i=0; i<M && k<N; k++,i++){
+                a.push_back(mat[i][k]);
             }
-        }
+            sort(begin(a),end(a));
+            for (int k=j, i=0,c=0; i<M && k<N; k++,i++){
+                mat[i][k] = a[c++];
+            }
+        }        
         return mat;
     }
 };
