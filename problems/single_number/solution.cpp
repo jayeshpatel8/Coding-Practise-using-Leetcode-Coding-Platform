844bfe22d185ddabae8a7ce36baf7b0b7912e932
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-15 10:38:23 +0000

    Sync LeetCode submission - Single Number (cpp)

--- problems/single_number/solution.cpp
+++ problems/single_number/solution.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.size() < 3) return nums[0];
-        int xoring=nums[0];
-        for (int i =1;i< nums.size();i++)
-            xoring ^=nums[i];
-        return xoring;
+        int ans = 0;
+        for (auto &i : nums) ans ^= i;
+        return ans;
     }
 };
