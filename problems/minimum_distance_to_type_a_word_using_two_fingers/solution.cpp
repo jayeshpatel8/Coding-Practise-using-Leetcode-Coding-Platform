75b43ec3a0dc0d8a086563ca2cc20ed087a4cb29
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-17 16:05:31 +0000

    Sync LeetCode submission - Minimum Distance to Type a Word Using Two Fingers (cpp)

--- problems/minimum_distance_to_type_a_word_using_two_fingers/solution.cpp
+++ problems/minimum_distance_to_type_a_word_using_two_fingers/solution.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int dp[301][27][27]={};
-    int minimumDistance(string word) { 
-        return dfs(word,0,26,26);
+    int dp[30][30][301];
+    array<int,2> dist[128];
+    int minimumDistance(string word) {
+        memset(dp,-1,sizeof(dp));
+        int ans = INT_MAX;
+        for(  int i=0, l='A'; i<5; i++){
+            for (int j = 0; j<6; j++,l++){
+                dist[l][0]=i, dist[l][1]=j;
+            }
+        }
+        for ( int i='A'; i<'Z'; i++){
+            for (int j=i+1; j<='Z'; j++){
+                ans = min (ans, dfs(word,0,i,j));
+            }
+        }
+        return ans;
     }
-    int dfs(string & W, int i, int ch1, int ch2){
-        if (i >= W.size()) return 0;
+    int dfs(string& word,int i, int k1, int k2){
+        if (i >= word.size()) return 0;
+        if (dp[k1-'A'][k2-'A'][i] != -1) return dp[k1-'A'][k2-'A'][i];
+        int ans ;
+        // move k1        
+        ans =  abs(dist[k1][0] - dist[word[i]][0]) + abs(dist[k1][1] - dist[word[i]][1]) + dfs(word,i+1,word[i],k2);
+        ans =min (ans,abs(dist[k2][0] - dist[word[i]][0]) + abs(dist[k2][1] - dist[word[i]][1]) + dfs(word,i+1,k1,word[i]) );
         
-        if (dp[i][ch1][ch2]) return dp[i][ch1][ch2];
-        int ans, ch = W[i]-'A';
-        if (ch == ch1)
-            ans = dfs(W,i+1,ch,ch2 );
-        else if (ch == ch2)
-            ans = dfs(W,i+1,ch1,ch );
-        else
-            ans = min( cost(ch1,ch) + dfs(W,i+1,ch,ch2 ), cost(ch2,ch) + dfs(W,i+1,ch1,ch ));
-        return dp[i][ch1][ch2] = ans;
-    }
-    int cost(char ch1, char ch){
-        if(ch1 >= 26) return 0;
-        
-        int r1 = ch1/6,c1=ch1%6, r=ch/6,c=ch%6;
-        return abs(r1-r)+abs(c1-c);
+        return dp[k1-'A'][k2-'A'][i] = ans;
     }
 };
