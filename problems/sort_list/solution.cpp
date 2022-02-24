3e767eb66327d8a443c7db8b00ea376fd11f6654
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-24 12:45:54 +0000

    Sync LeetCode submission - Sort List (cpp)

--- problems/sort_list/solution.cpp
+++ problems/sort_list/solution.cpp
@@ -11,50 +11,49 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-
-        
-        if (!head || !head->next) return head;
-
-        return mergesort(head);
+     int len = 0;
+        ListNode* h = head;
+        while(h)
+            h=h->next,len++;
+        return sort(head,len);
     }
-    ListNode * mergesort(ListNode * head){
-        if (!head || !head->next) return head;
+    ListNode * sort(ListNode * h, int l){
+        if (l<=2){
+            if (l<2) return h;
+            if (h->next && h->val > h->next->val)
+                swap(h->val , h->next->val);
+           
+            return h;
+        }
         
-        ListNode *slow=head, *fast=head;
-        while(fast && fast->next){
-            fast=fast->next;
-            if (fast->next){
-                fast=fast->next;
-                slow=slow->next;
-            }
+        int m = l/2;
+        auto h1=h;
+        
+        int cnt=m-1;
+        while (cnt--)
+            h=h->next;
+        auto t = h->next;
+        h->next=NULL;
+        auto s1 = sort(h1,m);
+        auto s2 = sort(t,l-m);
+        //merge s1 , s2
+        return merge(s1,s2);
+    }
+    ListNode * merge(ListNode * s1, ListNode * s2){
+        ListNode dummy, * s = &dummy;
+        
+        while (s1 && s2){
+            if (s1->val > s2->val)
+                s->next = s2, s2=s2->next;
             else
-                break;
+                s->next = s1, s1=s1->next;            
+            s=s->next;
         }
-        //1,2,3,4
-        ListNode * a =  slow->next;
-        slow->next=NULL;
-        
-        ListNode *b = head;
-        
-        return merge( mergesort(a), mergesort(b));
-    }
-    ListNode * merge(ListNode * a, ListNode* b){
-        ListNode * p,dummy;
-        p=&dummy;
-        p->next=NULL;
-        while (a && b){            
-            if (a->val < b->val){
-                p->next = a;
-                a = a->next;                
-            }
-            else{
-                p->next = b;
-                b = b->next;
-            }
-            p=p->next;
-        }
-        if (!a) p->next = b;
-        if (!b) p->next = a;
+        while(s1)
+            s->next = s1, s1=s1->next,s=s->next;
+        while(s2)
+            s->next = s2, s2=s2->next,s=s->next;
+        s->next=NULL;
         return dummy.next;
     }
 };
