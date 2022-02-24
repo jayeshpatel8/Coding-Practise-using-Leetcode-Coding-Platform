6e6ac2b2bffbda73e2fda72460fc6d8e2664e81c
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-24 23:21:35 +0000

    Sync LeetCode submission - Minimum Insertion Steps to Make a String Palindrome (cpp)

--- problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.cpp
+++ problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     int dp[501][501];
     int minInsertions(string s) {
-        for (int len=1; len<s.size();len++){
-            for (int i=0,j=len; i<s.size()-len; i++,j++){
-                
-                if (s[i] == s[j] ){
-                    if (i+1<j )dp[i][j] = ( dp[i+1][j-1]);
-                }
-                else dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
-            }
-        }
-        return dp[0][s.size()-1];
+        memset(dp,-1,sizeof(dp));
+        return dfs(s,0,s.size()-1);
     }
-    /*
-    int mi(string &s, int i, int j){
+    int dfs(string &s, int i, int j){
         if (i>=j) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        
-        dp[i][j] = 1 + min(mi(s,i,j-1), mi(s,i+1,j));
-        if (s[i] == s[j]) dp[i][j] = min(dp[i][j], mi(s,i+1,j-1));
-        return dp[i][j];
+        if (dp[i][j] != -1) return dp[i][j] ;
+        int ans ;
+        if (s[i]==s[j])
+            ans = dfs(s,i+1,j-1);
+        else{
+            ans= 1 + min (dfs(s,i+1,j),dfs(s,i,j-1));
+        }
+        return dp[i][j]  = ans;
     }
-    */
 };
