627b713a7206fdd222a481ebd80244ac2ce58dcb
jayeshpatel8 <jnpatel81@gmail.com>
2022-02-25 13:07:16 +0000

    Sync LeetCode submission - Compare Version Numbers (cpp)

--- problems/compare_version_numbers/solution.cpp
+++ problems/compare_version_numbers/solution.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int compareVersion(string v1, string v2) {
-     // extract the rev, 
-        // remove leading zero
-        int l1 = v1.length(), l2=v2.length();
-        int i=0,j=0;
-        int result = -1;
-        while (i<l1 || j <l2)
-        {
-            int a =  getrev(v1,i);
-            int b =  getrev(v2,j);
+        
+        for (int i=0, j=0, N1 = v1.size(), N2 = v2.size(); i<N1 || j<N2;){
+            int a=0,b=0;
+            while (i<N1 && v1[i] == '0'){
+                i++; continue;
+            }
+            while (i<N1 && v1[i] != '.')
+                a = a*10 + v1[i++]-'0';
             
-            if (a==b) result = 0;
-            else
-                if (a==-1 && b==0) continue;
-                else if (b==-1 && a==0) continue;
-                else if (a < b) return -1;
-                else return 1;
+            
+            while (j<N2 && v2[j] == '0'){
+                j++; continue;
+            }
+            while (j<N2 && v2[j] != '.')
+                b = b*10 + v2[j++]-'0';
+            
+                        
+            if (a != b){
+                return a < b? -1 : 1;
+            }
+            j++;i++;
         }
         return 0;
-
-    }
-    int getrev(string v1, int & r){
-        if (r >= v1.size() ) return -1;
-        int num=0;
-        while (r < v1.size() ){
-            int c = v1[r++];
-            if ( c != '.')
-                num = c - '0' + (num * 10);
-            else
-                break;
-        }
-        return num;
     }
 };
