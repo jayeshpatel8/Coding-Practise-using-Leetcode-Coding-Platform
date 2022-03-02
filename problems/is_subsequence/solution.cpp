0673dc1cba6ab3ee63db2272f3f6e6e3de5d2988
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-02 11:28:43 +0000

    Sync LeetCode submission - Is Subsequence (cpp)

--- problems/is_subsequence/solution.cpp
+++ problems/is_subsequence/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int M = s.size(), N = t.size();
-        vector<vector<int>> dp(M+1, vector<int>(N+1));
-        
-        for ( int i = M-1; i>=0; i--)
-        {
-            for(int j = N-1; j>=0;j--){
-                if (s[i] == t[j])
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                else 
-                    dp[i][j] = dp[i][j+1]; //max(dp[i+1][j], dp[i][j+1]);
-            }
+        int i=0, j=0;
+        while (i <s.size() && j <t.size()){
+            if (s[i] == t[j])
+                i++;
+            j++;
         }
-        return dp[0][0]==M;
+        return i == s.size();
     }
 };
