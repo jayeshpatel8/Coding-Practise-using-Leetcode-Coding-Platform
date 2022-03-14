3328df15a3abd19ca70a90ac05e91e6958386361
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-14 17:08:44 +0000

    Sync LeetCode submission - Simplify Path (cpp)

--- problems/simplify_path/solution.cpp
+++ problems/simplify_path/solution.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> s;
+        string tmp,ans;
+        vector<string> st;
+        stringstream ss(path);
         
-        int n = path.size();
-        for (int i=0; i<n; ){
-            if ((i+1 <n && path[i+1]=='/') || i+1 ==n ){i++; continue;}
-            auto j = path.find('/',i+1);
-            string t;
-            if (j == string::npos)  j = n;
-            t = path.substr(i,j-i);
-            
-            if (t=="/.")i+=2;
-            else if (t=="/.."){
-                if(!s.empty())s.pop();
-                i+=3;
+        while (getline(ss, tmp,'/')){
+            if (tmp.empty() || tmp == ".") continue;
+            if (tmp == ".."){
+                if (!st.empty()) st.pop_back();
             }
             else
-            {s.push(t);i =  j;}
+                st.push_back(tmp);
         }
-        if (s.empty())  return "/";
-        string res;
-        while (!s.empty()) {res =s.top()+res;s.pop();}
-        return res;
+        for (auto &s  : st) ans += '/' + s;
+        
+        return ans.empty()? "/" : ans;
     }
 };
