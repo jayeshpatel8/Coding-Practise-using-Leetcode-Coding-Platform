7eb9e3d956040a28fa0d04a3674208bca47dadce
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-17 18:11:09 +0000

    Sync LeetCode submission - Score of Parentheses (cpp)

--- problems/score_of_parentheses/solution.cpp
+++ problems/score_of_parentheses/solution.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int scoreOfParentheses(string S) {
-        int res=0;
-        int len=0;
-        for (int i=0; i< S.size(); i++){
-            if (S[i]=='(') len++;
-            else if (S[i-1]=='(')  res += 1 << --len;
-            else --len;
+    int scoreOfParentheses(string s) {
+        stack<int>st,st2;
+        int ans=0,len=0;
+        for (int i=0; i<s.size(); i++){
+            if(s[i] =='(')len++;
+            else if(s[i-1] =='(') ans += 1 << --len;
+            else len--;
         }
-        return res;
+        return ans ;
+    }
+    int score(string &s, int i, int j){
+        if (i>=j)return 0;
+        if (i+1 == j) return 1;
+        if (s[i+1]==')') return 1 + score(s,i+2,j);
+        return 2 * score(s,i+1,j-1);
     }
 };
