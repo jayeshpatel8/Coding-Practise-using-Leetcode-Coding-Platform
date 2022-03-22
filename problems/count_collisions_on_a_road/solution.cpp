b455dc64746e4695dba6dd155dbda7b96e8ecc9f
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-22 20:31:46 +0000

    Sync LeetCode submission - Count Collisions on a Road (cpp)

--- problems/count_collisions_on_a_road/solution.cpp
+++ problems/count_collisions_on_a_road/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int countCollisions(string d) {
-        int ans=0,prev=d[0],r=prev=='R';
+        int ans=0,r=d[0]=='R';
         for (int i=1; i<d.size(); i++){
             if (d[i]=='L'){
-                if (prev == 'R'){
-                    ans +=2 + r -1; prev='S';r=0;continue;
-                }
-                else if (prev == 'S'){
-                    ans +=1; prev='S';continue;
+                if (d[i-1] != 'L'){
+                    ans +=r + 1; d[i]='S';r=0;continue;
                 }
             }
             else if (d[i]=='S'){
-                if (prev == 'R'){
-                    ans +=r; prev='S';r=0;continue;
+                if (d[i-1] == 'R'){
+                    ans +=r; d[i]='S';r=0;continue;
                 }
             }
             else
                 r++;
-                prev = d[i];
         }
               
         return ans;
