b20ab59e6c2a2ce9969bb6f935f7067793dc0096
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-24 17:06:56 +0000

    Sync LeetCode submission - Boats to Save People (cpp)

--- problems/boats_to_save_people/solution.cpp
+++ problems/boats_to_save_people/solution.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
-        
-        int i=0, j=people.size()-1;
-        int res=0;
-        while (i<=j){
-            if (people[j] + people[i] <= limit) i++;                
-            j--;
-            res++;
+        sort(begin(people),end(people));
+        int ans = 0;
+        for (int i=0 , j = people.size()-1; i <= j; ){
+            if (people[i] + people[j--] <= limit) i++;
+            ans++;
         }
-        return res;
+        return ans;
     }
 };
-
