38ce5dbdcd0ecd6f9ebdf97a0cfc628876b31cbc
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-27 10:09:55 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (cpp)

--- problems/the_k_weakest_rows_in_a_matrix/solution.cpp
+++ problems/the_k_weakest_rows_in_a_matrix/solution.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int n = mat.size(), m = mat[0].size();
-        vector<pair<int,int>> pq;
-        for (int i=0; i<n; i++){
-            int s = m-(upper_bound(mat[i].rbegin(),mat[i].rend(),0)-mat[i].rbegin());
-            pq.push_back({s,i}); 
+        set<array<int,2>> st;
+        for (int j =0; j<mat.size(); j++){
+            int cnt = 0;
+            for (auto &i : mat[j]){
+                if (i) cnt++ ;
+                else break;
+            }
+            st.insert({cnt,j});
         }
-        sort(pq.begin(), pq.end(), [](const pair<int,int>&a, const pair<int,int>&b){return (a.first==b.first?(a.second<b.second):(a.first<b.first));});
-        vector<int> res;
-        auto it = pq.begin();
-        for (int i=0; i<k; i++) {res.push_back(it->second); it++;}
-        return res;
+        vector<int> ans;
+        for ( auto & s : st){
+            if (k-- > 0){
+                ans.push_back(s[1]);
+            }
+        }
+        return ans;
     }
-
 };
