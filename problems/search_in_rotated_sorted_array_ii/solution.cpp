aeb160f9d84061ed3685f06388c7f69a273b5d7f
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-28 11:26:00 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int t) {
-      int l = 0, r = nums.size()-1;
-      while(l<=r){
-          int m = (l+r)/2;
-          if (nums[m] == t) return true;
-          if (nums[l] < nums[m]){
-              
-              if (t >= nums[l] && t < nums[m])
-                  r = m-1;
-              else 
-                  l= m+1;
-          }
-          else if (nums[m] < nums[r]){
-              
-              if (t > nums[m] && t <= nums[r])
-                  l = m+1;
-              else 
-                  r= m-1;              
-          }
-          else if (nums[r] != t)
-              r--;
-          else
-              return true;
-      }
-      return false;
+    bool search(vector<int>& nums, int target) {
+
+         return bs(nums,target,0,nums.size()-1);
+    }
+    bool bs(vector<int>& nums, int target, int l, int r){
+        if (l>=r) return nums[l] == target;
+        
+        int m = (l + r) / 2;
+        if (nums[m]==target) return true;
+        else if (nums[l] == nums[r]){
+            if (nums[l] == target) return true;
+            else
+                return bs(nums,target,l+1,r-1);
+        }
+        
+        else if (nums[m]  <= nums[r]){
+            if (target > nums[m] && target <= nums[r]){
+                return bs(nums,target,m+1,r);
+            }
+            else
+                return bs(nums,target,l,m-1);
+        }
+        else{
+            if (target >= nums[l] && target < nums[m]){
+                return bs(nums,target,l,m-1);
+            }
+            else
+                return bs(nums,target,m+1,r);
+        }
+        return false;
     }
 };
