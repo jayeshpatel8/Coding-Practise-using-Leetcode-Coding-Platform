da29936b1a8d3037c5830847e7349ac0a447206f
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-31 11:00:38 +0000

    Sync LeetCode submission - Split Array Largest Sum (cpp)

--- problems/split_array_largest_sum/solution.cpp
+++ problems/split_array_largest_sum/solution.cpp
@@ -1,61 +1,28 @@
 class Solution {
 public:
     int N;
-    int dp[1001][51];
-    int splitArray(vector<int>& nums, int M) {
+    int splitArray(vector<int>& nums, int m) {
+        int l= *max_element(begin(nums), end(nums));
          N= nums.size();
-        if(N==M) return * max_element(begin(nums),end(nums));
-        
-        int l=0, r = accumulate(begin(nums), end(nums),0);
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if (partation(nums,M,mid))
-                r = mid - 1;
-            else
-                l = mid + 1;
+        if (m == N) return l;
+        int r = accumulate(begin(nums), end(nums),0);   
+        while (l<r){
+            int mid = l +(r-l)/2;
+            if (partition(nums,mid,m)  )
+                r=mid;
+            else 
+                l=mid+1;
         }
         return l;
-        
-        vector<int> psum(N+1);
-        for (int i=0; i<N; i++) psum[i+1] = psum[i] + nums[i];
-        
-        
-        
-        // memo
-        //memset(dp,-1,sizeof(dp));
-        //return lsum(nums, 0,M,psum);
-
-        for(int i=0; i<N; i++) dp[i][1] = psum[N]-psum[i];
-        for (int m=2; m<=M; m++){            
-            for(int i=0; i<=N-m; i++){
-                dp[i][m]=INT_MAX;        
-                for(int j=i; j<=N-m; j++)                                   
-                    dp[i][m] = min(dp[i][m], max(psum[j+1]-psum[i], dp[j+1][m-1]));
-            }
-        }
-        return dp[0][M];
-        
     }
-    bool partation(vector<int>& nums, int m, int mid){
-        int i=0;
-        while (m > 0 ){
-            int sum = 0;
-            while (i < nums.size() && sum + nums[i] <= mid) 
+    bool partition(vector<int>& nums, int size, int m){
+        int i=0, sum=0;
+        while (m > 0){
+            m--,sum=0;
+            while (i < N && sum + nums[i] <= size)
                 sum += nums[i++];
-            --m;
         }
-        return (i == nums.size());
+        return i == N;
     }
-    int lsum(vector<int>& nums, int i, int m , vector<int> &psum){
-        if(i>=N || m<=0) return 0;
-        if ( m == 1) return psum[N] - psum[i];
-        
-        if (dp[i][m] != -1) return dp[i][m];
-        int ans  = INT_MAX;
-        for (int  j = i; j<N-m; j++)
-            ans = min(ans , max(psum[j+1]-psum[i] , lsum (nums, j+1,m-1,psum)));
-        return dp[i][m] = ans;
-        
-    }
-    
+   
 };
