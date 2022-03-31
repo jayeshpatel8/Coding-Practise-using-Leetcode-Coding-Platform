58b85322e96ccf7f8857a148ed97a4f7f030da7f
jayeshpatel8 <jnpatel81@gmail.com>
2022-03-31 19:51:14 +0000

    Sync LeetCode submission - Longest Substring of One Repeating Character (cpp)

--- problems/longest_substring_of_one_repeating_character/solution.cpp
+++ problems/longest_substring_of_one_repeating_character/solution.cpp
@@ -1,5 +1,68 @@
 class Solution {
 public:
+    struct Segment{
+        //int l , r;
+        int prefix, suffix, maxLen; 
+    };
+    string s;
+    Segment tree[4 * 100000 + 5]; 
+    void build(int i, int l, int r){
+        if (l==r){
+            //tree[i].r = tree[i].l = l;
+            tree[i].prefix = tree[i].suffix = tree[i].maxLen = 1;
+        }
+        else{
+            int m = (l + r)/2;
+            build(i*2,l,m);
+            build(i*2+1,m+1,r);
+            push_up(i,l,r);
+        }
+    }
+    void push_up(int i, int l, int r){
+        auto &left  = tree[2*i] ;
+        auto &right  = tree[2*i+1] ;
+        tree[i].maxLen = max(left.maxLen, right.maxLen);
+        tree[i].prefix = left.prefix;
+        tree[i].suffix = right.suffix;
+        int m = (l+r)/2;
+        if (s[m] == s[m+1]){
+            tree[i].maxLen = max(tree[i].maxLen, left.suffix + right.prefix);
+            if (m-l+1 == left.maxLen)
+                tree[i].prefix += right.prefix;
+            if (r-m == right.maxLen)
+                tree[i].suffix += left.suffix;
+        }
+    }
+    void query(int i, int l, int r, int idx, char ch){
+        if (l==r){
+            s[l] = ch;
+        }
+        else{
+            int m = (l + r)/2;
+            if (idx <= m )
+                query(i*2,l,m,idx,ch);
+            else
+                query(i*2+1,m+1,r,idx,ch);
+            push_up(i,l,r);
+        }
+    }
+    vector<int> longestRepeating(string _s, string qc, vector<int>& qi) {
+        s = _s;        
+        int qsz = qi.size(),sz=s.size();
+        build(1,0,sz-1);
+        vector<int> ans(qsz);
+        for ( int q = 0; q<qsz; q++){
+            int i = qi[q], ch=qc[q];
+            if (ch != s[i]){
+                query(1,0,sz-1,i,ch);
+            }
+            ans[q] = tree[1].maxLen;;
+        }
+        return ans;
+    }
+};
+/* class Solution {
+public:
     map<int,int> interval;
     multiset<int> len;
     vector<int> longestRepeating(string s, string qc, vector<int>& qi) {
@@ -60,4 +123,4 @@ public:
         }
         return ans;
     }
-};
+};*/
