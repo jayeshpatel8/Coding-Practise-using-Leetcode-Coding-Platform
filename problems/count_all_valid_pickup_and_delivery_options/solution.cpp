65c8881821b3b4ab020d4326e3dde3339af4d4b0
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-01 15:22:48 +0000

    Sync LeetCode submission - Count All Valid Pickup and Delivery Options (cpp)

--- problems/count_all_valid_pickup_and_delivery_options/solution.cpp
+++ problems/count_all_valid_pickup_and_delivery_options/solution.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    int MOD = 1e9 + 7;
     int countOrders(int n) {
-        long ans = 1, mod = 1e9 +7;
-            
-            for (int i=1; i<=n; i++){
-                ans = (ans * i ) % mod;
-                ans = (ans * (2 *i -1 ) ) % mod;
-            }
-        return ans;
+        vector<vector<long>> dp (n + 1, vector<long>(n + 1, -1));
+        return cnt(n,n,dp);
+    }
+    long cnt(int p, int d,  vector<vector<long>>& dp){
+        if (d<p) return 0;
+        if (dp[p][d] != -1) return dp[p][d];
+        if (p== 0 && d==0){
+            return dp[p][d] = 1;
+        }
+        long ans= 0 ;
+        if (p > 0) ans= (p * cnt (p-1,d,dp)) % MOD;
+        if (d > p )
+            ans =  (ans + ((d-p) * cnt(p,d-1,dp)) %MOD)%MOD;
+        return dp[p][d] = ans;
     }
 };
