c2279e7edc2d2a72617b1bb902f96efdfdf6307f
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-01 12:35:53 +0000

    Sync LeetCode submission - Delete and Earn (cpp)

--- problems/delete_and_earn/solution.cpp
+++ problems/delete_and_earn/solution.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& nums) {
-        int freq[10001]={}, dp[10001]={};
-        for(auto i : nums) freq[i]+=i;        
-        dp[0] = freq[0], dp[1] =max(dp[0],freq[1]);
-        for (int i=2; i<10001;i++)
-            dp[i] = max(dp[i-1], dp[i-2] + freq[i]);
-        return dp[1000];
+    int deleteAndEarn(vector<int>& nums) {        
+        vector<int> freq(10001);
+        for (auto i: nums)  freq[i]+=i;
+        int one_back=freq[1], two_back=0;
+        for (int i = 2; i<10001; i++){
+            int t = max(one_back, two_back + freq[i]);
+            two_back=one_back; one_back = t;
+        }
+        return one_back;
     }
+
 };
