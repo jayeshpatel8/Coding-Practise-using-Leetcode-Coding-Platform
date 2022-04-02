5b586cc707e702095436cf51d704a1a164d72822
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-02 20:44:52 +0000

    Sync LeetCode submission - Sum of Scores of Built Strings (cpp)

--- problems/sum_of_scores_of_built_strings/solution.cpp
+++ problems/sum_of_scores_of_built_strings/solution.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     static constexpr int64_t MOD = 1000000007;
     long long sumScores(string s) {
+        const int64_t n = s.length();
+        vector<int> z(n);
+        for ( int i=0,l=0,r=0; i<n; i++){
+            if (i>r){
+                l=r=i;
+                while(r<n && s[r-l] == s[r]) r++;
+                z[i] = r-l;
+                r--;
+            }
+            else{
+                int i1 = i-l;
+                if (i + z[i1] <= r) z[i] = z[i1];
+                else
+                {
+                    l = i;
+                    while(r<n && s[r-l] == s[r]) r++;
+                    z[i] = r-l;
+                    r--;
+                }
+            }
+        }
+        long long sum=n;
+        for (int i=1; i<n; i++) sum +=z[i];
+        return sum;
+    }
+        long long sumScores2(string s) {
         const int64_t N = s.length();
         int64_t h1 = 0, h2 = 0, prod = 1, sum = N;
         for (size_t i = 0, j = N - 1; j > 0; ++i, --j) {
