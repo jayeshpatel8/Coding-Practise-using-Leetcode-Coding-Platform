514b8f9b6ac4715dea5e8a9d0233c8e322fac23e
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-15 20:11:47 +0000

    Sync LeetCode submission - Trim a Binary Search Tree (cpp)

--- problems/trim_a_binary_search_tree/solution.cpp
+++ problems/trim_a_binary_search_tree/solution.cpp
@@ -13,11 +13,14 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if (!root) return root;
-        if (root->val < low) return  trimBST(root->right,low,high);
-        else if (root->val  > high) return  trimBST(root->left,low,high);
-        
-        root->left = trimBST(root->left,low,high);        
-        root->right = trimBST(root->right,low,high);      
-        return root;        
+        int val = root->val;
+        if (low <= val  && val <= high) {
+            root->left = trimBST(root->left,low,high);                  
+            root->right = trimBST(root->right,low,high);
+        }
+        else{   
+            return trimBST(high < val ? root->left : root->right ,low,high);
+        }
+        return root;
     }
 };
