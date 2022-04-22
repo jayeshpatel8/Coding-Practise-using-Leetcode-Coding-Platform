a61421b3e94fc2c24bf0eadca55ab79708e46b45
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-22 11:09:25 +0000

    Sync LeetCode submission - Design HashMap (cpp)

--- problems/design_hashmap/solution.cpp
+++ problems/design_hashmap/solution.cpp
@@ -1,36 +1,20 @@
 class MyHashMap {
-    const int M = 1000;
-    vector<vector<pair<int,int>>> kv;
 public:
-    /** Initialize your data structure here. */
-    MyHashMap() {        
-        kv.resize(1000);
+    vector<int> map;
+    MyHashMap() : map(1e6 +1,-1 ){
+        
     }
     
-    /** value will always be non-negative. */
     void put(int key, int value) {
-        int h=key%M;
-        for(auto & i :  kv[h]){
-            if(i.first == key) {i.second = value;return;}
-        }        
-        kv[h].push_back({key,value});
+        map[key]=value;
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        int h=key%M;
-        for(auto & i :  kv[h]){
-            if(i.first == key) return i.second;
-        }
-        return -1;
+        return map[key];
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        int h=key%M;
-        for(auto it=kv[h].begin(); it!=kv[h].end(); it++){
-            if(it->first == key) {kv[h].erase(it); return;}
-        }        
+        map[key]=-1;
     }
 };
 
