04ba6cc4b83de940bbdabda36701bb2844b13a98
jayeshpatel8 <jnpatel81@gmail.com>
2022-04-28 21:08:24 +0000

    Sync LeetCode submission - Path With Minimum Effort (cpp)

--- problems/path_with_minimum_effort/solution.cpp
+++ problems/path_with_minimum_effort/solution.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    int M, N, ans = INT_MAX;
-    int d [5] = {-1,0,1,0,-1};
-    int minimumEffortPath(vector<vector<int>>& H) {
-        M = H.size(), N = H[0].size();
-        priority_queue<pair<int, array<int,2>>> pq;
-        pq.push({0,{0,0}});        
-        vector<vector<int>> dp(M,vector<int>(N, INT_MAX));
-        dp[0][0]=0;
-        while (!pq.empty()){
-            auto [e , p] = pq.top();
-            pq.pop();
-            int x = p[0], y = p[1] ,eff = (-1) * e;
-            if (x==M-1 && y==N-1) return eff;
-            for (int r = 0; r<4; r++){
-                int x1 = x +d[r], y1 = y + d[r+1];
-                if (x1 <0 || x1 >= M || y1 < 0 || y1 >= N ) continue;
-                int e2 = max(abs(H[x][y] - H[x1][y1]),eff);
-                
-                if (e2  < dp[x1][y1]){
-                    dp[x1][y1] = e2;
-                    pq.push({(-1) * e2,{x1,y1}});
-                }
-            }            
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int l = 0, r= 1e6;
+        while (l<r){
+            int m = l + (r-l)/2;
+            auto c = check(heights,m);
+            if (c <= m)
+                r = c;
+            else
+                l= m+1;
         }
-        return -1;
+        return r;
+    }
+    int dirs[5]={-1,0,1,0,-1};
+    int check(vector<vector<int>>& hs, int k){
+        int n= hs.size(),m = hs[0].size();
+        priority_queue<array<int,2>,vector<array<int,2>>,greater<array<int,2>>> pq;
+        
+        vector<vector<bool>> seen(n, vector<bool>(m, false));
+        pq.push({0,0});
+        seen[0][0]=true;
+       int best=0;
+        while (!pq.empty()){
+            auto  i = pq.top(); pq.pop();            
+            
+            int r = i[0], c = i[1];
+            if (r==n-1 && c==m-1) return best;
+            for (int j=0; j<4; j++){
+                auto r1= r+dirs[j] ,c1 = c+dirs[j+1];
+                if (r1<0 || r1>=n || c1<0 ||c1>=m || seen[r1][c1]) continue;
+                auto diff = abs(hs[r][c] - hs[r1][c1]);
+                if (k < diff ) continue;
+                best= max(best,diff);
+                seen[r1][c1] =true;
+                pq.push({r1 , c1});
+            }
+        }
+        return k+1;
     }
 };
