959fb771054c27392283532f9c2a222672919e38
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-06 11:19:36 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (cpp)

--- problems/remove_all_adjacent_duplicates_in_string_ii/solution.cpp
+++ problems/remove_all_adjacent_duplicates_in_string_ii/solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        vector<pair<char,int>   > st;
-        st.push_back({'*',0});
-        for(auto & c : s){
-            if(c == st.back().first) ++st.back().second;
-            else st.push_back({c,1});
-            if (st.back().second == k) st.pop_back();
+        vector<array<int,2>> ans(1,{'!',0});
+        for (int i=0; i<s.size(); i++){
+            if (ans.back()[0] != s[i])
+                ans.push_back({s[i],1});
+            else {
+                if (ans.back()[1] == k-1){
+                    ans.pop_back();
+                }
+                else
+                    ++(ans.back()[1]);
+            }
         }
-        string ans="";
-        
-        for(auto & i : st){
-            //auto i = st.top(); st.pop();
-            ans.append( i.second, i.first);
+        string a;
+        for (int i=1;i <ans.size(); i++){
+            a.append(ans[i][1],ans[i][0]);
         }
-        return ans;
+        return a;
     }
 };
