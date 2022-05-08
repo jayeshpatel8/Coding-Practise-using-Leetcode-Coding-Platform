1719dc13910df6c393de6ccf6df0f9b1503d33f3
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-08 20:21:30 +0000

    Sync LeetCode submission - Minimum Number of K Consecutive Bit Flips (cpp)

--- problems/minimum_number_of_k_consecutive_bit_flips/solution.cpp
+++ problems/minimum_number_of_k_consecutive_bit_flips/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int flipped=0, ans = 0;
-        for (int i =0 , N = nums.size(); i<N; i++){
+        int flipped=0, ans = 0, N = nums.size();
+        vector<bool> flip(N);
+        for (int i =0 ; i<N; i++){
             if (i>=k){
-                if (nums[i-k]>1)
+                if (flip[i-k])
                     flipped--;
             }
             if (flipped%2 == nums[i]){
                 if (i+k-1>= N) return -1;
-                nums[i]+=2;
+                flip[i]=1;
                 flipped++;
                 ans++;
             }
