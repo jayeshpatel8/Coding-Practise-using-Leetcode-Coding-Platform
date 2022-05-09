667f3cfc0f3319add7899f19002a0be98e4f8be6
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-09 12:23:16 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+           vector<string> ch={"","","abc", "def", "ghi", 
+                                  "jkl", "mno","pqrs",
+                                  "tuv","wxyz"};
         vector<string> ans;
-        string d[8] = {"abc","def","ghi",
-                       "jkl","mno","pqrs",
-                       "tuv","wxyz"
-                      }; 
         
-        for (auto c  : digits){
-            vector<string> res;
-            for (auto c1 : d[c-'2']){
-                    
-                if (ans.size()==0){
-                    string s="";
-                    s += c1;
-                    res.push_back(s);
-                    
-                }
-                else{
-                    for (auto s : ans){
-                        res.push_back(s+c1);
+        for (auto d : digits){
+            vector<string> t;
+            if (ans.empty()){
+                   for (auto &s : ch[d-'0']){
+                        t.push_back(string(1,s));
+                    }
+            }
+            else{
+                for (auto &a : ans){
+                    for (auto &s : ch[d-'0']){
+                        t.push_back(a+s);
                     }
                 }
             }
-            ans=res;
+            ans.swap(t);
         }
         return ans;
     }
