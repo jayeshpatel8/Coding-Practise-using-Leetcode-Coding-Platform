64629e5f920d0bf80aa43adfbff4ccc29b967642
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-11 11:46:26 +0000

    Sync LeetCode submission - Count Sorted Vowel Strings (cpp)

--- problems/count_sorted_vowel_strings/solution.cpp
+++ problems/count_sorted_vowel_strings/solution.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        int dp[6] = {0, 1,1,1,1,1};
-        for ( int i=1; i<n; i++){
-            for ( int j = 1; j<=5; j++){
-                dp[j] += dp[j-1]; 
-            }
+        
+        int dp[5]={1,1,1,1,1};
+        
+        while (n-- > 1){
+           // dp[0] = dp[0] ; // ending at a
+            dp[1] += dp[0]  ; //ending at e
+            dp[2] += dp[1] ; //ending at i
+            dp[3] += dp[2] ; //ending at o
+            dp[4] += dp[3] ; //ending at u            
+            
         }
-        return accumulate(begin(dp),end(dp),0L);
+        
+        return accumulate(begin(dp),end(dp),0);
     }
+    /*
+    int dp[51][5];
+    string s = "aeiou";
+    int countVowelStrings2(int n) {
+        
+        memset(dp,-1,sizeof(dp));
+        
+        return dfs(n,0);
+    }
+    int dfs(int n, int i){
+        if (n == 0) return 1;
+        if (i>=5) return 0;
+        if (dp[n][i] != -1) return dp[n][i];
+        int ans = 0;
+        for (int j = i; j<5; j++){
+             ans += dfs(n-1,j);
+        }
+        return dp[n][i] = ans;
+    }
+    */
 };
