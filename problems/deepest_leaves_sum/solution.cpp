d89608573079e1751e2933d21b6715f6630ec44d
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-16 21:32:12 +0000

    Sync LeetCode submission - Deepest Leaves Sum (cpp)

--- problems/deepest_leaves_sum/solution.cpp
+++ problems/deepest_leaves_sum/solution.cpp
@@ -11,22 +11,34 @@
  */
 class Solution {
 public:
-    int  max_cnt=0,sum=0;
-    int deepestLeavesSum(TreeNode* root) {
-        dls(root,0);
+    int sum = 0, mxlvl=0;
+    int deepestLeavesSum(TreeNode* root ,int lvl=0) {
+        if (!root) return sum;
+        if (mxlvl == ++lvl)
+            sum +=root->val;
+        else if (mxlvl < lvl) 
+            mxlvl = lvl,  sum = root->val;
         
+        deepestLeavesSum(root->left,lvl);
+        deepestLeavesSum(root->right,lvl);
+        return sum;
+    }    
+    int deepestLeavesSum2(TreeNode* root) {
+        vector<TreeNode *> q;
+        q.push_back(root);
+        int sum=0 ,j=0 , i =1;
+        while (j<i){
+            auto sz = i; 
+            sum = 0;
+            while (j<sz){
+                auto * n = q[j++];
+                sum += n->val;
+                if (n->left) q.push_back(n->left), i++;;
+                if (n->right) q.push_back(n->right) , i++;;
+               
+            }
+        }
         return sum;
     }
-    void dls(TreeNode * root, int cnt){
-        if (!root){
-            if (cnt>max_cnt) 
-            {  max_cnt =cnt ; sum = 0;}
-            return ;
-        }
-        cnt++;
-        dls(root->left,cnt);
-        dls(root->right,cnt);
-        
-        if (cnt == max_cnt) sum +=root->val;
-    }
+    
 };
