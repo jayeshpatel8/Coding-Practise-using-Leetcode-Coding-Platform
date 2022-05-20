056d74a8693210d9fce91cf791b318f6554af7af
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-20 14:18:09 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- problems/unique_paths_ii/solution.cpp
+++ problems/unique_paths_ii/solution.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& og) {
-        int m = og.size(),n=og[0].size();
-        int dp[m+1][n+1];
-        memset(dp,0,sizeof(dp));
-        dp[0][1]=1;
-        for (int i=1; i<=m; i++){
-            for (int j=1; j<=n; j++){
-                if (og[i-1][j-1]){
-                    dp[i][j] = 0;
-                }
-                else{ 
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];                
-                }
+    int m  , n ;
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+        m = g.size(); n = g[0].size();
+        if (g[m-1][n-1] || g[0][0]) return 0;
+        if (m==1 && n==1 ) return !g[m-1][n-1];
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1));
+        dp[1][0]=1;
+        for (int i=0; i<m;i++){
+            for (int j=0; j<n; j++){
+                if (g[i][j])
+                    dp[i+1][j+1]=0;
+                else
+                    dp[i+1][j+1]= dp[i+1][j] + dp[i][j+1];
             }
         }
         return dp[m][n];
+    }
+    int dfs(int i, int j, vector<vector<int>> &g, vector<vector<int>>& dp){
+        if (i==m-1 && j==n-1) return 1;
+        if (dp[i][j]) return dp[i][j];
+        int ans = 0 ;
+        if (i+1<m && g[i+1][j] == 0)
+            ans = dfs(i+1,j,g,dp);
+        if (j+1<n && g[i][j+1] == 0)
+            ans += dfs(i,j+1,g,dp);
+        return dp[i][j] = ans;
     }
 };
