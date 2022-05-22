a3b12b2976d8836b9c02bc7a320cb6e0dd8dbc86
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-22 10:34:52 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

--- problems/palindromic_substrings/solution.cpp
+++ problems/palindromic_substrings/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n =  s.length();
-        uint64_t cnt=0;
-
-        for (int i=0; i<n; i++) {            
-            // even 
-            for (int start=i,end=i; start>=0 && end<n && s[start]==s[end]; start--,end++) 
-                cnt++;
-            // odd
-            for (int start=i-1,end=i; start>=0 && end<n && s[start]==s[end]; start--,end++) 
-                cnt++;
+        int n =  s.size();
+        
+        int ans = 0;
+        for (int i =0 ; i<n; i++){
+            
+            //odd
+            for (int start=i , end=i+1;  start >=0 && end <n &&  s[start] == s[end]; start--,end++ )
+                ans++;
+            //evn
+            for (int start=i , end=i;  start >=0 && end <n &&  s[start] == s[end]; start--,end++ )
+                ans++;
         }
-        return cnt;
+        return ans;
     }
 };
