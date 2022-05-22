a2b2bc4fa9b965a338020d2a1e4aae1ce5c03358
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-22 16:51:42 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- problems/coin_change/solution.cpp
+++ problems/coin_change/solution.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int dp[10005];
+    int dp[10001]={};
     int coinChange(vector<int>& coins, int amount) {
-        
+        //memset(dp,-1,sizeof(dp));
+        //int ans = dfs(coins, amount, 0);
+        //return ans >=1e5 ? -1 : ans ;
         if(!amount) return 0;
-
-        for(int i=0; i< 10005; i++) dp[i]=10001;
-        dp[0]=0;
-        for(int i=1; i<=amount; i++)
-        {
-            for(auto coin : coins){
-                if(coin<= i)
-                    dp[i] = min(dp[i],dp[i-coin]+1);
+        for (int j=1; j<=amount; j++){
+            dp[j]=1e5;
+            for (auto &c : coins){
+                if (j >= c)    
+                    dp[j] =  min(dp[j], dp[j - c] + 1);
             }
         }
-        
-        return dp[amount]==10001?-1:dp[amount];
+        return dp[amount] == 1e5 ? -1 : dp[amount];
     }
+    /*
+    int dfs(vector<int>& coins, int amount, int i) {
+        if (amount == 0) return 0;
+        if (i>= coins.size() )
+            return 1e5;
+        if (dp[amount][i] != -1 ) return dp[amount][i];
+        int ans = 1e5;
+        
+        if (coins[i] <= amount)
+            ans = min (ans, 1  + dfs(coins,amount-coins[i],i));
 
+        ans = min (ans, dfs(coins,amount,i+1));
+                    
+        return dp[amount][i] = ans;
+    }
+    */
 };
