191d5d82062114dd156711381b51a2fda3bdec15
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-26 22:50:18 +0000

    Sync LeetCode submission - Sum of Subarray Minimums (cpp)

--- problems/sum_of_subarray_minimums/solution.cpp
+++ problems/sum_of_subarray_minimums/solution.cpp
@@ -3,6 +3,22 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
         vector<int> l(n),r(n,n) ,st;
+        int ans = 0 , mod = 1e9 + 7;
+        for (int i=0; i<=n; i++){
+            while (!st.empty() && (i==n || arr[st.back()] >= arr[i])){
+                int j = st.back();st.pop_back();
+                int k = st.empty() ? -1: st.back();
+                ans = (ans  + ((long)arr[j] * (i-j) * (j-k))%mod) % mod;    
+            }
+            st.push_back(i);
+        }
+
+        return ans;
+    }
+
+    int sumSubarrayMins2(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> l(n),r(n,n) ,st;
         for (int i=0; i<n; i++){
             while (!st.empty() && arr[st.back()] >= arr[i])
                 st.pop_back();
