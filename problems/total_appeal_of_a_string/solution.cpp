23e90fa48e58a2da3bdddb5222d2a94ad7955132
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-26 11:54:39 +0000

    Sync LeetCode submission - Total Appeal of A String (cpp)

--- problems/total_appeal_of_a_string/solution.cpp
+++ problems/total_appeal_of_a_string/solution.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     long long appealSum(string s) {
-        long long ans = 0;
         
-        int n= s.size();
-        vector<int> idx(26,-1);
-        for (int i=0, cur=0; i<n; i++){
-            //abcab
-            auto c = s[i]-'a';
-            cur += i - idx[c];
-            ans += cur;
-            idx[c]=i;
+        int n = s.size();
+        long long ans = 0;
+        vector<int> substr_cnt(26);
+        for (int i=0,cnt=0; i<n; i++){
+            cnt += i+1 - substr_cnt[s[i]-'a'];
+            ans +=cnt;
+            substr_cnt[s[i]-'a'] = i+1;
         }
         return ans;
     }
