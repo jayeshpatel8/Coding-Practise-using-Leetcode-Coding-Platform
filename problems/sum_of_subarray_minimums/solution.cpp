c5f38a1defa86d9469f75d1f60a513b363f7cfab
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-26 20:19:20 +0000

    Sync LeetCode submission - Sum of Subarray Minimums (cpp)

--- problems/sum_of_subarray_minimums/solution.cpp
+++ problems/sum_of_subarray_minimums/solution.cpp
@@ -2,42 +2,22 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-        
-        vector<int> left(n,-1), right(n,n);
-        stack<int> s,s1;
-        for (int i = 0; i<n; i++){
-            while(!s.empty() && arr[i]< arr[s.top()])  
-            {right[s.top()]=i; s.pop();}
-            
-            s.push(i);
+        vector<int> l(n),r(n,n) ,st;
+        for (int i=0; i<n; i++){
+            while (!st.empty() && arr[st.back()] >= arr[i])
+                st.pop_back();
+            l[i] = st.empty() ? i+1 : i - st.back();
+            st.push_back(i);
         }
-       
-        for (int i = n-1; i>=0; i--){
-            while(!s1.empty() && arr[i]<= arr[s1.top()])  
-            {left[s1.top()]=i; s1.pop();
-            }
-            
-            s1.push(i);
-        }       
-       
-       
-        uint64_t ans=0;
-        int mod = 1e9+7;
-        for (uint64_t i=0; i<n;i++){
-            ans = (ans + arr[i] *( (i-left[i]) * (right[i]-i)% mod)) % mod;
-            
+        st.clear();
+        int ans = 0 , mod = 1e9 + 7; 
+        for (int i=n-1; i>=0; i--){
+            while (!st.empty() && arr[st.back()] > arr[i])
+                st.pop_back();
+            r[i] = st.empty() ? n-i :  st.back() - i;
+            st.push_back(i);
+            ans = (ans  + ((long)arr[i] * l[i] * r[i])%mod) % mod;
         }
-     
         return ans;
     }
 };
-/*
-3,1,2,4
-s - 1 2 4   
-R - 1 4 4 4
-L - -1 -1 1 2
-3 * (1 * 1)  = 3
-1 2 3= 6
-2 1 2 = 4
-4 1 1=4
-*/
