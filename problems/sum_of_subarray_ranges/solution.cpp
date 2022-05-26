90f06b42dd5257c277f9bf76decc133f32fdd43d
jayeshpatel8 <jnpatel81@gmail.com>
2022-05-26 22:37:17 +0000

    Sync LeetCode submission - Sum of Subarray Ranges (cpp)

--- problems/sum_of_subarray_ranges/solution.cpp
+++ problems/sum_of_subarray_ranges/solution.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long ans = 0;
+        int  n  = nums.size();
+        vector<int> st;
+        long long mi=0,ma=0;
         
-        for ( int i =0; i<nums.size()-1; i++){
-            int s = min(nums[i],nums[i+1]), l = max(nums[i],nums[i+1]);
-            ans += l - s;
-            for (int j=i+2; j<nums.size(); j++){
-                s = min(nums[j],s), l = max(nums[j],l);
-                ans += l - s;
+        for (int i=0; i<=n; i++){
+            while (!st.empty() &&(i==n || nums[st.back()] > nums[i]))
+            {
+                int j = st.back(), k = st.size() <2 ? -1 : st[st.size()-2];
+                mi += (i-j) * (j-k) * (long long)nums[j];
+                st.pop_back();
             }
+            st.push_back(i);
         }
-        return ans;
+        st.clear();
+        for (int i=0; i<=n; i++){
+            while (!st.empty() &&(i==n || nums[st.back()] < nums[i]))
+            {
+                int j = st.back(), k = st.size() <2 ? -1 : st[st.size()-2];
+                ma += (i-j) * (j-k) * (long long)nums[j];
+                st.pop_back();
+            }
+            st.push_back(i);
+        }
+        return ma-mi;
     }
 };
