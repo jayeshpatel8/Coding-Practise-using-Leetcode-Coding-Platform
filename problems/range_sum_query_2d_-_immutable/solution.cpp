4630a5ae3ff8ece03831f35eb0c706b8b4a7c04d
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-03 12:23:26 +0000

    Sync LeetCode submission - Range Sum Query 2D - Immutable (cpp)

--- problems/range_sum_query_2d_-_immutable/solution.cpp
+++ problems/range_sum_query_2d_-_immutable/solution.cpp
@@ -1,44 +1,22 @@
-#if 0
-class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
-public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row>0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + 
-                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
-            }
-        }
-    }
-
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
-    }
-};
-
-#else
 class NumMatrix {
 public:
     vector<vector<int>> psum;
-    
     NumMatrix(vector<vector<int>>& matrix) {
-        int  N = matrix.size(), M=matrix[0].size();
-        psum = vector<vector<int>>(N+1, vector<int>(M+1, 0));
-        for (int i=0;i<N;i++)        
-            for (int j=0; j<M ;j++)
-                psum[i+1][j+1] = matrix[i][j] + psum[i][j+1]-psum[i][j]+psum[i+1][j]; 
+        int M= matrix.size(), N = matrix[0].size();
+        psum.resize(M + 1, vector<int> (N+1));
+        
+        for (int i=1; i<=M; i++){
+            for (int j=1; j<=N; j++){
+                psum [i][j] = matrix[i-1][j-1] + psum [i-1][j] + psum [i][j-1] - psum [i-1][j-1];
+            }
+        }
     }
     
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return (psum[row2+1][col2+1] - psum[row1][col2+1] - psum[row2+1][col1] + psum[row1][col1]);
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        return psum [r2+1][c2+1]  - psum [r1][c2+1] - psum [r2+1][c1] + psum [r1][c1];
     }
 };
-#endif
+
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix* obj = new NumMatrix(matrix);
