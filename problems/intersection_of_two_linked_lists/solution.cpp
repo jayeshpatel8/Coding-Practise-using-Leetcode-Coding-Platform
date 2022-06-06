0d35bada42e9c0a729f40c6e81df5da5d3ec5a6d
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-06 09:16:08 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (cpp)

--- problems/intersection_of_two_linked_lists/solution.cpp
+++ problems/intersection_of_two_linked_lists/solution.cpp
@@ -9,28 +9,31 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (!headA || !headB) return NULL;
-        int l1=0,l2=0;
-        ListNode * h1=headA, *h2= headB;
-        while(h1){
-            l1++;
-            h1=h1->next;
+        int l1 = 0 ,l2 = 0;
+        auto * h1 = headA;
+        while (h1)
+            l1++, h1= h1->next;
+        h1 = headB;
+        while (h1)
+            l2++, h1= h1->next;
+        auto *h2=headB;
+
+        if (l1 > l2){
+            h1= headA;
+            int d = l1-l2;
+            while (d--)
+                h1=h1->next;
         }
-        while(h2){
-            l2++;
-            h2=h2->next;
-        }        
-        h1=headA;
-        h2= headB;
-        while (l1<l2){
-            l2--;h2=h2->next;
+        else{
+            h1= headB;
+            h2=headA;
+            int d = l2-l1;
+            while (d--)
+                h1=h1->next;
         }
-        while (l1>l2){
-            l1--;h1=h1->next;
-        }        
         while(h1){
-            if(h1 ==h2) return h1;
-            h1=h1->next;h2=h2->next;
+            if (h1 == h2) return h1;
+            h1 = h1->next,h2=h2->next;
         }
         return NULL;
     }
