321bc3a2034e8c7040aa8619fccc13b30764b9a9
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-15 14:02:44 +0000

    Sync LeetCode submission - Longest String Chain (cpp)

--- problems/longest_string_chain/solution.cpp
+++ problems/longest_string_chain/solution.cpp
@@ -1,59 +1,37 @@
 class Solution {
 public:
-    int wstart[18]={};
-    int wsize[18]={}  , dp[1000]={};
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),end(words),[](auto & s1, auto&s2){return s1.size() < s2.size();});
-        for ( auto i =0; i< words.size(); i++){
-            int sz = words[i].size();
-            if (wsize[sz] == 0) wstart[sz]= i;
-            wsize[sz]++;
-        }        
-        
-        int ans = 0;
-        for (int i = 0;i<words.size();i++){
-            int sz = words[i].size()+1;
-            for (int j = wstart[sz], jend = j + wsize[sz]; j<jend; j++ ){
-                if (isCommSeq(words[i],words[j])){
-                    dp[j] =max(dp[j], 1 + dp[i]);                    
-                }
+        vector<string> w[17];
+        vector<int> dp[17];
+        for (auto &s : words)
+            w[s.size()].push_back(s);
+        unordered_map<string,int> map;
+        int ans = 1;
+        for (int i=16; i>0; i--){
+            if (w[i].size()){
+                for (int j=0; j<w[i].size(); j++){
+                    dp[i].push_back(1);
+                    if (i==16) continue;
+                    for (int k=0; k<w[i+1].size(); k++){
+                        if (isvalid(w[i][j], w[i+1][k], i+1))
+                            dp[i][j] = max(dp[i][j], 1 + dp[i+1][k]);
+                    }
+                    ans= max(ans, dp[i][j]);
+                }                
             }
-            ans = max(ans, dp[i]);
         }
-        return ++ans;
+        return ans;
     }
-    /*
-    int longestStrChain(vector<string>& words) {
-        sort(words.begin(),end(words),[](auto & s1, auto&s2){return s1.size() < s2.size();});
-        memset(dp,-1,sizeof(dp));
-        for ( auto i =0; i< words.size(); i++){
-            int sz = words[i].size();
-            if (wsize[sz] == 0) wstart[sz]= i;
-            wsize[sz]++;
+    bool isvalid(string &s1, string &s2, int len){
+        int changed=0;
+        for (int i=0,j=0; j<len; j++){
+            if (s1[i] == s2[j]) i++;
+            else if (changed){
+                return false;
+            }
+            else
+                changed = 1;
         }
-        int ans = 0;
-        for ( int i=0; i<words.size(); i++){
-            ans = max(ans, dfs(words, i));
-        }
-        return ++ans;
-    }
-    int dfs(vector<string>& words, int i){
-        if (i >= words.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-        int ans = 0, sz =  words[i].size();;
-        for ( int j=wstart[sz+1], end = j + wsize[sz+1]; j<end; j++){
-            if (isCommSeq(words[i],words[j])){
-                ans  = max( ans, 1  + dfs(words, j));
-            }            
-        }        
-        
-        return dp[i] = ans;
-    }*/
-    bool isCommSeq(string & s1, string & s2){
-        int i=0, j=0;
-        for (; j<s2.size(); j++){
-            if (s1[i] ==s2[j]) i++;
-        }
-        return (i == s1.size());
+        return true;
     }
 };
