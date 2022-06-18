69f9d0a9d8f416d78af410af1bff6f1c016dcd40
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-18 14:29:27 +0000

    Sync LeetCode submission - Binary Tree Cameras (cpp)

--- problems/binary_tree_cameras/solution.cpp
+++ problems/binary_tree_cameras/solution.cpp
@@ -9,18 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+enum {
+  COVERED_NO_CAMERA=0,
+  NOT_COVERED_NO_CAMERA=1,
+  COVERED_CAMERA=2
+};
 class Solution {
 public:
     int ans=0  ;
-    unordered_set<TreeNode *> set={NULL};
+    
     int minCameraCover(TreeNode* root){
-        dfs(root);
+        
+        if (NOT_COVERED_NO_CAMERA == dfs(root) )
+            ans++;
         return ans;
     }
-    void dfs(TreeNode* root , TreeNode * p  = 0) {
+    int dfs(TreeNode *root){
+        if (!root) return COVERED_NO_CAMERA;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        if (l==NOT_COVERED_NO_CAMERA || r == NOT_COVERED_NO_CAMERA){
+            ans++;
+            return COVERED_CAMERA;
+        }
+        else if (l==COVERED_CAMERA || r==COVERED_CAMERA) return COVERED_NO_CAMERA;
+        else return NOT_COVERED_NO_CAMERA;
+    }
+    unordered_set<TreeNode *> set={NULL};
+    int min2CameraCover(TreeNode* root){
+        dfs2(root);
+        return ans;
+    }
+    void dfs2(TreeNode* root , TreeNode * p  = 0) {
         if (!root) return ;
-        dfs(root->left, root);
-        dfs(root->right,root);
+        dfs2(root->left, root);
+        dfs2(root->right,root);
         if (!set.count(root->left)  || !set.count(root->right)){
             ans++;
             set.insert(root);
