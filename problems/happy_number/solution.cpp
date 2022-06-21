ca4936c464712ee32ebae5e34909806f87e7f4e1
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-21 21:42:45 +0000

    Sync LeetCode submission - Happy Number (cpp)

--- problems/happy_number/solution.cpp
+++ problems/happy_number/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        int limit=100;
-        while (n!=1 && --limit){
+        int sqr[10]={0,1,4,9,16,25,36,49,64,81};
+        unordered_set<int>st;
+        while (!st.count(n)){            
+            st.insert(n);
             int sum = 0;
-            while (n){
-                    int d = n%10;  n= n/10;   
-                    sum += d * d;
-                }
-            n=sum;
+            while (n ){
+                sum += sqr[n%10];
+                n/=10;
+            }                        
+            n = sum;
         }
-        return n==1;
+        return n == 1;
     }
 };
