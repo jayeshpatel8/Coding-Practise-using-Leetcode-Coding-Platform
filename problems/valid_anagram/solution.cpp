ff1e6559a998aa92868072b795a82d5f44360000
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-21 22:09:16 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-        int ch[256];
-        for (int i='a'; i<'a'+26; i++) ch[i]=0;
-        for (int i=0; i<s.size(); i++) {
-            ch[s[i]]++;
-            ch[t[i]]--;
-        }
-        for (int i='a'; i<'a'+26; i++) if (ch[i]) return false;
-        return true;
+        vector<int> s1(26),t1(26);
+        for (auto c : s) s1[c-'a']++;
+        for (auto c : t) t1[c-'a']++;
+        return s1==t1;
     }
 };
