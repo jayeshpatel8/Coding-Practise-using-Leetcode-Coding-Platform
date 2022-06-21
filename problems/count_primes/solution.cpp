968d8373d1b3d03c42ba408fcf1f9e9fe8264651
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-21 22:23:22 +0000

    Sync LeetCode submission - Count Primes (cpp)

--- problems/count_primes/solution.cpp
+++ problems/count_primes/solution.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-
     int countPrimes(int n) {
-        int cnt=0;
-        vector<int> p(n+1,1);
-        
-        uint64_t ceil_ = ceil(sqrt(n));
-
-        for (int i=2;  i<n; i++ ){
-            if (p[i]==0 )  continue;
-            cnt += 1;
-            if (i>= ceil_) continue;
-            for (int j=i+i; j<=n; j+=i)
-                p[j]=0;            
+        vector<bool> notaPrime(n);
+        int ans=0;
+        for (int i=2; i<n; i++){
+            if (!notaPrime[i]){
+                ans++;
+                for (int j=2; j*i<n; j++)
+                    notaPrime[j*i]=true;
+            }
         }
-            
-        return cnt;
+        return ans;
     }
 };
