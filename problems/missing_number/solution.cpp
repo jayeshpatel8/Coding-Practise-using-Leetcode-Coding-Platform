6c723f30807d267b508fa40691bab194458ef73e
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-22 11:36:35 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- problems/missing_number/solution.cpp
+++ problems/missing_number/solution.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int sum = accumulate(begin(nums), end(nums),0);
         int  n = nums.size();
-        for (int i=0; i<n; i++){
-            int idx = nums[i] > 1e4 ? nums[i] - 1e4 - 1 : nums[i];
-            if (idx <n )
-                nums[idx] = nums[idx] + 1e4 + 1;
-        }
-
-        for (int i=0; i<n; i++)
-            if (nums[i] <= 1e4) return i;
-        
-        return n;
+        return (n*(n+1)/2) - sum;
     }
 };
