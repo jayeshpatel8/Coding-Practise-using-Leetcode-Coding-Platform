a8ef5927b01568f6aeb9248674edde954471ffdc
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-24 14:26:46 +0000

    Sync LeetCode submission - Construct Target Array With Multiple Sums (cpp)

--- problems/construct_target_array_with_multiple_sums/solution.cpp
+++ problems/construct_target_array_with_multiple_sums/solution.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        priority_queue<int> pq (target.begin(), target.end());
-        
-        int n=target.size();
-            // 9  3  5 
-        long long int sum = 0;
-        for (auto i : target)
-            sum += i;
-        if (1 == n && target[0]!=1 ) return false;
-        
-        while(1){
-            long long int  t = pq.top(); pq.pop();
+        priority_queue<int> pq(begin(target),end(target));
+        long sum = accumulate(begin(target),end(target),0L);
+
+        while ( 1){
+            long n = pq.top();pq.pop();
+            sum -=n;
+            if (sum == 1 || n ==1 ) return true; 
             
-            sum -= t;
-            if (sum == 1 || t==1) return true;          
-            long long int  c  = t %sum; 
+            if ( sum>=n || sum==0 || n%sum == 0) return false;       
+            long diff = n%sum;
+            pq.push(diff);            
+            sum += diff;
             
-            if ( t - sum <= 0|| c==0) return false;
-            
-            pq.push(c);
-            sum +=c;
-          
             
         }
+
         return true;
     }
+
 };
