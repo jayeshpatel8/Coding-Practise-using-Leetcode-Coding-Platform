0dbc86f9a838728c7b8617ae2aae5a2ea562e9bb
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-24 12:01:52 +0000

    Sync LeetCode submission - Sliding Window Median (cpp)

--- problems/sliding_window_median/solution.cpp
+++ problems/sliding_window_median/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> set(nums.begin(), nums.begin() + k);
-        int n = nums.size(), m = k-k/2-1;
-        auto mid = next(begin(set),m);
-
+        multiset<long> st(begin(nums),begin(nums)+k);
+        auto mid = next(st.begin() ,  k/2);
         vector<double> ans;
-        ans.push_back(k%2 ? *mid : ((double)*mid + *next(mid))/ 2.0);
-        for (int i=k; i<n; i++){
-            //if (nums[i] != nums[i-k])
-            {                
-                set.insert(nums[i]);
-                if (nums[i] < *mid) mid--;
-                if (nums[i-k] <= *mid) mid++;
-                set.erase(set.lower_bound(nums[i-k]) );                         
-            }
-            ans.push_back(k%2 ? *mid : ((double)*mid + *next(mid))/ 2.0);
+        int i=k, j =0;
+
+        ans.push_back( (*mid + *prev(mid, 1 - k%2))/ 2.0);
+        for (; i<nums.size(); i++,j++){
+            st.insert({nums[i]});
+            
+            if (nums[i] < *mid) mid--;
+            
+            if (nums[j] <= *mid) mid++;
+            st.erase({st.find(nums[j])});
+            
+            ans.push_back( (*mid + *prev(mid, 1 - k%2))/ 2.0);
         }
         return ans;
     }
-    
 };
