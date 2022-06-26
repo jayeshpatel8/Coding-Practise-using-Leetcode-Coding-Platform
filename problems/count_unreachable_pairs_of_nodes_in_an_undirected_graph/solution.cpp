4395b464d8a5be9b7d9bd95d1780aef13a725c4a
jayeshpatel8 <jnpatel81@gmail.com>
2022-06-26 16:31:19 +0000

    Sync LeetCode submission - Count Unreachable Pairs of Nodes in an Undirected Graph (cpp)

---
+++ problems/count_unreachable_pairs_of_nodes_in_an_undirected_graph/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> uf ,size, cnt;
+    int find(int a){
+        return uf[a] <0 ? a : uf[a] = find(uf[a]);
+    }
+    
+    void merge(int a, int b){
+        int pa= find(a), pb =find(b);
+        if (pa != pb){
+            if (size[pa] < size[pb]){
+                uf[pb]=pa;
+                size[pa] += size[pb];
+            }
+            else
+                uf[pa]=pb , size[pb] += size[pa];
+        }
+    }
+    
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        uf.resize(n, -1);
+        size.resize(n, 1);
+        //cnt.resize(n,1);
+        for (auto &e : edges){
+            //if (find(e[0]) != find(e[1]))
+                merge(e[0],e[1]);
+        }
+        long long ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            ans += (n - size[find(i)]);
+        }
+        return ans / 2;
+    }
+};
