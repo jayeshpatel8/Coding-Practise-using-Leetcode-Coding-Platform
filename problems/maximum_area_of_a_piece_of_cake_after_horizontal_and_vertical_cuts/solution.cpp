da9ad4f49084fdd6c38c6cb086ae06d8e90c8116
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-02 12:23:17 +0000

    Sync LeetCode submission - Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (cpp)

--- problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
+++ problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
-        sort(hc.begin(), hc.end());
-        sort(vc.begin(), vc.end());
         
-        int m = hc.size(), n = vc.size();
-       
-          int  hdiff=hc[0], vdiff=vc[0];
+        sort(begin(hc),end(hc));
+        sort(begin(vc),end(vc));
+        hc.push_back(h),vc.push_back(w);
+        int hmax = hc[0], wmax=vc[0] , mod = 1e9 + 7;
+        for (int i=1; i< hc.size();i++)
+            hmax = max( hmax, hc[i]-hc[i-1]);
         
-        for (int i=1; i< m; i++) hdiff = max(hdiff,( int)( hc[i]-hc[i-1])) ;
-        for (int i=1; i< n; i++) vdiff = max(vdiff, ( int) (vc[i]-vc[i-1])) ;
-        hdiff = max(hdiff, h-hc[m-1]);
-        vdiff = max(vdiff, w-vc[n-1]);
-               
-        return ((uint64_t)hdiff * vdiff) % ( (int)  1e9 + 7);
+        for (int i=1; i< vc.size();i++)
+            wmax = max( wmax, vc[i]-vc[i-1]);
+                
+        return ((long) hmax * wmax)% mod;
     }
 };
