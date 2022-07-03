09fd7841b8852cbcd82e28fefbed65b262842df8
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-03 11:39:37 +0000

    Sync LeetCode submission - Wiggle Subsequence (cpp)

--- problems/wiggle_subsequence/solution.cpp
+++ problems/wiggle_subsequence/solution.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        int n=nums.size();
-        if (n==1) return 1;
-       
-        int ans,prev=nums[0]-nums[1];
-        ans = prev==0? 1: 2;
-        const int shift = (sizeof(int)*8-1);
-        for(int i=2; i<n; i++){
-            int  diff = nums[i-1]-nums[i];
-            if(diff ==0 ) continue;
-            if(prev==0) {prev=diff,ans++;};
-            if((prev ^ diff ) >> shift) {
-                ans++, prev  = diff;
+        int N = nums.size(),  sign[2]={1,1};
+        for (int i=1; i<N; i++){
+            int diff = nums[i-1] - nums[i] ;
+            if (diff != 0){
+                bool pos = diff > 0;
+                sign[pos] = sign[!pos] + 1;        
             }
-            else
-                continue;            
         }
-        return ans;
+        return max(sign[0],sign[1]);
+    }    
+    /*
+    int wiggleMaxLength2(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> pos(N+1), neg(N+1);
+        for (int i=1; i<N; i++){
+            int diff = nums[i-1] - nums[i];
+            if (diff == 0){
+                pos[i] = pos[i-1];
+                neg[i] = neg[i-1];
+            }
+            else if (diff > 0){
+                pos[i] = neg[i-1]+1;
+                neg[i] = neg[i-1];
+            }
+            else{
+                neg[i] = pos[i-1] + 1;
+                pos[i] = pos[i-1];
+            }
+        }
+        return max(pos[N-1],neg[N-1])+1;
     }
+    */
 };
