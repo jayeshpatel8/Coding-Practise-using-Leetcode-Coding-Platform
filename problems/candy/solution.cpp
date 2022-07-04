b7ba89e5446890126849b28e623af5c0e0e99884
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-04 11:04:15 +0000

    Sync LeetCode submission - Candy (cpp)

--- problems/candy/solution.cpp
+++ problems/candy/solution.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> L(ratings.size(),0);
-        vector<int> R(ratings.size(),0);
-        for ( int i=1; i< ratings.size(); i++){
-            if (ratings[i] > ratings[i-1]) {
-                L[i] = L[i-1]+1;
-            }
+       
+        int N = ratings.size() ,ans = 0;
+        vector<int> cnt(N,1);
+        for (int i=1; i<N; i++)
+            if (ratings[i-1] < ratings[i])
+                cnt[i] = cnt[i-1] + 1;
+        ans = cnt[N-1];
+        for (int i=N-2; i>=0; i--){
+            if (ratings[i+1] < ratings[i])
+                cnt[i] = max(cnt[i+1] + 1, cnt[i]);
+            ans +=cnt[i];
         }
-        long ans=max(L[ratings.size()-1],R[ratings.size()-1]);
-        for (int i=ratings.size()-2; i>=0; i--){
-            if (ratings[i]>ratings[i+1])
-                R[i] = R[i+1] +1;
-            ans+=max(L[i],R[i]);
+        
+        
+        return ans;
+    }
+    int candy2(vector<int>& ratings) {
+        set<array<int,2>> set;
+        int N = ratings.size();
+        for (int i=0; i<N; i++)
+            set.insert({ratings[i] , i});
+        vector<int> cnt(N);
+        
+        for (auto & i : set){
+            int j = i[1] , can=0;
+            if (j>0 && ratings[j-1] < ratings[j])
+                can = cnt[j-1];
+            if (j<N-1 && ratings[j+1] < ratings[j])
+                can = max(can,cnt[j+1]);
+            cnt[j] = can +1;
         }
-        return ans + ratings.size();
+        return accumulate(begin(cnt),  end(cnt),0);
     }
 };
