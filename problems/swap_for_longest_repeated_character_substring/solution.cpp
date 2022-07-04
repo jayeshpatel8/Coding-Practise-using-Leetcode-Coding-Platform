de780d71e6d830c032f6be1731e22695bbbf383c
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-04 22:14:56 +0000

    Sync LeetCode submission - Swap For Longest Repeated Character Substring (cpp)

--- problems/swap_for_longest_repeated_character_substring/solution.cpp
+++ problems/swap_for_longest_repeated_character_substring/solution.cpp
@@ -2,6 +2,44 @@ class Solution {
 public:
     int maxRepOpt1(string text) {  
         int ans=0 ,N=text.size();
+        vector<int> f(26),f2(26) ;
+        for (int i=0;i<N; i++){
+            text[i]-='a';
+            f[text[i]]++;
+        }
+        int mask=0;
+        unordered_map<int,int>M;
+        for (int i=0; i<26; i++)
+            M[1<<i]=i;
+        for (int i=0,j=0,ch=0; i<N; i++)
+        {
+            char c = text[i],c2=text[j];
+            if (f2[c]++==0) ch++, mask |= 1<<c;
+            
+            while (ch > 2 ){
+                if (f2[text[j++]]-- == 1) ch--, mask ^= 1<<text[j-1];
+            }
+            
+            c=M[mask & -mask];
+            auto m2=mask & mask-1;
+            c2=M[m2 & -m2];
+            while(c!=c2 &&  (f2[c] > 1) && (f2[c2] > 1) ){
+                if (f2[text[j++]]-- == 1) ch--, mask ^= 1<<text[j-1];
+            }
+            c=text[i];
+            c2=text[j];
+
+            if(f2[c]==1){
+                ans= max(ans, f2[c2] + (f2[c2] < f[c2]) );    
+            }
+            else
+                ans= max(ans, f2[c]+ (f2[c] < f[c]) );                
+        }
+       
+        return ans;
+    }
+    int maxRepOpt12(string text) {  
+        int ans=0 ,N=text.size();
         vector<int> idx[26] ;
         for (int i=0;i<N; i++){
             idx[text[i]-'a'].push_back(i);
