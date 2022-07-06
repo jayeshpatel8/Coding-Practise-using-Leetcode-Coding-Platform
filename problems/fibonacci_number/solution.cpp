46685f9c5dad1059cd192ab2dc3bcd75ab6ec870
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-06 11:26:08 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    int f[31];
     int fib(int n) {
-        f[0]=0; f[1]=f[2]=1;
-        for (int i=3; i<=n; i++)
-            f[i] = f[i-1]+f[i-2];
-        return f[n];
+        int f1=0,f2=1 ;
+        for (int i=2; i<=n; i++){
+            int t = f2;
+            f2 +=f1;
+            f1=t;
+        }
+        return n==0? 0 : f2;
     }
 };
