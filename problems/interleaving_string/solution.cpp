37ff0eed533fcae5be076cc8569b2e695456ae48
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-07 09:23:36 +0000

    Sync LeetCode submission - Interleaving String (cpp)

--- problems/interleaving_string/solution.cpp
+++ problems/interleaving_string/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
-
 public:
-        int dp[101];
-    int m, n, N;
     bool isInterleave(string s1, string s2, string s3) {
-                
-        m = s1.length();
-        n = s2.length();
-        N = s3.length();
-        if (m+n != N) return false;
+        int l1 = s1.size(), l2 = s2.size(), l3=s3.size();
+        if (l1+l2 != l3) return false;
+        vector<bool> dp(l2+1);
         
-        for (int i=0; i<=m; i++){
-            for (int j=0; j<=n; j++){
-                if (i==0 &&  j==0) dp[0]=1;
-                else if(i==0){
-                    dp[j]= dp[j-1] && s2[j-1] ==s3[i+j-1];
-                } else if(j==0){
-                    dp[j]= dp[j] && s1[i-1] ==s3[i+j-1];
-                }else{
-                    dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) || (dp[j-1] && s2[j-1] == s3[i+j-1]);
-                }
+        for (int i=0; i<=l1; i++){
+            for (int j=0; j<=l2; j++){
+                if (i==0 && j==0)
+                    dp[j]=true;
+                else if (i==0 )
+                    dp[j] = dp[j-1] && s3[i+j-1] == s2[j-1];
+                else if (j==0 )
+                    dp[j] = dp[j] && s3[i+j-1] == s1[i-1];
+                else 
+                    dp[j] = (dp[j-1] && s3[i+j-1] == s2[j-1]) ||  (dp[j] && s3[i+j-1] == s1[i-1]);                
             }
         }
-        
-        return dp[n];
+        return dp[l2];
     }
-
 };
