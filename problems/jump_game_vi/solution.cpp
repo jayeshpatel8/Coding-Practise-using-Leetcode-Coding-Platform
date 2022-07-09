40e3c6e332473390fc5417e748b5dd7ce9398892
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-09 21:20:03 +0000

    Sync LeetCode submission - Jump Game VI (cpp)

--- problems/jump_game_vi/solution.cpp
+++ problems/jump_game_vi/solution.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n = nums.size();
-        int dp[n];
-        deque<int> dq;
-        dq.push_back(0);
-        dp[0]=nums[0];
-        for (int i=1; i<nums.size();i++)
-        {
-            if (dq.front() < i-k) dq.pop_front();
-            dp[i] = dp[dq.front()] + nums[i];
-            int cur = dp[i];
-            while (!dq.empty() && cur > dp[dq.back()]) dq.pop_back();
-            dq.push_back(i);
+        multiset<int> set ;
+        vector<int> dp(size(nums), INT_MIN);
+        set.insert(dp[0] = nums[0]);
+        int i=1,N = nums.size() ,ans = nums[0];
+        for (;i<N; i++){
+            if (i > k)
+                set.erase(set.find(dp[i-k-1]));
+            set.insert( dp[i] = *rbegin(set) + nums[i]);
         }
-        return dp[n-1];
+        
+        return dp[N-1];
+    }
+    
+    int maxResult2(vector<int>& nums, int k) {
+        priority_queue<array<int,2>> pq ;
+        pq.push({nums[0],k});
+        int i=1,N = nums.size();
+        for (;i<N-1; i++){
+            
+            while ( pq.top()[1] < i){
+                pq.pop();
+            }
+            int best= pq.top()[0];
+            
+            pq.push({nums[i] +best, i + k});
+            
+        }
+        while (  pq.top()[1] < i) 
+            pq.pop();
+       
+        return N >1 ? pq.top()[0] + nums[N-1] : pq.top()[0];
     }
 };
