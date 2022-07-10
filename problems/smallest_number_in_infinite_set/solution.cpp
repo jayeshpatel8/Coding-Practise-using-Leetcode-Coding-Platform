38da7923cf510575c8d7028ff893c22aed175161
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-10 02:49:54 +0000

    Sync LeetCode submission - Smallest Number in Infinite Set (cpp)

---
+++ problems/smallest_number_in_infinite_set/solution.cpp
@@ -0,0 +1,25 @@
+class SmallestInfiniteSet {
+public:
+    set<int> set;
+    SmallestInfiniteSet() {
+        for (int i=1; i<=1000; i++)
+            set.insert(i);
+    }
+    
+    int popSmallest() {
+        int ans = *begin(set);
+        set.erase(ans);
+        return ans;
+    }
+    
+    void addBack(int num) {
+        set.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
