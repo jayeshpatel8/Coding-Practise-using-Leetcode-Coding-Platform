7ef5b9132f912185862ff44992310a156b871ee2
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-17 22:34:14 +0000

    Sync LeetCode submission - K Inverse Pairs Array (cpp)

--- problems/k_inverse_pairs_array/solution.cpp
+++ problems/k_inverse_pairs_array/solution.cpp
@@ -1,38 +1,15 @@
+int dp[1001][1001] = { 1 };
 class Solution {
 public:
-    int dp[1001][1001];
-    int N;
     int kInversePairs(int n, int k) {
-
-        //N = n;
-        //memset(dp,0,sizeof(dp));
-        
-        for (int i=0; i<=n; i++){
-            dp[i][0]=1;
-            for (int j=1; j<=k; j++){
-                //if (j==0) dp[i][0]=1;
-                //else
-                {
-                    long sum =0, end = min(j,i-1);
-                    for (int k1=0; k1<=end; k1++){
-                        sum= (sum + dp[i-1][j-k1]);// % ((int)1e9 + 7 ) ;
-                    }
-                    dp[i][j] = (sum ) % ((int)1e9 + 7 ) ;
-                }
+        if (dp[n][k])
+            return dp[n][k];
+        for (int N = 1; N <= n; ++N)
+            for (int K = 0; K <= k; ++K) {
+                dp[N][K] = (dp[N - 1][K] + (K > 0 ? dp[N][K - 1] : 0)) % 1000000007;
+                if (K >= N)
+                    dp[N][K] = (1000000007 + dp[N][K] - dp[N - 1][K - N]) % 1000000007;
             }
-        }
-        return dp[n][k];                                
-        //return kInv(n,k);
-    }
-    int kInv(int n, int k){
-        if (n==0) return 0;
-        if (k==0) return 1;
-        if (dp[n][k] ) return dp[n][k];
-        int sum =0;
-        for (int i=0; i<=min(k,n-1); i++){
-            sum= (sum + kInv(n-1,k-i)) % ((int)1e9 + 7 ) ;
-        }
-        
-        return dp[n][k]=sum;
+        return dp[n][k];
     }
 };
