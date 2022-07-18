21cf4941d9a513238487bdb9145a9f03f49aa67d
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-18 21:25:08 +0000

    Sync LeetCode submission - Number of Submatrices That Sum to Target (cpp)

--- problems/number_of_submatrices_that_sum_to_target/solution.cpp
+++ problems/number_of_submatrices_that_sum_to_target/solution.cpp
@@ -1,31 +1,26 @@
-
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int M=matrix.size(), N=matrix[0].size();
-      
-        int ans=0;
-
-        for (int i=0; i<N; i++){
-            
-            vector<int> p_sum(M,0);    
-            for (int c=i; c<N; c++)
-            {                 
-                for (int r=0; r<M; r++){
-                    p_sum[r] +=matrix[r][c];                   
-                }                
-                
-                unordered_map<int,int> m;
-                m[0]=1;
-                int curr = 0;
-                for(auto k : p_sum){
-                    curr += k ;
-                    if (m.count(curr-target)){ans += m[curr-target];}
-                    m[curr]++;
+    int numSubmatrixSumTarget(vector<vector<int>>& mat, int target) {
+        int M =  mat.size() , N = mat[0].size();
+        for (int r =0; r<M; r++){
+            for (int c=1; c<N; c++)
+                mat[r][c] += mat[r][c-1] ;
+        }
+        int ans = 0;
+        for (int c1 =0; c1<N; c1++){
+            for (int c2=c1; c2<N; c2++){
+                unordered_map<int,int> map;
+                map[0]=1;
+                int sum = 0;
+                for (int r =0; r<M; r++){
+                    sum += mat[r][c2] - (c1>0 ? mat[r][c1-1] : 0);
+                    auto it  = map.find(sum-target);
+                    if (it != map.end())
+                        ans += it->second;
+                    map[sum]++;
                 }
             }
         }
- 
         return ans;
     }
 };
