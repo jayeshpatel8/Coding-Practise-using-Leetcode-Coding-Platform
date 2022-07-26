53332f63f831a523298409ad380142caf1588419
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-26 20:12:59 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

--- problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -9,13 +9,46 @@
  */
 class Solution {
 public:
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q) return root;
-        
-        TreeNode* lcm1 = lowestCommonAncestor(root->left,p,q);        
-        TreeNode* lcm2 = lowestCommonAncestor(root->right,p,q);
-        
-        if (lcm1 && lcm2) return root;
-        return lcm1 ? lcm1 : lcm2;
+        if (!root) return NULL;
+        if (root == p || root == q) return root;
+        auto l = lowestCommonAncestor(root->left,p,q);
+        auto r = lowestCommonAncestor(root->right,p,q);
+        if (l == NULL) return r;
+        else if (r==NULL) return l;
+        else return root;
+    }
+    unordered_set<TreeNode*> set;
+    TreeNode * ans = 0;
+    TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root,p);
+
+        dfs1(root,q);
+        return ans;
+    }
+    bool dfs(TreeNode * root, TreeNode * n){
+        if (!root) return false;
+        if (root == n) {
+            set.insert(n); 
+            return true;
+        }
+        if (dfs( root->left, n ) || dfs( root->right, n )){
+            set.insert(root);
+            return true;
+        }
+        return false;
+    }
+    bool dfs1(TreeNode * root, TreeNode * n){
+        if (!root) return false;
+        if (root == n) {
+            if (set.count(n) ) ans = n;
+            return true;
+        }
+        if (dfs1( root->left, n ) || dfs1( root->right, n )){
+            if (!ans && set.count(root) )  ans = root;
+            return true;
+        }
+        return false;
     }
 };
