d8bfd69799bdbb9098aead39b50be35e36c0c313
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-30 16:23:30 +0000

    Sync LeetCode submission - Word Subsets (cpp)

--- problems/word_subsets/solution.cpp
+++ problems/word_subsets/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
-        int freqB[26]={};
-        vector<string> ans;
-        for(auto &b : B){
-            int freqb[26]={};
-            for (auto c : b){
-                int i = c-'a';
-                freqb[i]++;
-                freqB[i] = max(freqB[i],freqb[i]);
-            }            
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        int w2[126]={};
+        for (auto &w : words2){
+            int w1[126]={};
+            for (auto &c : w)
+                w1[c]++;
+            for (int i='a';i<='z'; i++){
+                w2[i] = max(w2[i],w1[i]);
+            }
         }
-        for(auto &a : A){
-            int freqa[26]={};
-            for(auto c : a){
-                freqa[c-'a']++;
-            }
-            int i;
-            for (i=0; i<26; i++){
-                if(freqa[i]<freqB[i]) break;
-            }
-            if (i==26) ans.push_back(a);
+        vector<string> ans;
+        for (auto &w : words1){
+            int w1[126]={};
+            for (auto &c : w) w1[c]++;
+            int i='a';
+            for (;i<='z'; i++)
+                if (w1[i] < w2[i])break;
+            if (i >'z')
+             ans.push_back(w);
         }
         return ans;
     }
