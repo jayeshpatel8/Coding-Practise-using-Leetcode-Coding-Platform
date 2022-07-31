a35148d388347067f9e4d5d4ecec282c222d2c92
jayeshpatel8 <jnpatel81@gmail.com>
2022-07-31 12:01:08 +0000

    Sync LeetCode submission - Range Sum Query - Mutable (cpp)

--- problems/range_sum_query_-_mutable/solution.cpp
+++ problems/range_sum_query_-_mutable/solution.cpp
@@ -1,36 +1,31 @@
 class NumArray {
 public:
-    vector<int> bt,num;
-        int n;
-    //int num[101];
-    NumArray(vector<int>& nums) {
-        n=nums.size()+1;
-        bt.resize(n,0);
-        num.resize(n,0);
-        
-        for (int i=0; i<nums.size();i++){ update(i,nums[i]);
-                                         
-                                        }
-        
-    }
-    
-    void update(int index, int val) {
-        //index ++;
-        int nval = val -num[index];
-        num[index]=val;
-        for (int i=++index; i<n; i+=i & -i){
-            bt[i] += nval; 
+    static constexpr int N = 30001;
+    int BIT[N]={};
+    void add(int i, int val){
+        for (;i<N;i+= i & -i){
+            BIT[i]+=val;
         }
     }
-    int pointsum(int i){
+    int get(int i){
         int sum=0;
-        i++;
-        for (; i>0;i -=i & -i) sum +=bt[i];
+        for (; i>0; i-= i & -i)
+            sum += BIT[i];
         return sum;
     }
-    int sumRange(int l, int r) {
-     
-        return pointsum(r) - pointsum(l-1);
+    int v[N]={};
+    NumArray(vector<int>& nums) {
+        for (int i=0; i<nums.size(); i++)
+            v[i] = nums[i], add(i+1,nums[i]);
+    }
+    
+    void update(int i, int val) {
+        add(i+1,val - v[i]);
+        v[i] =val;
+    }
+    
+    int sumRange(int left, int right) {        
+        return get(right+1) - get(left);
     }
 };
 
