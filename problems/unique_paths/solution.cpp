90cd98b8b7d2144b94fc429da9ed14053aa44007
jayeshpatel8 <jnpatel81@gmail.com>
2022-08-01 14:20:32 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- problems/unique_paths/solution.cpp
+++ problems/unique_paths/solution.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        
+        /*
+            total possible step = m + n - 2;
+            out of this we can take right on n-1 step;
+            nCr = (m+n-2) choose (n-1)
+        */
+        if (m < n) swap(m,n);
+        long long ans = 1;
+        for (int i=1, j =n; j<m+n-1; i++,j++){
+            ans *= j;
+            ans /= i;
+        }
+        return ans;
+    }
+
+    int uniquePaths2(int m, int n) {
+        int dp[101][101]={};
         dp[0][1]=1;
         for (int i=1; i<=m; i++){
             for (int j=1; j<=n; j++){
-                dp[i][j] = dp[i-1][j] +dp[i][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m][n];
