7b17a831a4c4ad566ac188c13fffd57f103ba59d
jayeshpatel8 <jnpatel81@gmail.com>
2022-08-02 17:57:52 +0000

    Sync LeetCode submission - Kth Smallest Element in a Sorted Matrix (cpp)

--- problems/kth_smallest_element_in_a_sorted_matrix/solution.cpp
+++ problems/kth_smallest_element_in_a_sorted_matrix/solution.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    int vis[301][301];
-    int kthSmallest(vector<vector<int>>& matrix, int K) {
+    int M,N;
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        M=matrix.size(), N=matrix[0].size();
         
-        int N = matrix.size();
-        
-        int ans ;
-        if (K >= N*N/2){
-            // end                        
-            priority_queue<pair<int, pair<int,int>>> pq;
-            pq.push({matrix[N-1][N-1],{N-1,N-1}});
-            K = N*N-K;
-         
-            while(K--){
-                pair<int,int> p = pq.top().second;pq.pop();
-                int i = p.first, j= p.second;
-                if (i!=0 && vis[i-1][j]==0)
-                    pq.push({matrix[i-1][j],{i-1,j}} ), vis[i-1][j]=1;
-                if (j!=0 && vis[i][j-1]==0)
-                    pq.push({matrix[i][j-1],{i,j-1} }), vis[i][j-1]=1;
-               
-                
+        int l=matrix[0][0], r = matrix[M-1][N-1];
+        while (l<r){
+            int mid = (l +r)>>1;
+            if (countlessAndEqual(matrix,mid) >= k){
+                r = mid;
             }
-        
-            ans = pq.top().first;
+            else l = mid+1;
         }
-        else {
-            // start
-            priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
-            pq.push({matrix[0][0],{0,0}});
-            
-            while(--K){
-                pair<int,int> p = pq.top().second;pq.pop();
-                int i = p.first, j= p.second;
-                if (i!=N-1 && vis[i+1][j]==0)
-                    pq.push({matrix[i+1][j],{i+1,j} }),vis[i+1][j]=1;
-                if (j!=N-1 && vis[i][j+1]==0)
-                    pq.push({matrix[i][j+1],{i,j+1} }),vis[i][j+1]=1;
-                
-            }
-            ans = pq.top().first;
+        return r;
+    }
+    int countlessAndEqual(vector<vector<int>>& matrix, int k){
+        int ans = 0;
+        for (int r=0; r<M; r++){
+            int c = N-1; 
+            while (c>=0 && matrix[r][c] > k) c--;
+            ans += c + 1;
         }
         return ans;
     }
