a3a92373c180950a3fae70de2f2dac2b2ded2d5e
jayeshpatel8 <jnpatel81@gmail.com>
2022-08-18 16:55:26 +0000

    Sync LeetCode submission - Reduce Array Size to The Half (cpp)

--- problems/reduce_array_size_to_the_half/solution.cpp
+++ problems/reduce_array_size_to_the_half/solution.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    //int map[100001];
     int minSetSize(vector<int>& arr) {
-        int m = *max_element(begin(arr),end(arr)), mf=0;
-        int map[m+1],freq[arr.size()+1];
-        memset(map,0,sizeof(map));
-        memset(freq,0,sizeof(freq));
-        for (auto i : arr) {
-            map[i]++;
-            if (map[i]==1){
-                freq[1]++;
-            }else{
-                freq[map[i]-1]--;
-                freq[map[i]]++;
-            }
-            mf = max(mf,map[i]);
-        }        
-        int n = arr.size()/2,cnt=0;
-        for (int i=mf; i>1;i--)
-        {
-            while(freq[i]){
-                int  p = freq[i]* i;
-                if ( p <=n){
-                    n -= p;
-                    cnt +=freq[i];
-                    freq[i]=0;
-                }else{       
-                    int d = n/i + (n%i != 0);
-                    n -=i * (d); 
-                    return cnt+ d;
-                }
-                if (n <=0) return cnt;
-            }   
+        unordered_map<int,int> map;
+        int half = arr.size()/2;
+        for (auto i : arr) map[i]++;
+        vector<int> cnt;
+        for (auto &[k,v] : map) cnt.push_back(v);
+        int size = 0,ct=0;
+        sort(begin(cnt),end(cnt));
+        for (auto it = rbegin(cnt); it!= rend(cnt); it++){
+            size += *it;
+            ++ct;
+            if (size >= half) return ct;
+            
         }
-    
-        return cnt+n;
+        return ct;
     }
 };
