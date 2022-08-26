442305a9ef7b4ced857e6fffbdc95c4fa954ef57
jayeshpatel8 <jnpatel81@gmail.com>
2022-08-26 12:19:19 +0000

    Sync LeetCode submission - Reordered Power of 2 (cpp)

--- problems/reordered_power_of_2/solution.cpp
+++ problems/reordered_power_of_2/solution.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    bool reorderedPowerOf2(int N) {
-        int digit[10]={};
-        int len=0;
-        while (N){
-            digit[N%10]++;
-            N/=10;
-            len++;
+    bool reorderedPowerOf2(int n) {
+        int num = 1, len=0;
+        vector<int> dig(10),dig2(10);
+        while (n){
+            dig[(n%10)]++;
+            n/=10;
+            ++len;
         }
-        int lower_bound=0,upper_bound=0;
-        for (int i=0; i<10; i++){
-            int j = digit[i];
-            while(j--){
-                lower_bound =lower_bound*10+i;
+        int l=1;
+        dig2[1]++;
+
+        while (l<= len){
+            if (l == len){
+                if (dig2 == dig) return true;
             }
+            num <<=1;
+            vector<int> d(10);
+            n=num;
+            l=0;
+            while (n){
+                d[n%10]++;
+                n/=10;
+                ++l;
+            }
+            dig2=d;
         }
-        for (int i=9; i>=0; i--){
-            int j = digit[i];
-            while(j--){
-                upper_bound =upper_bound*10+i;
-            }
-        }  
-        int n=1;
-        
-        for (int i=1; i< 31; i++,n<<=1){
-            if (lower_bound <=n && upper_bound>=n){
-                int digit2[10]={0};
-                int num=n;
-                
-                while(num){
-                    digit2[num%10]++;
-                    num/=10;
-                }
-                int i1=0;
-                for (i1=0;  i1<10; i1++){
-                    if(digit[i1]!= digit2[i1]) break;
-                }
-                if(i1==10) return true;
-            }
-        }
-        return false;
+        return false ;
     }
 };
