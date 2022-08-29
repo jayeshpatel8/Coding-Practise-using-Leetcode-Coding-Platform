936f760e1f6727464f4db04d1376164c3b9fc30f
jayeshpatel8 <jnpatel81@gmail.com>
2022-08-29 15:40:07 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    int m,n;
+    int m,n,ans = 0, dirs[5] = {-1,0,1,0,-1};
     int numIslands(vector<vector<char>>& grid) {
-        m= grid.size(), n=grid[0].size();
-        
-        
-         int island=0;
-         for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (grid[i][j]=='1'){
-                    DFS(grid,  i, j);
-                    island++;
-                }
-            }
-         }
-         return island;
+        m = grid.size(), n =grid[0].size();
+        for (int i=0; i<m; i++){
+            for (int j= 0; j<n; j++)
+                if (grid[i][j]=='1')
+                    ++ans, dfs(grid, i, j);
+        }
+        return ans;
     }
-    void DFS (vector<vector<char>>& grid,   int i , int j){
-        
-        if (i<0 || j <0 || i>=m || j >=n  || '0'==grid[i][j])return;
+    void dfs (vector<vector<char>>& grid, int i, int j) {        
         
         grid[i][j]='0';
-        
-        {
-            DFS(grid,  i+1, j);
-            DFS(grid,  i,j+1);
-            DFS(grid,  i-1, j);
-            DFS(grid,  i, j-1);
-        }
+        for (int d = 0; d<4; d++){
+            int i1=i+dirs[d], j1 = j+dirs[d+1];
+            if (i1<0 || j1<0|| i1>=m || j1>=n || '0' == grid[i1][j1]) continue;
+                dfs(grid,i1,j1);
+        }        
     }
 };
