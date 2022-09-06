cd5ec9f37dad159f1f03a363aeb1bd366b92795d
jayeshpatel8 <jnpatel81@gmail.com>
2022-09-06 10:22:41 +0000

    Sync LeetCode submission - Binary Tree Pruning (cpp)

--- problems/binary_tree_pruning/solution.cpp
+++ problems/binary_tree_pruning/solution.cpp
@@ -12,13 +12,22 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        if (!root) return root;
+        if (!root) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        return root->val || root->left || root->right ? root: NULL;
+    }
+    TreeNode* pruneTree2(TreeNode* root) {
+        if (!dfs(root)) return 0;
+        dfs(root);
+        return root;
+    }
+    int dfs(TreeNode * root){
         
-        root->left  =  pruneTree(root->left);
-        root->right =  pruneTree(root->right);
-        
-        if(root->val || root->left || root->right) 
-            return root;
-        return NULL;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        if (!l) root->left = NULL;
+        if (!r) root->right = NULL;
+        return root->val | l | r;
     }
 };
