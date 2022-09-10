0330560560c23a5b1b71600b6b94747c842d9e1e
jayeshpatel8 <jnpatel81@gmail.com>
2022-09-10 20:04:16 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

--- problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
+    int dp[2][1001][101]={}, N ;
+    
     int maxProfit(int k, vector<int>& prices) {
-        const int  len =prices.size();
-        if (len < 2) return 0;
-        if (k>= len/2)
-        {
-            int ret=0;
-			for (auto i = 1; i < len; i++)
-			{
-                if (prices[i] > prices[i - 1]) ret+=prices[i] - prices[i - 1];
-			}
-            return ret;
-		}            
+        N = prices.size();
+        return dfs(prices,0,1,k);
+    }
+    int dfs(vector<int> & prices, int i,bool buy,  int k){
+        if (i ==N ||k ==0) return 0;
+        if (dp[buy][i][k]) return dp[buy][i][k];
+        int             ans = dfs(prices,i+1,buy,k);
+        ans= max(ans, (buy ? -prices[i] : prices[i])  + dfs(prices,i+1,!buy,k- !buy));     
         
-        
-        vector<vector<int>> dp( k+1, vector<int>(2));
-        for (int  i=0; i<=k; i++) {
-        dp[i][1]=INT_MIN;
-         dp[i][0]=0;
-        }
-        
-        for (int day=0; day<len; day++)
-        {
-            //dp[j][1] = INT_MIN; dp[j][0]=0;
-           
-            for (int j=1; j<=k;j++)
-            {
-                dp[j][0] = std::max(dp[j][0], prices[day] + dp[j][1]);    
-                dp[j][1] = std::max(dp[j][1], dp[j-1][0]-prices[day]);
-            }
-            
-        }
-        return dp[k][0];
+        return dp[buy][i][k]  = ans;
     }
 };
