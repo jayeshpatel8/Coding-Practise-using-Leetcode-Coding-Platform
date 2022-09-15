3c8ce5e3ccb75c848aa3286e0338d66c48cec381
jayeshpatel8 <jnpatel81@gmail.com>
2022-09-15 13:01:34 +0000

    Sync LeetCode submission - Find Original Array From Doubled Array (cpp)

--- problems/find_original_array_from_doubled_array/solution.cpp
+++ problems/find_original_array_from_doubled_array/solution.cpp
@@ -1,23 +1,69 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        if(changed.size() & 1) return {};
+        int sz  =changed.size();
+        if(sz & 1) return {};
         sort(begin(changed), end(changed));
-        int j=changed.size()-1;
+        int j=sz-1;
         vector<int> ans;
         int freq[100001]={};        
         while(j>=0){
-            int id = changed[j] * 2;
+            int i = changed[j], id = i * 2;
             if ( id<= 100001 && freq[id]){ 
-                ans.push_back(changed[j]);
+                ans.push_back(i);
                 freq[id]--;
             }
             else
-                freq[changed[j]]++;
+                freq[i]++;
             j--;
         }
-        if (ans.size() * 2 != changed.size()) return {};
-        reverse(ans.begin(),ans.end());
+        if (ans.size() * 2 != sz) return {};
+        
+        return ans;
+    }  
+    vector<int> findOriginalArray1(vector<int>& changed) {
+        vector<int> ans;
+        map<int,int> A;
+        for (auto  i : changed) A[i]++;
+        for (auto it = begin(A); it != end(A); it++){
+            if (it->first == 0){
+                if (it->second & 1) return {};
+                int cnt = it->second/2;
+                while (cnt-- > 0) ans.push_back(0);                
+            }
+            else if (it->second > 0){
+                auto it2 = A.lower_bound(it->first * 2);
+                if (it2 == end(A) || it2->first > it->first*2 ) {
+                    return {};
+                }
+                int cnt = it->second ,n = it->first;
+                it2->second -= cnt;
+               
+                if (it2->second < 0) return {};
+                while (cnt-- > 0) ans.push_back(n);                                
+            }
+        }
+        return ans;
+    }
+    vector<int> findOriginalArray2(vector<int>& changed) {
+        vector<int> ans;
+        int f[100001]={};
+        for (auto i : changed) f[i]++;
+        if (f[0]){
+          if (f[0] & 1) return {};
+            f[0]/=2;
+            while (f[0]-- > 0)
+                    ans.push_back(0);
+        } 
+        for (int i=100000; i>0; i--){
+            if (f[i]){
+                if (i&1) return {};
+                f[i/2] -=f[i];
+                if (f[i/2] < 0) return {};                
+                while (f[i]-- > 0)
+                    ans.push_back(i/2);
+            }
+        }
         return ans;
     }
 };
