a90d75bc71ef2e3a54cf272ac7435ccfdb2edade
jayeshpatel8 <jnpatel81@gmail.com>
2022-09-16 22:24:47 +0000

    Sync LeetCode submission - Falling Squares (cpp)

--- problems/falling_squares/solution.cpp
+++ problems/falling_squares/solution.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
     int N = 1024*1024;
-    vector<int> height,lazy, ans;
-     void push_down(int i) {
-        if (lazy[i]) {
-            lazy[i*2] = lazy[i*2+1] = lazy[i];
-            height[i*2] = height[i*2+1] = lazy[i];
+    vector<int> height,lazy, ans;    
+    #define lc  (i<<1 |1)
+    #define rc  ((i<<1) + 2)
+    #define mid  (lo+((hi-lo)>>1))    
+     void push_down(int i, int lo, int hi) {
+         
+        if (lazy[i]) {  
+            height[i] =  lazy[i];
+            if (hi !=lo)
+                lazy[lc] = lazy[rc] = lazy[i];
             lazy[i] = 0;
         }
-      }
-    int query(int l, int r, int idx, int lx, int rx){
-        if (l<=lx && rx<= r) return height[idx];
-        int mid = lx +(rx-lx)/2, res = 0;
-        push_down(idx);
-        if (l <mid) res=query(l,r,idx*2 , lx,mid);
-        if (r>mid) res=max(res, query(l,r,2*idx +1, mid,rx));
-        return res;
-    }
-    void upd(int l, int r,int v, int idx, int lx, int rx){
-      //  if (l>rx || r< lx) return ;
-        if (l<=lx && rx<= r){
-            height[idx] = v; 
-            lazy[idx] = v;return;    
-        }
+      }   
 
-        int mid = lx +(rx-lx)/2,res= 0 ;
-        push_down(idx);
-        if (l <mid) upd(l,r, v, idx*2 , lx,mid);
-        if (r > mid) upd(l,r, v, idx*2 +1, mid,rx);
-        height[idx]= max(height[idx*2], height[idx*2 + 1]);
+    int query( int l, int r, int i, int lo, int hi){
+        
+        if (hi < l || lo > r || lo > hi) return 0;
+        push_down(i,lo,hi);
+        if (l<= lo && hi<=r) return height[i];
+         return max(query(l,r,lc,lo,mid),query(l,r,rc,mid+1,hi)); 
     }
+    void upd( int l, int r,int val, int i, int lo, int hi){
+
+       
+        if (hi < l || lo > r || lo > hi) return ;
+        push_down(i,lo,hi);
+        if (l<= lo && hi<=r) {
+            lazy[i] = height[i] = val;
+           
+            if (lo!=hi)
+                lazy[lc] = lazy[rc] =val;
+            return ;
+        }
+       
+        upd(l,r,val,lc,lo,mid); 
+        upd(l,r,val,rc,mid+1,hi);
+        height[i] = max(height[lc],height[rc]);
+        return;
+    }    
     vector<int> fallingSquares(vector<vector<int>>& positions) {
         vector<int> a;
         for (auto& p : positions) {
           a.push_back(p[0]);
-          a.push_back(p[0]+p[1]);
+          a.push_back(p[0]+p[1]-1);
         }
         sort(a.begin(), a.end());
         N = unique(a.begin(), a.end()) - a.begin();
@@ -42,16 +52,17 @@ public:
         //while (N <= 100000000) N *=2;
         height.resize(4 * N);
         lazy.resize(4 * N);
-      
-      
+
+      int m=0;
         for (auto &p : positions){
-              int l = lower_bound(a.begin(), a.end(), p[0]) - a.begin();
-              int r = lower_bound(a.begin(), a.end(), p[0]+p[1]) - a.begin();            
-            int h = query(l, r,1,0,N);
-            
-            upd(l, r,h+p[1], 1,0,N);
-            ans.push_back( query(0, N,1,0,N));
+            int l = lower_bound(a.begin(), a.end(), p[0]) - a.begin();
+            int r = lower_bound(a.begin(), a.end(), p[0]+p[1]-1) - a.begin();            
+            int h = query(l, r,0,0,N-1);
+            upd(l, r,h+p[1], 0,0,N-1);
+            m=max(m,h + p[1]);
+            ans.push_back( m);
         }
-        return ans;
+
+        return ans;        
     }
 };
