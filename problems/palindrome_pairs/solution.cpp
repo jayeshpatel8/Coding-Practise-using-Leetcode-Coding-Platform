4b251ab00743267619f64a2a13d71c654667276c
jayeshpatel8 <jnpatel81@gmail.com>
2022-09-17 16:23:11 +0000

    Sync LeetCode submission - Palindrome Pairs (cpp)

--- problems/palindrome_pairs/solution.cpp
+++ problems/palindrome_pairs/solution.cpp
@@ -1,5 +1,72 @@
+struct TrieNode
+{
+    TrieNode *child[26]={};
+    int curIndex=-1;
+    vector<int> wordIndex;
+};
+class Solution
+{
+    bool isPalindrome(string &s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+    TrieNode *root;
+    void insert(string &s, int index)
+    {
+        TrieNode *cur = root;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            int c = s[i] - 'a';
+            if (cur->child[c] == nullptr)
+                cur->child[c] = new TrieNode();
+            if (isPalindrome(s, 0, i))
+                cur->wordIndex.push_back(index);
+            cur = cur->child[c];
+        }
+        cur->wordIndex.push_back(index);
+        cur->curIndex = index;
+    }
+
+public:
+    vector<vector<int>> palindromePairs(vector<string> &words)
+    {
+        root = new TrieNode();
+        for (int i = 0; i < words.size(); i++)
+            insert(words[i], i);
+        vector<vector<int>> ans;
+        for (int i = 0; i < words.size(); i++)
+        {
+            TrieNode *cur = root;
+            string &s = words[i];
+            for (int j = 0; j < s.size(); j++)
+            {
+                if (cur->curIndex != -1 && cur->curIndex != i && isPalindrome(s, j, s.size() - 1))
+                    ans.push_back({i, cur->curIndex});
+                cur = cur->child[s[j] - 'a'];
+                if (cur == nullptr)
+                    break;
+            }
+            if (cur == nullptr)
+                continue;
+            for (int j : cur->wordIndex)
+            {
+                if (i == j)
+                    continue;
+                ans.push_back({i, j});
+            }
+        }
+        return ans;
+    }
+};
+#if 0
 class Solution {
 public:
+    unordered_map<string,bool> pal;
     vector<vector<int>> palindromePairs(vector<string>& words) {
         unordered_map<string,int> rev;
         for (int i=0; i< words.size(); i++){
@@ -23,14 +90,12 @@ public:
         }
         return ans;
     }
-bool isPal(string &st)
-{
-    int s = 0, e = st.size()-1;
-	while(s<e)
-	{
-		if(st[s++]!=st[e--])
-			return false;
+    bool isPal(string &s){
+        if (pal.count(s)) return pal[s];
+        int i=0, j = s.size()-1;
+        while (i<j)
+            if (s[i++] != s[j--]) return pal[s] = false;
+        return pal[s] = true;
     }
-	return true;
-}
 };
+#endif
