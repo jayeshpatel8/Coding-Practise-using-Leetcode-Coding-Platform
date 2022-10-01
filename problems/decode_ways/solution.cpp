5271ec91cfbd71d2f864a92accde56e67f93c301
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-01 08:30:10 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- problems/decode_ways/solution.cpp
+++ problems/decode_ways/solution.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+int dp[101] = {};
     int numDecodings(string s) {
-        int  N = s.size();
-        int dp[N][2];
-        dp[0][0] = s[0] != '0'; dp[0][1]=0;
-        
-        for(int i=1; i<N; i++){
-            dp[i][0]=dp[i][1]=0;
-            if(s[i] != '0')    
-                dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            if (s[i-1] == '1' || ( s[i-1] == '2' && s[i]<='6' ))
-                dp[i][1] = dp[i-1][0];
+        return dfs(s,0);
+    }
+    int dfs(string &s, int i){
+        if (i >= s.size()) return 1;
+        if (dp[i]) return dp[i];
+        int ans = 0;
+        if (s[i] != '0'){
+          ans = dfs(s,i+1);
+          if (i +1 < s.size() ){
+              int num = (s[i]-'0') * 10 + (s[i+1]-'0') ;
+              if (num <= 26)
+                ans += dfs(s,i+2);
+          }
         }
-        return dp[N-1][0] + dp[N-1][1];
+        return dp[i] = ans;
     }
 };
