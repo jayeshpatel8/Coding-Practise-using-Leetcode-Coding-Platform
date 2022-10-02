217e39861ad19dd695769b9733c994c132abe34e
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-02 22:11:51 +0000

    Sync LeetCode submission - Number of Dice Rolls With Target Sum (cpp)

--- problems/number_of_dice_rolls_with_target_sum/solution.cpp
+++ problems/number_of_dice_rolls_with_target_sum/solution.cpp
@@ -1,43 +1,29 @@
 class Solution {
+    int dp[31][1001] = {}, mod = 1e9 + 7;
 public:
-     int mod = (int)1e9 +7;
-    int dp[31][1001];
-    int numRollsToTarget(int d, int f, int target) {
-      memset(dp,-1,sizeof(dp));
-        return roll(d,f,target);
-    }
-    int roll(int d, int f, int target){
-        if(d == 0 || target < 0)
-            return target == 0   ;  
-        
-        if (dp[d][target] != -1) return dp[d][target];
-        int ans=0;
-        for (int face=1; face <=f; face++){
-            if ( target >= face)
-                ans =(ans+ roll(d-1,f,target-face))%mod;
+    int numRollsToTarget(int n, int k, int target) {
+        dp[0][0]=1;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=k; j++){
+                for (int t=j; t<=target; t++)
+                    dp[i][t] = (dp[i][t] + dp[i-1][t - j]) %mod;
+            }
         }
-        return dp[d][target] = ans ;
-        
+        return dp[n][target];
     }
-    /**
-    int numRollsToTarget(int d, int f, int target) {
-        int mod = (int)1e9 +7;
-        vector<int> ways(target+1);
-        ways[0]=1;        
-        for (int rep = 1; rep <= d; ++rep) {
-           vector<int> new_ways(target+1);
-           for (int already = 0; already <= target; ++already) {
-               for (int pipe = 1; pipe <= f; ++pipe) {
-                   if (already - pipe >= 0) {
-                       new_ways[already] += ways[already - pipe];
-                       new_ways[already] %= mod;
-                   }
-               }
-           }
-           ways = new_ways;
+    int numRollsToTarget2(int n, int k, int target) {    
+        memset(dp,-1,sizeof(dp));
+        return dfs(n,k,target);
+    }
+    int dfs(int n, int k, int target) {
+        if (n == 0 )
+            return target == 0;
+        if (target <= 0) return 0;
+        if (dp[n][target] != -1 ) return dp[n][target];
+        long ans = 0;
+        for (int i=1; i<=k; i++){
+            ans = (ans + dfs(n-1,k,target- i)) % mod;
         }
-        return ways[target];
-    }
-    */
+        return dp[n][target] = ans;
+    }    
 };
-
