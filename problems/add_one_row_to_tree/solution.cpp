5aa559d94780382d72543708b3e7e51f9178cd13
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-05 20:42:06 +0000

    Sync LeetCode submission - Add One Row to Tree (cpp)

--- problems/add_one_row_to_tree/solution.cpp
+++ problems/add_one_row_to_tree/solution.cpp
@@ -11,58 +11,41 @@
  */
 class Solution {
 public:
-    TreeNode* addRow(TreeNode* root, int v, int d) {
-        if(!root) return root;
-        if(d==2){
-                    
-            TreeNode *n1 = new TreeNode(v);
-            TreeNode *n2 = new TreeNode(v);
-
-            n1->left= root->left;
-            n2->right= root->right;
-            root->left = n1;
-            root->right = n2;            
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if (depth == 1){
+            TreeNode *r  = new TreeNode(val);
+            r->left = root;
+            return r;
         }
-        else {
-            addRow(root->left, v, d-1);
-            addRow(root->right, v, d-1);
-        }
-            
-        return root;
-    }
-    TreeNode* addOneRow(TreeNode* root, int v, int d) {
-       if (d==1){
-           TreeNode * t = new TreeNode(v);
-           t->left = root;
-           return t;
-       }   
-        
-        return  addRow(root, v, d);
         queue<TreeNode*> q;
+        int d=2;
         q.push(root);
-        while(!q.empty()){
-            d--;
-            int sz = q.size();
-            while (sz-->0){
-                if(d==1){
-                    
-                    TreeNode *t = q.front();q.pop();    
-                    TreeNode *n1 = new TreeNode(v);
-                    TreeNode *n2 = new TreeNode(v);
-                    
-                    n1->left= t->left;
-                    n2->right= t->right;
-                    t->left = n1;
-                    t->right = n2;
+        while (d <=depth){
+            if (d == depth){
+                int sz = q.size();
+                while (sz-->0){
+                    auto * n = q.front(); q.pop();
+                    auto * l = new TreeNode (val);
+                    auto * r = new TreeNode (val);
+                    l->left = n->left;
+                    r->right = n->right;
+                    n->left = l;
+                    n->right = r;
                 }
-                else{                
-                    TreeNode *t = q.front();q.pop();    
-                    if(t->right) q.push(t->right);
-                    if(t->left) q.push(t->left);
+                
+            }
+            else{
+                int sz = q.size();
+                while (sz-->0){
+                    auto n = q.front(); q.pop();
+                    if (n->left)
+                        q.push(n->left);
+                    if (n->right)
+                        q.push(n->right);
                 }
-            }            
+            }
+            d++;
         }
-
         return root;
     }
 };
