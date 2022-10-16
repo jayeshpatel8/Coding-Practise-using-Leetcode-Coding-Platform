18560ce765680bcda0e6643da34bb80be864602d
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-16 19:55:50 +0000

    Sync LeetCode submission - Minimum Difficulty of a Job Schedule (cpp)

--- problems/minimum_difficulty_of_a_job_schedule/solution.cpp
+++ problems/minimum_difficulty_of_a_job_schedule/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dp[1001][11], N;
-    int minDifficulty(vector<int>& jd, int d) {
+int dp[301][11],N;
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
         memset(dp,-1,sizeof(dp));
-        N = jd.size();
-        dfs(jd,0,d);
-        return dp[0][d] >=100000 ? -1 :  dp[0][d];
+        N = jobDifficulty.size();
+        int ans = dfs(jobDifficulty,0,d) ;
+        return ans == 1e5 ? -1 : ans;
     }
-    int dfs(vector<int>& jd, int i, int d){
-        if (i>=N || d==0){
-            if (d==0 && i==N) return 0;
-            return 100000;
+    int dfs(vector<int> & job,int i, int d){
+        if (i==N || d==0){
+            if (i==N && d==0) return 0;
+            return 1e5;
         }
         if (dp[i][d] != -1) return dp[i][d];
-        int ans = INT_MAX, ma= 0;;
-        for ( int j=i; j<N-d+1; j++){
-            ma= max(ma, jd[j]);
-            ans= min(ans, ma+dfs(jd,j+1,d-1));
+        int ans = 1e5 , ma=0;
+        for (int j = i; j<N-d+1; j++){
+            ma = max(ma, job[j]);
+            ans = min(ans, ma + dfs(job, j+1,d-1));
         }
-        return dp[i][d]  =ans;
+        return dp[i][d] = ans;
     }
 };
