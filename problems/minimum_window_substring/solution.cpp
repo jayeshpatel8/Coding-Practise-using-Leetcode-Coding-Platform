f57b4d3958f21f055256ec882a91d97aaf509adc
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-22 22:16:57 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- problems/minimum_window_substring/solution.cpp
+++ problems/minimum_window_substring/solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int sz = s.size(), st = t.size();
+         int sz = s.size(), st = t.size();
         if (sz< st) return  "";
-
         int map[128]={}, start=-1,d=INT_MAX;
         for (auto c : t)  map[c]++;
-        
-        for ( int r=0,l=0, cnt = st; r < sz; r++){            
-            char c = s[r];
+
+        for (int l=0,r=0,cnt=st;  r<sz; r++){
+            auto c = s[r];
             map[c]--;
-            if (map[c]>=0) cnt--;
-            while (cnt == 0 && l<=r){
-                if(d > r-l+1) d = r-l+1, start = l;
+            if (map[c] >= 0 ) cnt--;
+            while (cnt==0 && l<=r){
+                if (d > r-l+1){ d = r-l+1, start = l;}
                 map[s[l]]++;
                 if (map[s[l++]] > 0) cnt++;
             }
         }
-        return start==-1 ? "" : s.substr(start,d);
-    }
+        return start==-1? "" : s.substr(start,d);
+    } 
 };
