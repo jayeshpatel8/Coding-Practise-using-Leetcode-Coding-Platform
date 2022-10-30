e1d392ee699a162b9fb5cdf68ba23213c4b73fe3
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-30 11:23:26 +0000

    Sync LeetCode submission - Earliest Possible Day of Full Bloom (cpp)

--- problems/earliest_possible_day_of_full_bloom/solution.cpp
+++ problems/earliest_possible_day_of_full_bloom/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int earliestFullBloom(vector<int>& p, vector<int>& g) {
-        vector<pair<int,int>> gp(p.size());
-        for (int i=0; i<p.size(); i++)
-            gp[i] = {g[i],p[i]};
-        sort(begin(gp),end(gp),[](auto & a, auto &b){return a.first > b.first; });
-        int d=-1, ans=0;
-        for (auto & [gt,pt] : gp){
-            d+=pt;
-            ans = max(ans, d+gt+1);
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+        vector<int> trees(growTime.size());
+        iota(begin(trees), end(trees), 0);
+        sort (begin(trees), end(trees), [&](auto & a, auto &b){
+            return growTime[a] > growTime[b];
+        });
+        int plantdays=0, ans = 0;
+        for ( auto & i : trees){
+            plantdays +=plantTime[i];
+            ans = max(ans , plantdays + growTime[i]);
         }
         return ans;
     }
