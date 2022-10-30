4fc66e2ec4976d61a5f3078aa46891ad272b45bf
jayeshpatel8 <jnpatel81@gmail.com>
2022-10-30 12:52:52 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (cpp)

--- problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.cpp
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.cpp
@@ -4,31 +4,29 @@ public:
         int M = grid.size(), N =grid[0].size();
         if (k >= M+N-2) return M+N-2;
         queue<array<int,3>> q;
-        int vis[M+1][N+1][k+2];
+        bool vis[M+1][N+1][k+2];
         memset(vis,0,sizeof(vis));
         q.push({0,0,k});
         int d=-1;        
         int r[5]={-1,0,1,0,-1};
         vis[0][0][k]=1;
-        while(!q.empty()){
-            int sz = q.size();
+        
+        int dirs[5] = {-1,0,1,0,-1};
+        while (!q.empty()){
             d++;
-            while(sz--){
-                auto i  = q.front(); 
-                int x = i[0], y =i[1], k1=i[2];
-
-                if (x == M-1 && y ==N-1) return d;
-
-                for (int j=0; j<4; j++ )
-                {
-                    int x1 = x+r[j], y1= y+r[j+1],k2;
-                    if (x1 < 0 || y1< 0 || x1>=M || y1>=N ) continue;
-                    k2 = k1-grid[x1][y1];
-                    if (k2 >=0 && !vis[x1][y1][k2] )     {                     
-                            q.push({x1,y1,k2}),   vis[x1][y1][k2]=1;
-                    }
+            int sz = q.size();
+            
+            while (sz-- > 0){
+                auto [r,c,k1] = q.front(); q.pop();
+              
+                if (r == M-1 && c == N-1) return d;
+                for (int i=0; i < 4; i++){
+                    int r1 = r + dirs[i], c1 = c + dirs[i+1];                    
+                    if (r1<0 || r1>=M || c1<0 || c1>=N  ) continue;
+                    int k2 = k1 - grid[r1][c1];
+                    if (k2 >= 0 && !vis[r1][c1][k2])
+                        q.push({r1,c1,k2}),vis[r1][c1][k2]=1;
                 }
-                q.pop();
             }
         }
         return -1;
