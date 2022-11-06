925a1421dae6de6f97acd3d6d005387df18d03d5
jayeshpatel8 <jnpatel81@gmail.com>
2022-11-06 01:05:32 +0000

    Sync LeetCode submission - Orderly Queue (cpp)

--- problems/orderly_queue/solution.cpp
+++ problems/orderly_queue/solution.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
-        string ans;
-        if (k == 1){
-            int smallest=1;
-            for (int i=0; i<s.size(); i++){
-                for (int j=0,i1=i,j1=smallest; j<s.size();j++){
-                    if (s[i1] != s[j1]){
-                        if (s[i1] < s[j1])
-                            smallest=i;
-                        break;
-                    }
-                    i1= (i1+1)%s.size(),j1= (j1+1)%s.size();
-                }
-            }
-            ans = s.substr(smallest) + s.substr(0,smallest);
-        }
+        if (k > 1){sort(begin(s), end(s)); return s;}
         else{
-            int freq[26]={};
-            for ( auto c :s) freq[c-'a']++;
-            
-            for (int i =0; i< 26; i++) ans += string(freq[i],'a'+i);
+            string ans=s;
+            for (int i = 0; i< s.size(); i++){
+                auto s2 = s.substr(i)  + s.substr(0,i);
+                if (s2 < ans) ans = s2;
+            }
+            return ans;
         }
-        return ans;
     }
 };
