e08207f05b73a835f9b3d25c8f5c6d62c595e488
jayeshpatel8 <jnpatel81@gmail.com>
2022-11-08 17:58:39 +0000

    Sync LeetCode submission - Maximum 69 Number (cpp)

--- problems/maximum_69_number/solution.cpp
+++ problems/maximum_69_number/solution.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        auto s = to_string(num);
-        for (auto & c : s){
-            if (c == '6')
-            {
-                c = '9'; break;
-            }
-        }
-        return stoi(s);
+        
+        int ans = 0 , flag=1;
+        if (num >=9666)     ans = 9000, num -=9000;
+        else if (num >= 6666)
+            ans =9000, flag=0 , num -=6000;     
+        if (flag == 0) return ans + num;  
+
+        if (num >=966)     ans += 900, num -=900;
+        else if (num >= 666)
+            ans +=900, flag=0 , num -=600;
+        if (flag == 0) return ans + num;  
+
+        if (num >=96)     ans += 90, num -=90;
+        else if (num >= 66)
+            ans +=90, flag=0 , num -=60;
+        if (flag == 0) return ans + num;  
+
+        if (num >=9)     ans += 9;
+        else if (num >= 6)
+            ans +=9;
+
+        return ans;
     }
+
 };
