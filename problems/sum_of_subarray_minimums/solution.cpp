68f1c86156dbc622dc62428b08e631739667331e
jayeshpatel8 <jnpatel81@gmail.com>
2022-11-25 23:54:27 +0000

    Sync LeetCode submission - Sum of Subarray Minimums (cpp)

--- problems/sum_of_subarray_minimums/solution.cpp
+++ problems/sum_of_subarray_minimums/solution.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> l(n),r(n,n) ,st;
-        int ans = 0 , mod = 1e9 + 7;
-        for (int i=0; i<=n; i++){
-            while (!st.empty() && (i==n || arr[st.back()] >= arr[i])){
-                int j = st.back();st.pop_back();
-                int k = st.empty() ? -1: st.back();
-                ans = (ans  + ((long)arr[j] * (i-j) * (j-k))%mod) % mod;    
+        vector<int> stack;
+        long ans = 0;
+        int mod  = 1e9 + 7;
+        for (int i=0, sz = arr.size(); i<=sz; i++){
+            while (!stack.empty() && (i==sz || arr[stack.back()] >= arr[i])){
+                int mid = stack.back(); stack.pop_back();
+                int  l  = stack.empty() ?  -1 : stack.back();
+                int r = i;
+                long cnt = ((mid- l) * (r-mid) )%mod;
+                ans =  (ans  +  (cnt * arr[mid])%mod) %mod;                
             }
-            st.push_back(i);
+            stack.push_back(i);
         }
 
-        return ans;
-    }
-
-    int sumSubarrayMins2(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> l(n),r(n,n) ,st;
-        for (int i=0; i<n; i++){
-            while (!st.empty() && arr[st.back()] >= arr[i])
-                st.pop_back();
-            l[i] = st.empty() ? i+1 : i - st.back();
-            st.push_back(i);
-        }
-        st.clear();
-        int ans = 0 , mod = 1e9 + 7; 
-        for (int i=n-1; i>=0; i--){
-            while (!st.empty() && arr[st.back()] > arr[i])
-                st.pop_back();
-            r[i] = st.empty() ? n-i :  st.back() - i;
-            st.push_back(i);
-            ans = (ans  + ((long)arr[i] * l[i] * r[i])%mod) % mod;
-        }
-        return ans;
+        return ans % mod;
     }
 };
