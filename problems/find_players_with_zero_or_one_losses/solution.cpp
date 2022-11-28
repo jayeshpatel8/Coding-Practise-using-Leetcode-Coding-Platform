96205fc2401ca072e9c2af216665142309473b12
jayeshpatel8 <jnpatel81@gmail.com>
2022-11-28 23:12:57 +0000

    Sync LeetCode submission - Find Players With Zero or One Losses (cpp)

--- problems/find_players_with_zero_or_one_losses/solution.cpp
+++ problems/find_players_with_zero_or_one_losses/solution.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        vector<int> loss(100001) , win(100001);
-        for (auto &i : matches){
-            loss[i[1]]++;
-            win[i[0]]++;
+        vector<int> result(100001,-1);
+        for (auto i : matches){
+            auto win = i[0], los = i[1];
+            if (result[win] == -1) result[win]=0;
+            if (result[los] == -1) result[los]=1;
+            else result[los]++;
         }
-        vector<int> noloss, oneloss;
-        for ( int i=1; i<100001; i++ ){
-            if (win[i]){
-                if (loss[i] == 0){
-                    noloss.push_back(i);
-                }
-                else if (loss[i] == 1)
-                    oneloss.push_back(i);
-            }
-            else if (loss[i] == 1)
-                    oneloss.push_back(i);
+        vector<vector<int>> ans(2);
+        for (int i=1; i<=100000; i++){
+            if (result[i]==0) ans[0].push_back(i);
+            else if (result[i]==1) ans[1].push_back(i);
         }
-        return {noloss, oneloss};
+        return ans;
+    }
+    vector<vector<int>> findWinners2(vector<vector<int>>& matches) {
+        unordered_map<int,int> loser;
+        for (auto i : matches) loser[i[1]]++;
+        vector<vector<int>> ans;
+        set<int> winner, singlerloser;
+        for (auto i : matches)  
+            if (loser.count(i[0])==0) winner.insert(i[0]);
+        for  ( auto &[k,v] : loser)
+            if (v==1) singlerloser.insert(k);
+        ans.push_back(vector<int>(begin(winner),end(winner)));
+        ans.push_back(vector<int>(begin(singlerloser),end(singlerloser)));
+        return ans;
     }
 };
