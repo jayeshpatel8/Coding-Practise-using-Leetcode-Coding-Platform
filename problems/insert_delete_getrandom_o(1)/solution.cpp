257a290d0e85456cad883b63a4a17dbdc014a164
jayeshpatel8 <jnpatel81@gmail.com>
2022-11-29 23:57:36 +0000

    Sync LeetCode submission - Insert Delete GetRandom O(1) (cpp)

--- problems/insert_delete_getrandom_o(1)/solution.cpp
+++ problems/insert_delete_getrandom_o(1)/solution.cpp
@@ -1,29 +1,28 @@
 class RandomizedSet {
 public:
-    unordered_map<int,int> M;
-    vector<int> A;
+vector<int> A;
+unordered_map<int,int> map;
     RandomizedSet() {
         
     }
     
     bool insert(int val) {
-        if (M.count(val)) return false;
-        M[val] = A.size();
+        if (map.count(val)) return false;
+        map[val] = A.size();
         A.push_back(val);
         return true;
     }
     
     bool remove(int val) {
-        if (M.count(val)==0) return false;
-        int i = M[val];
-        M.erase(val);
+        if (map.count(val)==0) return false;
+        int i = map[val];
+        map.erase(val);
         if (i < A.size()-1){
-            A[i] = A[A.size()-1];                
-            M[A[i]]=i;
+            A[i] = A[A.size()-1];
+            map[A[i]]=i;
         }
-        
         A.pop_back();
-        return true;        
+        return true;
     }
     
     int getRandom() {
