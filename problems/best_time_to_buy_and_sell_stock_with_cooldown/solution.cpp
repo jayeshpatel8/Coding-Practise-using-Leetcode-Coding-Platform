d1d08b78d6221575177659dcd320a80b08d82ad6
jayeshpatel8 <jnpatel81@gmail.com>
2022-12-23 23:25:37 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (cpp)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int dp[5001][2] , N;
-    
+int dp[5001][2] ={}, N;
+
     int maxProfit(vector<int>& prices) {
         N = prices.size();
-        memset(dp,-1,sizeof(dp));
-        return dfs(prices,0,1);
-    }
-    int dfs(vector<int>& prices, int i, int buy){
-        if (i >= N) return 0;
-        if (dp[i][buy] != -1) return dp[i][buy];
-        int ans = dfs(prices, i+1, buy), ans2;
-        if (buy){
-            ans2 = -prices[i] + dfs(prices, i+1, 0);
+        //memset(dp,0,(N+2)*sizeof(int)*2);
+        for (int i=N-1; i>=0; i--){
+            for (int sell=0; sell<2; sell++){
+                int ans =  dp[i+1][sell];
+                if (sell)
+                    ans  = max(ans, prices[i] + dp[i+2][0]);
+                else
+                    ans  = max(ans,-prices[i] + dp[i+1][1]); 
+                dp[i][sell]=ans;
+            }
         }
+        return dp[0][0];
+    }
+    int dfs(vector<int>& prices, int i, bool sell) {
+        if (i >= N) return 0;
+        if (dp[i][sell] != -1) return dp[i][sell];
+
+        int ans =  dfs(prices, i+1,sell);
+        if (sell)
+            ans  = max(ans, prices[i] + dfs(prices,i+2,0));
         else
-            ans2 = prices[i] + dfs(prices, i+2, 1);
-        return dp[i][buy] = max(ans,ans2);
+            ans  = max(ans,-prices[i] + dfs(prices,i+1,1));        
+        
+        return dp[i][sell]=ans;
     }
 };
