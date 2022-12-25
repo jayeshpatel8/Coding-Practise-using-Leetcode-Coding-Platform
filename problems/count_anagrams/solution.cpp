a12610fde3315fb62e9a9c9a574df0a2639d353f
jayeshpatel8 <jnpatel81@gmail.com>
2022-12-25 22:23:53 +0000

    Sync LeetCode submission - Count Anagrams (cpp)

--- problems/count_anagrams/solution.cpp
+++ problems/count_anagrams/solution.cpp
@@ -8,31 +8,29 @@ public:
         fact[0]=1;
         for (int i=1; i<=100005; i++)
             fact[i] = (fact[i-1] * i) % mod;
-        auto powMod  = [&](ll n, ll m)->ll{ 
+        auto powMod  = [&](ll n, ll m){ 
             ll ans = 1;
             while (m > 0){
-                if (m & 1) ans = (ans *n)%mod;
-                
+                if (m & 1) ans = (ans *n)%mod;                
                 n = (n *n) %mod;
                 m>>=1;
             }
             return ans%mod;
         };
-        auto invMod  = [&](int n)->ll{ return powMod(n,mod-2);};
+        auto invMod  = [&](int n){ return powMod(n,mod-2);};
         s.push_back(' ');
         vector<int> freq(26);
         for (int i=0, j = 0; i<s.size(); i++){
-            if (s[i] == ' '){
-              
+            if (s[i] == ' '){              
                 ll perm = fact[i-j];
-                for (auto c : freq){
-                    if (c>1){
+                for (auto &c : freq){
+                    if (c>1)
                         perm =(perm  * invMod(fact[c]) )%mod ;
-                    }
+                    c=0;
                 }
+
                 j=i+1;
                 ans= (ans * perm) %mod;
-                fill(begin(freq),end(freq),0);
             }
             else
                 freq[s[i]-'a']++;
