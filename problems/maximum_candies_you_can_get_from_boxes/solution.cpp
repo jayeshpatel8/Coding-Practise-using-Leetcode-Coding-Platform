860a5ecfdc1fedbffa9cc846f861a55e22a4f3bf
jayeshpatel8 <jnpatel81@gmail.com>
2022-12-31 20:15:37 +0000

    Sync LeetCode submission - Maximum Candies You Can Get from Boxes (cpp)

--- problems/maximum_candies_you_can_get_from_boxes/solution.cpp
+++ problems/maximum_candies_you_can_get_from_boxes/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& cBoxes, vector<int>& ib) {
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& cBoxes, vector<int>& unopen) {
         int N = status.size();
         vector<bool> box(N);
-        set<int> unopen;
+        
         int ans = 0;
-        for (auto i : ib) unopen.insert(i);
+        //for (auto i : ib) unopen.insert(i);
         bool f=true;
         while (f){
-            set<int> n;
+            vector<int> n;
             f=false;
             for( auto i : unopen){                
                 if (status[i]){
                     ans += candies[i];
                     f=1;
                     for (auto k : keys[i]) status[k]=1;
-                    for (auto j : cBoxes[i]) n.insert(j);
+                    n.insert(end(n),begin(cBoxes[i]),end(cBoxes[i]));
                 }
-                else n.insert(i);
+                else n.push_back(i);
             }
             unopen = n;
         }
