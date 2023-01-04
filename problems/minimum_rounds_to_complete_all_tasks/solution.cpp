2ac855188b2b5e099187f1b91553d0f3d4b821ee
jayeshpatel8 <jnpatel81@gmail.com>
2023-01-04 18:29:26 +0000

    Sync LeetCode submission - Minimum Rounds to Complete All Tasks (cpp)

--- problems/minimum_rounds_to_complete_all_tasks/solution.cpp
+++ problems/minimum_rounds_to_complete_all_tasks/solution.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-       unordered_map<int,int> M;
-        for (auto i : tasks){
-            M[i]++;
+        unordered_map<int,int> cnt;
+        for (auto i : tasks) cnt[i]++;
+        int ans = 0;
+        for (auto &[k,v]: cnt){
+            if (v==1) return -1;
+            ans += (v/3) + (v%3!=0);
         }
-        int ans=0;
-        
-        for (auto &[i,f] : M){
-            int r =0;
-            if (f==1) return -1;
-            
-            if (f%3 == 1 ){
-                if (f==2) ans+=1;
-                else{
-                    int d = f/3;
-                 
-                        ans += (f - 3*(d-1))/2 + d-1;
-                   
-                }
-            }
-            else{
-                ans += f/3 + (f%3) /2;
-            }        
-            
-        }
-      
         return ans;
     }
 };
