9c7a7f279f15b8de676898078744745b11deed68
jayeshpatel8 <jnpatel81@gmail.com>
2023-01-05 22:01:39 +0000

    Sync LeetCode submission - Minimum Number of Arrows to Burst Balloons (cpp)

--- problems/minimum_number_of_arrows_to_burst_balloons/solution.cpp
+++ problems/minimum_number_of_arrows_to_burst_balloons/solution.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    int findMinArrowShots2(vector<vector<int>>& points) {
-        sort(begin(points), end(points), [](auto & a, auto &b){return a[1] < b[1];});
-        // put a arrow to end (x2) 
-        int ans = 1, x=points[0][1];
-        for ( auto & p  : points){
-            if (x >= p[0]){
-                continue;
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(begin(points),end(points),[&](auto &a, auto &b){return a[1] < b[1];});
+        int ans = 1, end=points[0][1];
+        for (auto &i : points){
+            if (i[0] <= end){
+                 continue;
             }
-            else
-                ans++, x=p[1];
+            ans++;
+            end = i[1];
         }
         return ans;
     }
-    int findMinArrowShots(vector<vector<int>>& points) {
-        sort(begin(points), end(points));
-        // put a arrow to end (x2) 
-        int ans = 1, x=points[0][1];
-        for ( auto & p  : points){
-            if (x >= p[0]){
-                x = min(x,p[1]); // select min end of both
-                continue;
-            }
-            else
-                ans++, x=p[1];
-        }
-        return ans;
-    }    
 };
-/*
-
-[[9,12],,,[8,12],,]
-[1,10][3,9][4,11][6,9],[6,7]
-
-*/
