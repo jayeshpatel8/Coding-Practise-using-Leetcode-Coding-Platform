7d7d758ecfb085641dd1912d6503db6bdf0d20f7
jayeshpatel8 <jnpatel81@gmail.com>
2023-01-08 13:49:06 +0000

    Sync LeetCode submission - Max Points on a Line (cpp)

--- problems/max_points_on_a_line/solution.cpp
+++ problems/max_points_on_a_line/solution.cpp
@@ -1,47 +1,17 @@
-
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        // slop of line remains constant for any two point on line
-        // slop  = (change in y) /  (change in x)
         
-        int n = points.size();
+        int n = points.size(), ans=0;
         if (n<3) return n;
-        int maxofall=0;
-        int i=0;
-        if (points[0][0]==0 && points[0][1]==0) {
-            points.push_back({0,0});
-            i++;n++;
-            
-        }
-        for (; i<n-1; i++){
 
-            unordered_map<double ,int> slop;
-            int maxPoint=0;
-            int same=0, xsame=0;
-            for (int j=i+1; j<n; j++){
-                //if (i==j) continue;
-               // if (points[i][0] ==0 && points[i][1]==0 ) {continue;
-                  //  points[i][0]=points[i][1]=1; 
-               //}
-                
-                int x = points[j][0] - points[i][0];
-                if (x==0) xsame++;
-                 {
-                    int y = points[j][1] - points[i][1];
-                    if (x==0 && y==0) same++;
-                    else{
-
-                        maxPoint = max (maxPoint, 1 + slop[ ((double)y )/ x ]++);
-                    }
-                }
+        for (int i=0; i<n; i++){
+            unordered_map<double,int> slop;
+            for (int j=0; j<n; j++){
+                if (i != j )
+                    ans = max({ans, ++slop[atan2(points[j][1]-points[i][1],points[j][0]-points[i][0])]});;                
             }
-            
-            maxofall = max(maxPoint+same, maxofall);
-            if (xsame > maxofall) maxofall = xsame;
-            if (maxofall == n-1) break;
-            slop.clear();
         }
-        return maxofall+1;
+        return ans+1;
     }
 };
