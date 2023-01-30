132c812de1be45b6c11f18075285e1f168726f91
jayeshpatel8 <jnpatel81@gmail.com>
2023-01-30 22:58:16 +0000

    Sync LeetCode submission - N-th Tribonacci Number (cpp)

--- problems/n-th_tribonacci_number/solution.cpp
+++ problems/n-th_tribonacci_number/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+int dp[38]= {};
     int tribonacci(int n) {
-     int t[38]={};
-        if (n > 0) t[1]=1;
-        if (n > 1) t[2]=1;
-        for (int i=3; i<=n; i++)
-            t[i]=t[i-1]+t[i-2]+t[i-3];
-        return t[n];
+
+        dp[1]=1;dp[2]=1;
+        for (int i=3; i<=37; i++)              
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3] ;                
+        
+        return dp[n];
     }
 };
