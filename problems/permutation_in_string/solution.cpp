89b8bcd34d0864ab0b447c7866c1479671445e64
jayeshpatel8 <jnpatel81@gmail.com>
2023-02-04 11:38:09 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- problems/permutation_in_string/solution.cpp
+++ problems/permutation_in_string/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> freq1(128);
-        for (auto  c : s1) freq1[c]++;
-        int ans=0, cnt = s1.length();
-        for (int i=0,N=cnt-1; i<s2.length(); i++){
-            if (freq1[s2[i]]-- >=1) cnt--;
-            if (i>=N){
-                ans += cnt==0;
-                if (++freq1[s2[i-N]] >0) cnt++;
-            }
+        vector<int> freq(26,0);
+        for (auto c : s1) freq[c-'a']++;
+        
+        for (int i = 0,N=s1.size()-1,cnt=N+1; i<s2.size(); i++){
+            if (freq[s2[i]-'a']-->0)
+                cnt--;
+            if (i >= N){
+                if (cnt==0) return true;
+                if (++freq[s2[i-N]-'a'] > 0) cnt++;    
+            }       
         }
-        return ans;
+        return false;
     }
 };
