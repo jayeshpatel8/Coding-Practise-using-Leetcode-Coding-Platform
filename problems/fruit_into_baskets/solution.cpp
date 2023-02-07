5364ccdd6ffb6e00e1b9ee1790cd943048832832
jayeshpatel8 <jnpatel81@gmail.com>
2023-02-07 23:08:56 +0000

    Sync LeetCode submission - Fruit Into Baskets (cpp)

--- problems/fruit_into_baskets/solution.cpp
+++ problems/fruit_into_baskets/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& tree) {
-        unordered_map<int,int> type;
-        int j=0, ans=0;
-        for (int i=0; i<tree.size(); i++){
-            ++type[tree[i]];
-             if(type.size()>2){
-                 if(--type[tree[j]]==0) type.erase(tree[j]);
-                 j++;                 
-             }
-            ans = max(ans, i-j+1);
+    int totalFruit(vector<int>& fruits) {
+        vector<int> freq(fruits.size());
+        int ans = 0;
+        for (int i=0, j=0,cnt=0;i<fruits.size(); i++){
+            if (freq[fruits[i]]++ ==0) cnt++;
+            while (cnt > 2){
+                if (--freq[fruits[j++]] ==0) cnt--;
+            }
+            ans= max(ans, i-j+1);
         }
         return ans;
     }
