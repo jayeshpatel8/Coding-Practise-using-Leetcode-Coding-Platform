c81b874eaec727254268e2056ac8096404b68eb2
jayeshpatel8 <jnpatel81@gmail.com>
2023-02-08 16:35:43 +0000

    Sync LeetCode submission - Jump Game II (cpp)

--- problems/jump_game_ii/solution.cpp
+++ problems/jump_game_ii/solution.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        size_t l = nums.size() ;
-        if (l==1) return 0;
-        vector<int> dp(l+1,1);
-        dp[0] = 0;
-        size_t jump=1;
-       
-        for (int i = 0 ; i <l; i++){
+        int N= nums.size()-1, i = 0, j  = 0, jump=0;
+        
+        while (j < N){
+            int k = 0;
+            while (i<=j && i<N) 
+                k = max(k,i+nums[i++]);
             
-            if (i + nums[i] >= jump)
-            {
-                while(jump<=nums[i]+i)
-                {
-                    dp[jump++]=dp[i]+1;
-                    if (jump==l) return dp[jump-1];
-                }
-            }
+            j = k; jump++;
         }
-        return 0;
+        return jump;
     }
 };
