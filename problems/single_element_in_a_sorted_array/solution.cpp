12ef60115f56e1f74a3ad275e3e91222fab74db0
jayeshpatel8 <jnpatel81@gmail.com>
2023-02-21 23:18:35 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (cpp)

--- problems/single_element_in_a_sorted_array/solution.cpp
+++ problems/single_element_in_a_sorted_array/solution.cpp
@@ -4,21 +4,12 @@ public:
         int N = nums.size(),  l = 0, r= N-1 ;
         while (l<r){
             int m = (l+r) / 2;
-            if (m+1< N && nums[m]==nums[m+1]){
-                if ((N-m)& 1){ //odd
-                    l = m+1;
-                }
-                else
-                    r=m-1;
-            }
-            else if (m-1 >=0  && nums[m]==nums[m-1]){
-                if (m & 1){ //even
-                    l=m+1;
-                }
-                else
-                    r = m-1;
-            }
-            else return nums[m];
+            if (((m&1) && nums[m]==nums[m-1]) || ((m&1)==0  && nums[m]==nums[m+1]))
+                l = m+1;
+
+            else
+                r=m;
+
         }
         
         return nums[l];
