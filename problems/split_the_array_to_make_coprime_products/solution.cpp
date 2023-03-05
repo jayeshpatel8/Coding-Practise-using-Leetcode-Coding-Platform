dcb42be089c31aedf62731448df2b798138e03ff
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-05 20:18:49 +0000

    Sync LeetCode submission - Split the Array to Make Coprime Products (cpp)

--- problems/split_the_array_to_make_coprime_products/solution.cpp
+++ problems/split_the_array_to_make_coprime_products/solution.cpp
@@ -1,83 +1,54 @@
- using ll = unsigned long long;
+
 class Solution {
 public:
-    vector<int> prime;
-    ll gcd1(ll a, ll b){
-       while (a > 0 && b > 0) {
-            if (a > b) {
-                a = a % b;
+   vector<int> sieve;
+    int N= 1e6+1;;
+    vector<int> PrimeFactors (int x) {
+        if (sieve.empty()) {
+            sieve.resize(N);
+            for (int j = 0; j < N; j ++) sieve[j] = j;
+            
+            for (int j = 2; j*j < N; j ++) {
+                if (sieve[j] != j) continue;
+                for (int k = j*j; k < N; k += j)
+                    sieve[k] = j;
             }
-            else {
-                b = b % a;
-            }
         }
-        if (a == 0) {
-            return b;
+        
+        vector<int> result;
+        while (x != 1) {
+            int div = sieve[x];
+            while (x % div == 0) x /= div;
+            result.push_back(div);
         }
-        return a;
+        return result;
     }
-    unordered_set<int> getnum(ll a , unordered_set<int> &st){
-        ll b=1;
-        //unordered_set<int> st; 
-        for (auto di : prime)      
-        {
-            if (a >= di && (a % di == 0)) st.insert(di);
-            while (a >= di && (a % di == 0)) a /=di;  
-            if (a < di) break;
-        }
-        if (a> 1)
-        st.insert(a);
-        return st;
-    }
-    
     int findValidSplit(vector<int>& nums) {
-               int possible =0;
         int n = nums.size();
-        for(int i=0;i<=possible;i++){
-            int ele = nums[i];
-            for(int j=i+1;j<n;j++){
-                if(nums[j]%ele==0) possible = max(possible,j);
-                if(ele%nums[j]==0) possible = max(possible,j);
-            }
+        vector<int> last_factor_ind (N, 0);
+        for (int i= 0; i<nums.size(); i++){
+            for (int f : PrimeFactors(nums[i])) last_factor_ind[f]=i;
         }
-        return possible<=(n-2)?possible:-1;
-        vector<unordered_set<int> > pro(nums.size());
-        int max_e = *max_element(begin(nums), end(nums));
-        vector<int> no_pr(max_e+1,0) ;
-        
-        for (int i=2; i*i<=max_e; i++){
-            for (int j=i+i; j<=max_e; j+=i)
-                no_pr[j]=1;
+        int i=0 ,last=0;
+        for (; i<=last  ;i++){
+             for (auto f  : PrimeFactors(nums[i])){
+                last = max(last,last_factor_ind[f]);                 
+             }
         }
-        for (int i=2; i<=max_e; i++) 
-            if (no_pr[i]==0) prime.push_back(i);
-       
-        
-        unordered_set<int> p ,p1;
-        for (int i=nums.size()-1; i>=0; i--){
-            getnum(nums[i],p);            
-            pro[i] = p;
-        }
-        //ll p1=1;
-        for (int i=0; i<nums.size()-1; i++){
-             getnum(nums[i],p1);
-            //for (auto s : st) p1.insert(s); 
-            //unordered_map<int,int>m;
-            //for (auto i : p1) m[i]++;
-            //for (auto j : pro[i+1]) m[j]++;
-            int f=0;
-            for (auto j : p1)
-            {
-                if ( pro[i+1].count(j)) {
-                    f=1;
-                    break;
-                }
-            }
-            if (f==0)  {
-                return i;
-            }
-        }
-        
-        return -1;
+        if (i==n)        
+         return -1;
+        return i-1;
     }
+vector<int> factorize(int n) {
+    vector<int> res;
+    for (int i = 2; n > 1 && i < 1000; i += 1 + (i % 2))
+        if (n % i == 0) {
+            res.push_back(i);
+            while(n % i == 0)
+                n /= i;
+        }
+    if (n > 1)
+        res.push_back(n);
+    return res;
+}
 };
