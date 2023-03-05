d805b6ed70fac755cd868e58fae3ddf089fceadf
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-05 14:28:04 +0000

    Sync LeetCode submission - Jump Game IV (cpp)

--- problems/jump_game_iv/solution.cpp
+++ problems/jump_game_iv/solution.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
-        int  N = arr.size();
-        if (N<2) return 0;
-        if (arr[0]==arr[N-1]) return 1;
-        //BFS
-        queue<int>q; q.push(0);
-        vector<bool> vis(N);
-        unordered_map<int,vector<int>>map;
-        for ( int i=0; i<N; i++)
-            map[arr[i]].push_back(i);
-        int end=N-1,best=N+1,ans=0;
-        vis[0]=1;
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz-->0)
-            {
-                auto i = q.front(); q.pop();
+        unordered_map<int, vector<int> > map;
+        int N = arr.size();
+        if  (N<2) return 0;
+        vector<int> vis(N);
 
+        for ( int i=0;i< N; i++) map[arr[i]].push_back(i);
+
+        queue<int>   q;
+        q.push(0);vis[0]=1;
+        int dist = 0;
+        while (!q.empty()){
+            int sz = q.size();
+            while (sz-- > 0){
+                
+                int i = q.front(); q.pop();
+                
+                if (i-1 >= 0 && !vis[i-1]){
+                    q.push(i-1); vis[i-1]=1;
+                }
+                if (i+1 < N && !vis[i+1]){
+                    q.push(i+1); vis[i+1]=1;
+                }
                 if (map.count(arr[i])){
                     for (auto j : map[arr[i]]){
                         if (vis[j]) continue;
@@ -26,16 +31,12 @@ public:
                     }
                     map.erase(arr[i]);
                 }
-                if (i+1<N && vis[i+1]==0){
-                    vis[i+1]=1; q.push(i+1);
-                }
-                if (i-1>=0 && vis[i-1]==0){
-                    vis[i-1]=1; q.push(i-1);
-                }                
-                if (vis[end]) return ans+1;
+                if (vis[N-1]) return dist+1; 
             }
-            ans++;
+
+            dist++;
         }
-        return N;
+        return dist;
+
     }
 };
