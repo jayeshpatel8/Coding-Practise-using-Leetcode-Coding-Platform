430744c2d906daade7b2472256fbcb2956d127ed
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-06 21:39:22 +0000

    Sync LeetCode submission - Kth Missing Positive Number (cpp)

--- problems/kth_missing_positive_number/solution.cpp
+++ problems/kth_missing_positive_number/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n=arr.size();
-        //[2,3,4,7,11]
-        for (int i=1,j=0; i<arr[n-1] ; i++){
-            if(arr[j] == i) {j++; continue;}            
-            if (k<=arr[j]-i) return i+k-1;
-            k -=arr[j]-i; 
-            i =arr[j++];
-        }
-        return arr[n-1]+k;
-    }
+        int l=0,r = arr.size();
+        
+        while (l<r){
+            int m = (l+r)/2;
 
+            if (arr[m] - (m +1)< k) l = m+1;
+            else
+                r=m;
+        }
+        return l+k;
+    }
 };
