26f52f894e6a3694ad567ef47cccee56c708c5d4
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-11 10:24:36 +0000

    Sync LeetCode submission - Convert Sorted List to Binary Search Tree (cpp)

--- problems/convert_sorted_list_to_binary_search_tree/solution.cpp
+++ problems/convert_sorted_list_to_binary_search_tree/solution.cpp
@@ -22,39 +22,25 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        if (!head) return 0;
-       // vector<int> arr;
+        ListNode* h = head ;
+        if (!h) return nullptr;;
         int len=0;
-        ListNode *cur=head;
-        while (head ){
-            //arr.push_back(head->val); 
-            head=head->next;
-            len++;
-        }
-        //return sortedArrToBST1(arr, 0,arr.size()-1);
-        return sortedArrToBST2(cur, 0,len-1);
+        while (h) h=h->next, len++;
+
+        return sortedArrToBST2(head,0,len-1);
     }
-    TreeNode* sortedArrToBST1(vector<int>& arr,int l, int r){
-        if (l<=r){
-            int m = l + (r-l)/2;
-            TreeNode * root = new TreeNode(arr[m]);
-            root->left = sortedArrToBST1(arr, l,m-1);
-            root->right = sortedArrToBST1(arr, m+1, r);
-            return root;
-        }
-        return 0;
-    }
-    TreeNode* sortedArrToBST2(ListNode* &head,int l, int r){
+        TreeNode* sortedArrToBST2(ListNode* &head,int l, int r){
         if (l<=r){
             int m = l + (r-l)/2;
              TreeNode * root = new TreeNode;
             root->left = sortedArrToBST2(head, l,m-1);
+            
             root->val = head->val;
             head=head->next;
+            
             root->right = sortedArrToBST2(head, m+1, r);
             return root;
         }
         return 0;
     }
-    
 };
