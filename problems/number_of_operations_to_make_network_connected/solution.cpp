011216f0e82e1f95dd7820d3e0f7be1148c471bc
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-23 13:15:43 +0000

    Sync LeetCode submission - Number of Operations to Make Network Connected (cpp)

--- problems/number_of_operations_to_make_network_connected/solution.cpp
+++ problems/number_of_operations_to_make_network_connected/solution.cpp
@@ -1,53 +1,27 @@
 class Solution {
 public:
-    
     vector<int> uf;
-    vector<int> size;
-    int makeConnected(int n, vector<vector<int>>& conn) {
-        uf.resize(n,-1);
-        size.resize(n);
-        int unused = 0;
-        vector<int> vis(n);
-        for (int i=0; i<conn.size(); i++){
-            int u=conn[i][0], v= conn[i][1];
-            if (isconn(u,v)){
-                unused++;
-            }
-            else{
-                connect(u,v);
-            }
-        }
-        int cnode=0;
-        for (int i=1; i<n; i++){
-            if (!isconn(0,i)){
-                if (unused){
-                    connect(0,i);
-                    cnode++;
-                    unused--;
-                }
-                else return -1;
-            }
-        }
-        
-        return cnode;
-    }
-    bool isconn(int u, int v){
-        return find(u) == find(v);
-    }
     int find(int i){
         return uf[i] < 0 ? i : uf[i] = find(uf[i]);
     }
-    void connect(int u, int v){
-        int pu = find(u), pv=find(v);
-        if (pu != pv){
-            if (size[pu] < size[pv]){
-                uf[pu]=pv;
-                size[pv]+=size[pu];
-            }
-            else{
-                uf[pv]=pu;
-                size[pu]+=size[pv];
-            }
+    void join(int a, int b){
+        int pa = find(a)  , pb  = find(b);
+        if (pa != pb){
+            if (uf[pa]< uf[pb]) swap(pa,pb);
+            uf[pb] += uf[pa];
+            uf[pa]=pb;
         }
+    }
+    int makeConnected(int n, vector<vector<int>>& conn) {
+        uf.resize(n,-1);
+        int extra=0;
+        for (auto & e : conn){
+            if (find(e[0])== find(e[1]))
+                extra++;
+            else
+                join(e[0],e[1]);
+        }
+        int cc =  count_if(begin(uf),end(uf),[](int a){return a< 0;})  ;
+        return cc -1 > extra ? -1 : cc -1;
     }
 };
