b02493e00257c6636469e7d061488338404ec0ed
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-25 16:56:37 +0000

    Sync LeetCode submission - Count Unreachable Pairs of Nodes in an Undirected Graph (cpp)

--- problems/count_unreachable_pairs_of_nodes_in_an_undirected_graph/solution.cpp
+++ problems/count_unreachable_pairs_of_nodes_in_an_undirected_graph/solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    vector<int> uf ,size;
-    int find(int a){
-        return uf[a] <0 ? a : uf[a] = find(uf[a]);
-    }
-    
-    void merge(int a, int b){
-        int pa= find(a), pb =find(b);
-        if (pa != pb){
-            if (size[pa] < size[pb])
-                uf[pb]=pa, size[pa] += size[pb];
-            else
-                uf[pa]=pb , size[pb] += size[pa];
-        }
-    }
-    
+    vector<bool> vis;
     long long countPairs(int n, vector<vector<int>>& edges) {
-        uf.resize(n, -1);
-        size.resize(n, 1);
-        
-        for (auto &e : edges)
-                merge(e[0],e[1]);
-        
-        long long ans = 0;
-        for (int i=0; i<n; i++)   
-            ans += (n - size[find(i)]);
-
-        return ans / 2;
+        vis.resize(n);
+        vector<vector<int>> G(n);
+        for (auto & e : edges)
+            G[e[0]].push_back(e[1]),G[e[1]].push_back(e[0]);
+        long long ans = 0,   rem = n;
+        for (int i=0; i<n; i++){
+            if (!vis[i]){
+                vis[i]=true;
+                long long c = dfs(G,i) + 1;
+                rem -= c;
+                ans = (ans + rem * c);
+            }
+        }
+        return ans;
+    }
+    int dfs(vector<vector<int>> & G, int u){
+        int ans = 0;
+        for (auto v : G[u]){
+            if (vis[v]) continue;
+            vis[v]=true;
+            ans += 1 + dfs(G,v);
+        }
+        return ans;
     }
 };
