5d793b7a2291c406085466515ce58b0b3b246f8e
jayeshpatel8 <jnpatel81@gmail.com>
2023-03-29 07:17:21 +0000

    Sync LeetCode submission - Reducing Dishes (cpp)

--- problems/reducing_dishes/solution.cpp
+++ problems/reducing_dishes/solution.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
-    int maxSatisfaction(vector<int>& s) {
-        sort(begin(s),end(s));
-        int res = 0, total=0;
-        for(int i =s.size()-1; i>=0  && (s[i] + total > 0); i--){
-            total +=s[i];
-            res+=total;
+    int maxSatisfaction(vector<int>& sat) {
+        sort(rbegin(sat),rend(sat));
+        int ans = 0;
+        for (int i=0 , psum = 0 ; i<sat.size(); i++){
+            psum += sat[i];
+            if (psum < 0) 
+                return ans;
+            else
+                ans += psum;
+            
         }
-        return res;
+        return ans;
+    }
+    int maxSatisfaction2(vector<int>& sat) {
+        sort(rbegin(sat),rend(sat));
+        vector<int> psum(sat.size()+1);
+        for (int i=0; i<sat.size(); i++){
+            psum[i+1] = psum[i] + sat[i];
+        }
+        int ans = 0;
+        for (int i=0; i<sat.size(); i++){
+            int coe = ans + psum[i+1];
+            if (coe < ans) return ans;
+            ans = coe;
+        }
+        return ans;
     }
 };
