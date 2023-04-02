293194b10bdcc0a0e5625b9eb6992f07ff4f8e52
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-02 14:05:35 +0000

    Sync LeetCode submission - Successful Pairs of Spells and Potions (cpp)

--- problems/successful_pairs_of_spells_and_potions/solution.cpp
+++ problems/successful_pairs_of_spells_and_potions/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& po, long long su) {
-        int N = spells.size();
-        sort(begin(po) , end(po));
-        vector<int> ans(N,0);
-        for (int i=0; i<N; i++){
-            long long num = (su / spells[i])  + ((su % spells[i])!=0);
-            auto it = lower_bound(begin(po) , end(po), num);
-            
-            ans[i] = end(po) - it;
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(begin(potions), end(potions));
+        vector<int>  ans(spells.size());
+        
+        for (int i=0, N = potions.size(); i<spells.size(); i++){
+            ans[i] = end(potions)- lower_bound(begin(potions), end(potions), (success + spells[i]-1)/spells[i]) ;
+
         }
         return ans;
     }
