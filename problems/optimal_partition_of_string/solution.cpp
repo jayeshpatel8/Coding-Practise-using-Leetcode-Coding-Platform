ab8fbfe14f2fbf97685b3816801fcd6c74cccf8c
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-04 12:36:01 +0000

    Sync LeetCode submission - Optimal Partition of String (cpp)

--- problems/optimal_partition_of_string/solution.cpp
+++ problems/optimal_partition_of_string/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int f[128]={}, ans = 1;
-        
-        for (int i = 0,j=0; i<s.size(); i++){
-            f[s[i]]++;        
-            if (f[s[i]]<2) continue;
-            while (j<i ){
-                f[s[j++]]--;
+        int ans = 1, f[126]={};
+        for (int i=0,j=0; i<s.size();i++ ){
+            if (f[s[i]]++){
+                while (j<i ) f[s[j++]]=0;
+                f[s[i]]=1;
+                ans++;
             }
-            ans++;
+            
         }
-        return ans;
+        return ans ;
     }
 };
