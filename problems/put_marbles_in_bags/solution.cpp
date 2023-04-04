fa03042ef1aac6a4aab8f34e0a6cc48b81aea135
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-04 22:14:09 +0000

    Sync LeetCode submission - Put Marbles in Bags (cpp)

---
+++ problems/put_marbles_in_bags/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size()-1;
+        for (int i=0; i<n; i++){
+            weights[i] += weights[i+1];
+        }
+        weights.pop_back();
+        nth_element(begin(weights),begin(weights) + k - 1, end(weights));
+        long long ans = 0;
+        for (int i=0; i<k-1; i++)
+            ans -= weights[i];
+        nth_element(begin(weights),begin(weights) + n - k + 1, end(weights));
+        for (int i=n+1-k; i<n; i++)
+            ans += weights[i];
+        return ans;
+    }
+};
