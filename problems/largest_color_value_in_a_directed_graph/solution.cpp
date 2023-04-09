73d7f4f9af856dd78ba0ddc42f51d8fe1f7fa81e
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-09 18:03:28 +0000

    Sync LeetCode submission - Largest Color Value in a Directed Graph (cpp)

--- problems/largest_color_value_in_a_directed_graph/solution.cpp
+++ problems/largest_color_value_in_a_directed_graph/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int N = colors.size(),ans = 0;
-        vector<vector<int>> freq(N,vector<int>(26)); 
-        vector<int> ed[N], in(N);
-        for (auto& e : edges){
-            ed[e[0]].push_back(e[1]);
-            in[e[1]]++;
-        }
+        int N=  colors.size();
+        vector<int> deg(N);
+        vector<vector<int>> G(N);
         queue<int> q;
-        for (int i=0; i<N; i++)
-            if (in[i]==0)
-                q.push(i);
-        int vis=0;
-        while(!q.empty()){
-            int u = q.front(), c = colors[u]-'a';q.pop();
-            vis++;
-            ans = max(ans, ++freq[u][c]);
-            for (auto v : ed[u]){
-                for (auto k = 0; k < 26; ++k)
-                    freq[v][k] = max(freq[v][k], freq[u][k]);
-                if (--in[v] == 0)
+        for (auto & e : edges)
+            G[e[0]].push_back(e[1]), deg[e[1]]++;
+
+        for (int i=0; i<N; i++){            
+            if (!deg[i])
+                q.push(i); 
+        }
+        vector<vector<int>> freq(N,vector<int>(26));
+        int visited=0, ans = 0;
+        while (!q.empty()){
+            visited++;
+            int u = q.front(); q.pop();
+            freq[u][colors[u]-'a']++;
+            ans=  max(ans, freq[u][colors[u]-'a']);
+
+            for (auto v : G[u]){
+                for (int i=0; i<26; i++)
+                    freq[v][i] = max(freq[v][i], freq[u][i]);
+                if (deg[v]-- == 1) {
                     q.push(v);
+                }
             }
         }
-        return vis==N? ans : -1;
+        return visited==N ? ans : -1;
     }
 };
