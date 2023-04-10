d29d68f0bb62c765ed89dbcd42acb3ebe734a8b8
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-10 10:30:58 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- problems/valid_parentheses/solution.cpp
+++ problems/valid_parentheses/solution.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int>st;
-        string s1 = "[({";
-        unordered_map<char,char> bra={{']','['},{')','('}, {'}','{'}};
-        for (auto c :s){
-            if (s1.find(c) != string::npos)
-                st.push(c);
-            else{
-                if (st.empty() || st.top() != bra[c] ) return false;
-                st.pop();
+        vector<int> stack;
+        
+        for (auto c : s){
+            if ( c == ')'){
+                if (stack.empty() || stack.back() != '(') return false;
+                stack.pop_back();
             }
+            else if ( c == '}'){
+                if (stack.empty() || stack.back() != '{') return false;stack.pop_back();
+            }
+            else if ( c == ']'){
+                if (stack.empty() || stack.back() != '[') return false;stack.pop_back();
+            }
+            else
+                stack.push_back(c);
+
         }
-     return st.empty();   
+        return stack.empty();
     }
 };
