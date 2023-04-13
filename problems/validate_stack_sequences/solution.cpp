e8930e156269e6b99025bc0df7fcb6b6f87daeff
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-13 13:05:46 +0000

    Sync LeetCode submission - Validate Stack Sequences (cpp)

--- problems/validate_stack_sequences/solution.cpp
+++ problems/validate_stack_sequences/solution.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-       stack<int>st;
-        int j=0, N = pushed.size();
-       for (auto i : pushed) {
-           if (i == popped[j] ){
-               j++;
-               //if (j==N) return true;
-               while(st.empty() != true && st.top() == popped[j]){
-                   j++;st.pop();
-               }
-           }
-           else
-               st.push(i);
-       }
-        return j==N;
+        vector <int> st;
+        for (int i=0,j=0; i<pushed.size(); i++ ){
+            st.push_back(pushed[i]);
+            while (!st.empty() && st.back() == popped[j]){
+                st.pop_back(); j++;
+            }
+        }
+        return st.empty();
     }
 };
