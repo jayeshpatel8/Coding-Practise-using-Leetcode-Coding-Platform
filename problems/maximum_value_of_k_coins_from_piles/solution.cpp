2946b6c31b409dde0e399f33b6f64ba46b505c4d
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-15 12:10:20 +0000

    Sync LeetCode submission - Maximum Value of K Coins From Piles (cpp)

--- problems/maximum_value_of_k_coins_from_piles/solution.cpp
+++ problems/maximum_value_of_k_coins_from_piles/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int dp [1001][2001];
+int dp[1001][2001]={}, K;
+
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         memset(dp,-1,sizeof(dp));
-        
+        K= k ;
         return dfs(piles,0,k);
     }
-    int dfs(vector<vector<int>>& piles,int i, int k){
-        if (i >= piles.size() || k<=0) return 0;
-        if (dp[i][k] != -1 ) return dp[i][k];
-        
-        int ans = dfs(piles,i+1,k), sum =0;
-        for (int j=0, N = min<int>(piles[i].size(), k); j< N; j++){
+    int dfs(vector<vector<int>>& piles, int i, int k) {
+        if (i>=piles.size() || k==0) return 0;
+        if (dp[i][k] != -1) return dp[i][k];
+        int ans = dfs(piles, i+1,k);
+        for ( int j=0, sum = 0 ,N = min<int>(piles[i].size(), k); j< N; j++){
             sum += piles[i][j];
-            ans = max (ans, sum + dfs(piles,i+1,k-j-1));
+            ans = max(ans, sum + dfs(piles, i+1,k-j-1));
         }
-        return dp[i][k] = ans;
+        return dp[i][k]  = ans;
     }
 };
