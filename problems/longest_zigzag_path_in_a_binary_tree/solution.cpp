99527f2c81082ed846d294fad0f7bbff1ebb1443
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-19 11:06:51 +0000

    Sync LeetCode submission - Longest ZigZag Path in a Binary Tree (cpp)

--- problems/longest_zigzag_path_in_a_binary_tree/solution.cpp
+++ problems/longest_zigzag_path_in_a_binary_tree/solution.cpp
@@ -11,27 +11,19 @@
  */
 class Solution {
 public:
-    int ans = 0;
+int ans = 0;
     int longestZigZag(TreeNode* root) {
-        //max(ans, dfs(root,0));
         dfs(root);
-        return ans-1;
+        return ans;
     }
-    array<int,2> dfs(TreeNode* root){
-        if (!root) return {0,0};
-        int r = (dfs(root->right)[0]) +1;
-        int l = (dfs(root->left)[1]) +1;        
-        ans= max({ans,r,l});
-        
-        return {l,r};
-    }    
-    int dfs2(TreeNode* root, int dir){
-        if (!root) return 0;
-        int r = (dfs2(root->right,1)) +1;
-        int l = (dfs2(root->left,0)) +1;
-        int k = dir ==0 ? r : l;
-        ans= max({ans,r,l});
-        
-        return k;
+    array<int,2> dfs(TreeNode * root){
+        if (!root) return{0,0};
+        auto l = dfs(root->left);
+        auto r = dfs(root->right);
+        int left=0,right=0;
+        if (root->left) left = 1 + l[1];
+        if (root->right) right = 1 + r[0];
+        ans= max({ans,left,right});
+        return {left,right};
     }
 };
