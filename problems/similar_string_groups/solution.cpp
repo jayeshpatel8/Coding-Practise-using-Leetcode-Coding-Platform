fc521ea40c35cec2c3a0c15fdb529e85205a9255
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-28 10:49:43 +0000

    Sync LeetCode submission - Similar String Groups (cpp)

--- problems/similar_string_groups/solution.cpp
+++ problems/similar_string_groups/solution.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    vector<int> uf;
-    int find (int i){ return uf[i] < 0 ? i : uf[i] = find(uf[i]);}
-    void connect(int i, int j){
-        int pi = find(i), pj = find(j);
-        if (pi != pj)
-            uf[pi]=pj;
+    vector<int> uf,sz;
+    int find ( int i){
+        return uf[i] <0 ? i : uf[i] = find(uf[i]);
+    }
+    void join (int a, int b){
+        int pa= find(a), pb= find(b);
+        if (pa != pb){
+            if (sz[pa] < sz[pb])
+                uf[pa]=pb, sz[pb] += sz[pa];               
+            else
+                uf[pb]=pa, sz[pa] += sz[pb];   
+        }
     }
     int numSimilarGroups(vector<string>& strs) {
-         int N = strs.size();
-        uf.resize(N,-1);
-        for ( int i =0; i<N; i++){
-            for (int j=i+1; j<N; j++){
-                if (match(strs[i],strs[j])){// matches
-                    connect(i,j);
-                }                
+        uf.resize(strs.size(),-1);
+        sz.resize(strs.size());
+        for (int i=0; i<strs.size(); i++)
+            for (int j=i+1; j<strs.size(); j++){
+                if (similar(strs[i],strs[j])){
+                    join(i,j);
+                }
             }
-        }
-        return (int)count(begin(uf),end(uf),-1);
+        return count(begin(uf),end(uf),-1);
     }
-    bool match(string& i, string & j){
-        int l=0,cnt=0;
-        for (; l<i.size();  l++){
-            if (i[l] != j[l] && ++cnt > 2) 
-                return false;
+    bool similar(string &a, string &b){
+        for (int i=0,cnt=0; i<a.size(); i++){
+            if (a[i] != b[i] && ++cnt >2) return false;
         }
         return true;
     }
