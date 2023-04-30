cde4993ee76499b5f0982bc8c4ea116a6f4ae89a
jayeshpatel8 <jnpatel81@gmail.com>
2023-04-30 10:44:55 +0000

    Sync LeetCode submission - Remove Max Number of Edges to Keep Graph Fully Traversable (cpp)

--- problems/remove_max_number_of_edges_to_keep_graph_fully_traversable/solution.cpp
+++ problems/remove_max_number_of_edges_to_keep_graph_fully_traversable/solution.cpp
@@ -1,58 +1,40 @@
-
-
 class Solution {
 public:
-    vector<int> uf[3],size[3];
-    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        //sort(rbegin(edges),rend(edges));
-        uf[1].resize(n+1,-1);
-        uf[2].resize(n+1,-1);
-        size[1].resize(n+1);
-        size[2].resize(n+1);        
-        int rem=0;
-        for (auto & e : edges)
-            if (e[0]==3)
-                rem += connect(e);
-        for (auto & e : edges){
-            if (e[0] != 3) rem += connect(e);
-        }
-        if (count(begin(uf[1]),end(uf[1]),-1) != 2 || count(begin(uf[2]),end(uf[2]),-1) != 2 )
-           return -1;
-        return rem;
+    vector<int> uf[2],sz[2];
+    int find (int id, int i){
+        return uf[id][i] < 0 ? i : uf[id][i] = find (id,uf[id][i]);
     }
-    int find(int e, int i){
-        return uf[e][i] < 0 ? i : uf[e][i] = find(e,uf[e][i]);
-    }
-    bool check(vector<int> &e){
-        if (e[0] == 3)
-            return (find(1,e[1])==find(1,e[2])  && find(2,e[1])==find(2,e[2]) );
+    int connect(int id, int i, int j){
+        int pi = find(id,i), pj = find (id,j);
+        if (pi==pj) return 1;
+        if (sz[id][pi] < sz[id][pj])
+            uf[id][pi]=pj, sz[id][pj] += sz[id][pi];
         else
-            return (find(e[0],e[1])==find(e[0],e[2]));
+            uf[id][pj]=pi, sz[id][pi] += sz[id][pj];
+        return 0;
     }
-    int connect(vector<int> &e){
-        if (e[0] == 3){
-            if ((find(1,e[1])==find(1,e[2])  && find(2,e[1])==find(2,e[2]) ))return 1;
-            for (int i=1; i<=2; i++){
-                int pi=find(i,e[1]), pj=find(i,e[2]);
-                if (pi != pj){
-                    if (size[i][pi] > size[i][pj])
-                    {size[i][pi]+= size[i][pj]; uf[i][pj]=pi;}
-                    else
-                        size[i][pj]+= size[i][pi], uf[i][pi]=pj;
-                }            
-            }
-            return 0;
-        }
-        else{
-            int pi=find(e[0],e[1]), pj=find(e[0],e[2]);
-            if (pi != pj){
-                if (size[e[0]][pi] >  size[e[0]][pj])
-                    size[e[0]][pi] += size[e[0]][pj], uf[e[0]][pj]=pi;
-                else
-                    size[e[0]][pj] += size[e[0]][pi], uf[e[0]][pi]=pj;
-                return 0;
-            }
-            return 1;
-        }
+    bool connect(vector<int> &e){
+        int i=e[1], j = e[2];
+        
+        if (e[0]==3)
+           return connect(0,i,j) || connect(1,i,j);
+        else if (e[0]==2)
+            return connect(1,i,j);
+        else
+            return connect(0,i,j);
+        return 0;
+    }
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        uf[0].resize(n+1,-1),uf[1].resize(n+1,-1);
+        sz[0].resize(n+1),sz[1].resize(n+1);
+        int rem=0;
+        for (auto &e : edges)
+            if (e[0]==3)  
+                rem += connect(e);
+        for (auto &e : edges)
+            if (e[0]!=3)  
+                rem += connect(e);
+
+        return (count(begin(uf[0]),end(uf[0]),-1) != 2) || (count(begin(uf[1]),end(uf[1]),-1)!=2) ? -1 : rem;
     }
 };
