452825b767d9e02e68f353c4c19865377f06e20a
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-02 09:57:55 +0000

    Sync LeetCode submission - Sign of the Product of an Array (cpp)

--- problems/sign_of_the_product_of_an_array/solution.cpp
+++ problems/sign_of_the_product_of_an_array/solution.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int cnt=0;
-        for (auto i  : nums){
-            if (i==0) return 0;
-            if (i<0) cnt++;
-        }
-        return cnt%2==0? 1 : -1;
+        auto p  =  accumulate(begin(nums), end(nums), 1,[](auto x,int a){return x * (a<0 ? -1 : a!=0); });
+        return p<0 ? -1 : p!=0;   
     }
 };
