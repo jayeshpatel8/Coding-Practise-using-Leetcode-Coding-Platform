8c0b1693d190dab1b68c0e8756946bffb2804b87
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-03 09:44:02 +0000

    Sync LeetCode submission - Find the Difference of Two Arrays (cpp)

--- problems/find_the_difference_of_two_arrays/solution.cpp
+++ problems/find_the_difference_of_two_arrays/solution.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         int f1[2001]={},f2[2001]={};
-        for (auto i:nums1) f1[i+1000]=1;
-        for (auto i:nums2) f2[i+1000]=1;
-        vector<vector<int>> ans;
-        vector<int> ans1, ans2;
-        int  i=0, j=0;
-        for ( ; i <2001; i++){
-            if ( f1[i] == f2[i]){continue;
-            }
-            else if (f1[i]){
-                ans1.push_back(i-1000);
-            }
-            else
-                ans2.push_back(i-1000);
-        }
-       
-        return {ans1,ans2};
+        for (auto i : nums1) f1[i+1000]++;
+        for (auto i : nums2) f2[i+1000]++;
+        vector<int> a,b;
+        for (auto i : nums1) 
+            if (f1[i+1000]>0 && !f2[i+1000])  f1[i+1000]=-1,a.push_back(i);
+        for (auto i : nums2) 
+            if (!f1[i+1000] && f2[i+1000]>0)  f2[i+1000]=-1,b.push_back(i);
+        return {a,b};
     }
 };
