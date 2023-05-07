b3c8ffe5c4fdaa2aac1208a612324e84a1447f50
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-07 18:34:52 +0000

    Sync LeetCode submission - Find the Longest Valid Obstacle Course at Each Position (cpp)

--- problems/find_the_longest_valid_obstacle_course_at_each_position/solution.cpp
+++ problems/find_the_longest_valid_obstacle_course_at_each_position/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obs) {
-        vector<int> ans, lis;
-        ans.push_back(1);
-        lis.push_back(obs[0]);
-        for (int i=1; i<obs.size(); i++){
-            auto it = upper_bound(begin(lis) , end(lis) , obs[i]);
-            if (it == end(lis))              
-                ans.push_back(lis.size()+1), lis.push_back(obs[i]);   
-            else { 
-                ans.push_back(it - begin(lis) +1);
-                *it = obs[i];
+
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+            vector<int> ans,lis;
+            int len=0;
+            for (auto i : obstacles){
+                auto it =  upper_bound(begin(lis),end(lis),i);
+                if (it == end(lis)){
+                    lis.push_back(i);
+                    ans.push_back(++len);
+                }
+                else{
+                    ans.push_back(it-begin(lis)+1);
+                    *it = i;
+                }
             }
-        }
-        return ans;
+            return ans;
     }
 };
