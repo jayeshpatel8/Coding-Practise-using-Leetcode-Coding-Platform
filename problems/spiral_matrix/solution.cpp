deb501c90fbed82829d9e03a531d160a51665d42
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-09 10:47:50 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

--- problems/spiral_matrix/solution.cpp
+++ problems/spiral_matrix/solution.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int M = matrix.size(), N = matrix[0].size();
-        int n = (M+1)/2;
         vector<int> ans;
-        int r1=0,r2=M-1,c1=0,c2=N-1;
-        while (r1<=r2 && c1<=c2){
-            for (int j=c1 ; j<=c2; j++)
-                ans.push_back(matrix[r1][j]);
-
-            for (int r=r1+1,c = c2; r<r2 ; r++)
+        int M = matrix.size(), N=  matrix[0].size();
+        int cnt[2]={N,M-1};
+        int dirs[] ={0,1,0,-1,0},s=0,d=0,r=0,c=-1;
+        while(cnt[s] > 0){
+            for (int t=0; t<cnt[s]; t++){                
+                r += dirs[d],c += dirs[d+1];
                 ans.push_back(matrix[r][c]);
-            
-            if (r2>r1)
-                for (int c=c2, r= r2; c>=c1 ; c--)
-                    ans.push_back(matrix[r][c]);            
-            if (c1<c2)
-                for (int c=c1, r= r2-1; r>r1 ; r--)
-                    ans.push_back(matrix[r][c]);         
-            r1++,r2--,c1++,c2--;
+            }
+            cnt[s]--;
+            d= (d+1)%4;
+            s=(s+1)%2;
         }
         return ans;
     }
