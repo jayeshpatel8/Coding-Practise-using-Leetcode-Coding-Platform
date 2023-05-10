1e0025da910af74ccff47ec672051347fb8f0388
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-10 10:34:13 +0000

    Sync LeetCode submission - Spiral Matrix II (cpp)

--- problems/spiral_matrix_ii/solution.cpp
+++ problems/spiral_matrix_ii/solution.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int dirs[] = {0,1,2,3};
-        int cnt= n*n, i=0,j=0 ,c=0 ,r=n,d=n,l=-1,u=0,dir=0;
         vector<vector<int>> ans(n,vector<int>(n));
-        while (cnt-- > 0){
-            switch(dir){
-                    case 0:
-                        ans[i][j++]=++c; 
-                        if (j==r)
-                            dir=1,i++,j=j-1,r--;
-                    break;
-                    case 1:
-                        ans[i++][j]=++c;
-                        if (i==d)
-                            dir=2,i=i-1,j--,d--;
-                    break;
-                    case 2:
-                        ans[i][j--]=++c;
-                        if (j==l)
-                            dir=3,i--,j=j+1,l++;
-                    break;
-                    case 3:
-                        ans[i--][j]=++c;
-                        if (i==u)
-                            dir=0,i+=1,j=i,u++;
-                    break;
+        int dirs[5]={0,1,0,-1,0}, dir=0;
+        int rc[2]={n,n-1},r=0,c=-1,cnt=0,j=1;
+        while (rc[cnt] > 0){
+            for (int i=0; i<rc[cnt]; i++){
+                r += dirs[dir], c  += dirs[dir+1];
+                ans[r][c]= j++;
             }
+            rc[cnt]--;
+            cnt=(cnt+1)%2;
+            dir = (dir+1)%4;
         }
         return ans;
     }
