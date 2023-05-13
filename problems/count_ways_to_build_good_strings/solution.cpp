6fff7228565f7e59730432521fe54cbd0e221a9f
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-13 14:16:23 +0000

    Sync LeetCode submission - Count Ways To Build Good Strings (cpp)

--- problems/count_ways_to_build_good_strings/solution.cpp
+++ problems/count_ways_to_build_good_strings/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high+2);
-        int mod= 1e9 + 7;
+        vector<int> dp(high+2);        
         dp[0]=1;
-        for (int i=1; i<=high; i++){
-            if (i >= zero)
-                dp[i] = (dp[i-zero] + dp[i] )%mod;
-            if (i >= one)
-                dp[i] = (dp[i-one] + dp[i])%mod;
-        }
+        if (zero < one) swap(zero,one);
+        int mod= 1e9 + 7,i=one;
+        for (; i<zero; i++)
+            dp[i] = dp[i-one];
+        for (; i<=high; i++)
+            dp[i] = (dp[i-one] + dp[i-zero])%mod;
+        
         int ans = 0;
         for (int i=low; i<=high; i++){
             ans = (ans + dp[i])%mod;
