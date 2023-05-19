3806362c8279dd0bca08347552f6d62266a9c3af
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-19 09:09:27 +0000

    Sync LeetCode submission - Is Graph Bipartite? (cpp)

--- problems/is_graph_bipartite?/solution.cpp
+++ problems/is_graph_bipartite?/solution.cpp
@@ -1,51 +1,22 @@
 class Solution {
 public:
-    vector<int> uf,size;
+    int vis[101]={};
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> colour(n);
-        for (int i=0; i<n; i++){
-            if (colour[i]==0 && !assign(graph, i, 1, colour))
-                return false;
-        }
-        return true;
-    }
-    bool assign(vector<vector<int>>& graph, int i , int c ,vector<int>& colour) {
-        if (colour[i] != 0 ) return colour[i] == c;
-        colour[i] = c;
-        for (auto j : graph[i]){
-            if (colour[j]==0 && !assign(graph, j , -c,colour))
-                return false;
-            else if (colour[j] != -c)
-                return false;                
-        }
-        return true;
-    }
-    bool isBipartite2(vector<vector<int>>& graph) {
-        int n = graph.size();
-        uf.resize(n+2,-1);
-        size.resize(n+2);
-        for (int i=0; i<n; i++){
-            auto pi = find(i);
-            for (auto j : graph[i]){
-                if (pi == find(j)) return false;
-                join(graph[i][0],j);
-            }
-        }
+        for (int u=0; u<n; u++)
+            if (!vis[u])
+                if (!dfs(graph, u)) return false;
 
         return true;
     }
-    int find(int i){
-        return uf[i]<0?i : (uf[i] = find(uf[i]));
-    }
-    void join(int i, int j){
-        auto pi = find(i), pj = find(j);
-        if (pi != pj){
-            if (size[pi] <= size[pj]){
-                size[pi] += size[pj], uf[pj]=pi;
-            }
-            else
-                size[pj] += size[pi], uf[pi]=pj;
-        }
+    bool dfs(vector<vector<int>>& graph,int u,int cnt=1) {
+        if (vis[u])
+            return (vis[u]&1) == (cnt&1);
+        
+        vis[u]=cnt++;
+        for (auto v : graph[u])
+            if (!dfs(graph,v,cnt)) return false;
+        
+        return true;
     }
 };
