5f496110178bb912de76eb2370598b24c75938a4
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-20 18:15:53 +0000

    Sync LeetCode submission - Evaluate Division (cpp)

--- problems/evaluate_division/solution.cpp
+++ problems/evaluate_division/solution.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    vector<double> calcEquation(vector<vector<string>>& equa, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string,vector<pair<string,double>>> g;
-        int i=0;
-        for (auto &e : equa){
-            g[e[0]].push_back({e[1],values[i]});
-            g[e[1]].push_back({e[0],1.0/values[i++]});
+unordered_map<string,vector<pair<string,double>>> G;
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+        for (auto i=0; i< equations.size(); i++){
+            auto &e = equations[i];
+            G[e[0]].push_back({e[1], values[i]});
+            G[e[1]].push_back({e[0], 1/values[i]});
         }
-        auto bfs = [&](string u, string v){
+        vector<double> ret;
+        for ( auto &q : queries){
             unordered_set<string> vis;
-            queue<pair<string, double>> q;
-            q.push({u,1});
-            vis.insert(u);
-            
-            while (!q.empty()){
-                int sz = q.size();
-                while (sz-- > 0){
-                    auto i = q.front(); q.pop();
-                    auto u1 = i.first;
-                    auto c1 = i.second;
-                    if (u1 == v) return c1;
-                    for (auto &j : g[u1]){
-                        if (vis.count(j.first) == 0){
-                            vis.insert(j.first);
-                            q.push({j.first,c1 * j.second});
-                        }
-                    }
-                }
-            }
-            return -1.0;
-        };
-        vector<double> ans;
-        for (auto &q : queries){
-            if ( g.count(q[0]) == 0 || g.count(q[1]) == 0 )
-                ans.push_back(-1);
-            else{
-                ans.push_back(bfs(q[0],q[1]));
-            }
+            if (G[q[0]].size()==0 ||G[q[1]].size()==0 )
+                ret.push_back(-1.0);
+            else
+                ret.push_back(dfs(q[0],q[1],vis));
         }
-        return ans;
+        return ret;
+    }
+    double dfs(string u , string t, unordered_set<string> vis){
+        if (u==t ) return G[u].size() ? 1.0 : -1.0;
+        auto ret = -1.0;
+        for (auto [v,d] : G[u]){
+            if (vis.count(u+v)) continue;
+            vis.insert(u+v);
+            auto r = dfs(v,t,vis);
+            if (r != -1.0) return d * r;
+        }
+        return ret;
     }
 };
