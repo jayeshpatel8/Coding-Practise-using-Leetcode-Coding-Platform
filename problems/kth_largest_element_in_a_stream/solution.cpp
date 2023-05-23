607e1787a798c430606c685af0a266d059c13c0b
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-23 10:07:21 +0000

    Sync LeetCode submission - Kth Largest Element in a Stream (cpp)

--- problems/kth_largest_element_in_a_stream/solution.cpp
+++ problems/kth_largest_element_in_a_stream/solution.cpp
@@ -1,22 +1,20 @@
 class KthLargest {
 public:
-    priority_queue<int,vector<int>,greater<int>> pq;
-    int K ,sz = 0;
+    multiset<int> mst;
+    int sz=0,K;
     KthLargest(int k, vector<int>& nums) {
-        K= k;
-        for(auto i : nums) 
-            pq.push(i);
-        sz=nums.size();
-        while (sz > k  )
-            pq.pop(),sz--;
+        for (auto i : nums) mst.insert(i),sz++;
+        int n = nums.size()-k;
+        while (n-- > 0 )
+            mst.erase(begin(mst)),sz--;
+        K=k;
     }
     
     int add(int val) {
-        if (sz < K)
-            pq.push(val),sz++;
-        else if (val > pq.top())
-            pq.pop(),pq.push(val);
-        return pq.top();
+        mst.insert(val);
+        if (++sz > K)
+            mst.erase(begin(mst)),--sz;        
+        return *begin(mst);
     }
 };
 
