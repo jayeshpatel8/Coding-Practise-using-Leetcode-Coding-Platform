a647798b8d74dc4fcb991945767a4176870718b6
jayeshpatel8 <jnpatel81@gmail.com>
2023-05-27 14:45:51 +0000

    Sync LeetCode submission - Stone Game III (cpp)

--- problems/stone_game_iii/solution.cpp
+++ problems/stone_game_iii/solution.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& V) {
-        int N = V.size();
+
+    string stoneGameIII(vector<int>& stoneValue, int i=0) {
+        int N = stoneValue.size();
         vector<int> dp(N+1);
-        for (int i = N-1; i>=0; i--){
-            int ans = INT_MIN, sum=0;
-            for (int j=i; j< N && j<i+3 ; j++){
-                sum +=V[j];
-                ans = max(ans, sum - dp[j+1]); 
+        function<int(int)> dfs = [&](int i)->int{
+            if (i >=  N) return 0;
+            if (dp[i]) return dp[i];
+            int ans = INT_MIN, sum = 0;
+            for (int j=i; j< N && j<i+3;j++){
+                sum += stoneValue[j];
+                ans= max(ans, sum - dfs(j+1));
             }
-            dp[i] = ans;
-        }       
-
-        return dp[0] == 0 ? "Tie" : (dp[0] > 0 ? "Alice" : "Bob"); 
-    }    
-    /**
-    int dp[50000];
-    string stoneGameIII(vector<int>& V) {
-        for (int i =0; i<V.size(); i++)dp[i] =10000000;
-        play(V,0);
-
-        return dp[0] == 0 ? "Tie" : (dp[0] > 0 ? "Alice" : "Bob"); 
-    }
-    int play(vector<int>& V, int i){
-        if(i>=V.size()) return 0;
-        if(dp[i] != 10000000) return dp[i];
-        int ans = INT_MIN, sum=0;
-
-        for (int j=i; j<V.size() && j<i+3 ; j++){
-            sum +=V[j];
-            ans = max(ans, sum - play(V,j+1));            
+            return dp[i]=ans;
+        };
+        //int ans = dfs(0) ;
+        //return ans==0? "Tie" : (ans > 0 ? "Alice" : "Bob"); 
+        for ( int i=N-1; i>=0; i--){
+            int sum = stoneValue[i];            
+            dp[i]=sum-dp[i+1];            
+            
+            if (i+2<N){
+                sum +=stoneValue[i+1];
+                dp[i] =max(dp[i], sum - dp[i+2]);
+                dp[i] =max(dp[i], sum +stoneValue[i+2] - dp[i+3]);            
+            }
+            else if (i+1<N){
+                sum +=stoneValue[i+1];
+                dp[i] =max(dp[i], sum - dp[i+2]);
+            }
         }
-        return dp[i] = ans;
+ 
+        return dp[0]==0? "Tie" : (dp[0] > 0 ? "Alice" : "Bob"); 
     }
-    */
 };
