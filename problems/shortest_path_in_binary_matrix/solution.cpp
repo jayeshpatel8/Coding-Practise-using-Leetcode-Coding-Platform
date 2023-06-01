c2ecc77d66f641f3947c01ebd0ee39c01dc14d62
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-01 12:04:16 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (cpp)

--- problems/shortest_path_in_binary_matrix/solution.cpp
+++ problems/shortest_path_in_binary_matrix/solution.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size(),n = grid[0].size();
-        //vector<vector<int>> vis(m,vector<int>(n));
-        if (grid[0][0] || grid[n-1][n-1]) return -1;
-        queue<array<int,2>> q;
-        int row[8]={0,-1,1,0, 1,-1,-1, 1};
-        int col[8]={-1,0,0,1, 1, 1,-1,-1};        
-        q.push({0,0});
-        int len=0;
-        while(!q.empty()){
-            len++;
-            int sz = q.size();
-            while (sz-- > 0){
-                auto i = q.front(); q.pop();
-                int i1=i[0],j1=i[1];
-                
-                if (i1==n-1 && j1==n-1)return len;
-                for (int k=0; k<8; k++){
-                    auto i2 = i1+row[k], j2 = j1+col[k];
-                    if (i2<0 || j2<0 || i2 >= n || j2 >= n || grid[i2][j2] )
-                        continue;
-                    grid[i2][j2]=1;
-                    q.push({i2,j2});
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {        
+        int n = grid.size();
+        if (grid[0][0] || grid[n-1][n-1]) return -1;        
+        vector<int> q{0};
+        grid[0][0]=true;
+        int d=  1;
+        int dirs[] = {-1,0,1,0,-1,-1,1,1,-1};
+        while (!q.empty()){
+            vector<int> t;            
+            for (auto u : q){
+                int i=u/n, j = u%n;
+                if (i==n-1 && j==n-1) return d;
+                for (auto d1 = 0; d1<8; d1++){
+                    int i1=  i +dirs[d1], j1 = j+ dirs[d1+1]; 
+                    if (i1<0 || i1 >=n || j1 < 0 || j1>=n || grid[i1][j1]) continue;
+                    grid[i1][j1]=true;
+                    t.push_back(i1*n +j1);
                 }
-            }            
-        }
-        return -1;        
+            }
+             q.swap(t);++d;
+        }   
+        return -1;
     }
 };
