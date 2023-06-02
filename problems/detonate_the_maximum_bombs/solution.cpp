41e38997e3d070a44668c5d57ef3d9419249569b
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-02 11:03:37 +0000

    Sync LeetCode submission - Detonate the Maximum Bombs (cpp)

--- problems/detonate_the_maximum_bombs/solution.cpp
+++ problems/detonate_the_maximum_bombs/solution.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-   // int vis[101]={};
+
     int maximumDetonation(vector<vector<int>>& bombs) {
-        vector<vector<int>> g(bombs.size());
-        for (int i =0; i<bombs.size(); i++){
-            long x = bombs[i][0], y = bombs[i][1], r = (long)bombs[i][2] *bombs[i][2];
-            for (int  j=0; j<bombs.size(); j++){
+        int ans = 0, N =  bombs.size();
+        vector<vector<int>> G(N);
+        for (int i=0; i<bombs.size(); i++){
+            long long x = bombs[i][0], y = bombs[i][1], r = (long long)bombs[i][2] * bombs[i][2] ;
+            for (int j=0; j<bombs.size(); j++){
                 if (i==j) continue;
-                long x1 = x-bombs[j][0], y1 = y-bombs[j][1];
-                if ((x1)*(x1)  + (y1)*(y1) <= r )
-                    g[i].push_back(j);
-            }
+                long long X =  x-bombs[j][0]  , Y =  y - bombs[j][1];
+                if (X*X+Y*Y <= r)
+                    G[i].push_back(j);
+            }            
         }
-        int ans = 0;
-        for (int i =0; i<bombs.size(); i++){
-            vector<int> vis(bombs.size(),0);
-            ans = max(ans, dfs(g,i, vis));
+        
+        function<int(int, vector<bool> &)> dfs = [&](int i, vector<bool> &vis)->int{         
+            vis[i]=1;
+            int ans = 0;
+            for (auto j : G[i])
+                if (!vis[j])
+                    ans += 1 + dfs(j,vis);
+            return ans;
+        };
+        for (int i=0; i<N; i++){
+            vector<bool> vis(N,0);
+            ans= max(ans,1 + dfs(i,vis));
         }
         return ans;
-    }
-    int dfs(vector<vector<int>>& g,int i, vector<int>& vis){
-        int ans=0;
-        if (!vis[i]){
-            vis[i]=1;
-            for(auto j : g[i])
-                if (!vis[j])
-                    vis[i] += dfs(g,j,vis);
-        }
-        return vis[i];
     }
 };
