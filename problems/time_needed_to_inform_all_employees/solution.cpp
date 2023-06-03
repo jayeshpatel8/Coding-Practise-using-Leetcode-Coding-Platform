1d7f7d8246ba8693f710b833a46bd0b32613b083
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-03 15:33:33 +0000

    Sync LeetCode submission - Time Needed to Inform All Employees (cpp)

--- problems/time_needed_to_inform_all_employees/solution.cpp
+++ problems/time_needed_to_inform_all_employees/solution.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& T) {
-        int h = 0;
-        vector<int> sub[n+1];
-        for ( int i=0; i<n; i++){
-            int m = manager[i];
-            if ( m != -1){
-                sub[m].push_back(i);
-            }
-            else
-                h = i;
-        }
-        
-        queue<array<int,2>> q;
-        q.push({h,T[h]});
-        int ans=0;
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<array<int,2>> q;
+        q.push_back({headID,informTime[headID]});
+        vector<vector<int>> G(n);
+        for (auto i =0; i<n; i++) if (i != headID) G[manager[i]].push_back(i);
+
+        int ans = INT_MIN;
         while (!q.empty()){
-            auto [i,t] = q.front(); q.pop();
-            ans= max(ans,t);
-            for (auto  j : sub[i]){
-                q.push({j,T[j] + t});
+            vector<array<int,2>> t;
+            for (auto & [u,ti] : q){
+                ans= max(ans, ti);
+                for (auto v : G[u])
+                    t.push_back({v,ti+informTime[v]});
             }
+            q.swap(t);
         }
         return ans;
     }
