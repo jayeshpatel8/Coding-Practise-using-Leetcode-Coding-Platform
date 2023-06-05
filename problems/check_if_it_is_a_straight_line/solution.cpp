d41bc8dad5089d3d01e2fecdbde101f740da1f4e
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-05 10:22:38 +0000

    Sync LeetCode submission - Check If It Is a Straight Line (cpp)

--- problems/check_if_it_is_a_straight_line/solution.cpp
+++ problems/check_if_it_is_a_straight_line/solution.cpp
@@ -3,12 +3,11 @@ public:
     bool checkStraightLine(vector<vector<int>>& co) {
      auto X = (co[0][0] - co[1][0]);
      auto Y = (co[0][1] - co[1][1]);
-        auto check = [&]( int c){
-
-            return (X * (co[1][1] - co[c][1])) == (Y *(co[1][0] - co[c][0]));
-        };
+        // slope DeltY / DeltaX
+        // same line => DY1/DX1 == DY2/DX2
+        //           => DY1 * DX2  == DY2 * DX1
         for ( int  i=2;  i<co.size(); i++){
-            if (!check(i)) return false;
+            if (Y * (co[0][0] - co[i][0]) !=  X * (co[0][1] - co[i][1])) return false;
         }
         return true;
     }
