cfef7b2cb2432333327d261086b12eaad5a80c10
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-07 10:28:28 +0000

    Sync LeetCode submission - Minimum Flips to Make a OR b Equal to c (cpp)

--- problems/minimum_flips_to_make_a_or_b_equal_to_c/solution.cpp
+++ problems/minimum_flips_to_make_a_or_b_equal_to_c/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int cnt=0;
-        while (c || b || a){
-            if ((c &1) != ((b | a) &1)){
-                if (c&1) cnt+=1;
-                else
-                    cnt += (a&1) + (b&1);
+        if ((a|b) == c) return 0;
+        int ans = 0;
+        for ( int i=0; i<31; i++){
+            int bit =  1u << i;
+            int abit = (a & bit), bbit= (b & bit), cbit = c &bit;
+            if (  (abit|bbit) == cbit ) continue;
+            if (cbit ) ans++;
+            else{
+                ans += (abit != 0) + (bbit != 0);
             }
-            c>>=1; b>>=1; a>>=1;
         }
-        return cnt;
+        return ans;
     }
 };
