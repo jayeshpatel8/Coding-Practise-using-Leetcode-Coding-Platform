7ba9163fd35493fee788d87013137257cbd61f13
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-13 13:44:29 +0000

    Sync LeetCode submission - Equal Row and Column Pairs (cpp)

--- problems/equal_row_and_column_pairs/solution.cpp
+++ problems/equal_row_and_column_pairs/solution.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int N= grid.size(), ans = 0;
-
+        unordered_map<int,vector<int>> m;
+        for (int c=0; c<N; c++) m[grid[0][c]].push_back(c);
         for (int r=0; r<N; r++){            
-            for (int c=0; c<N; c++){
+            for (auto c : m[grid[r][0]]){
                 bool f=true;
                 for (int i=0; i<N;i++)
                     if (grid[r][i] != grid[i][c]) {
