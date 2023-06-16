3e275767a54400cc5093d52ad2c16030ff5ac2a5
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-16 15:18:22 +0000

    Sync LeetCode submission - Number of Ways to Reorder Array to Get Same BST (cpp)

--- problems/number_of_ways_to_reorder_array_to_get_same_bst/solution.cpp
+++ problems/number_of_ways_to_reorder_array_to_get_same_bst/solution.cpp
@@ -1,39 +1,24 @@
+int mod = 1e9 +7;
 class Solution {
 public:
-    int mod = 1e9 +7;
-    //vector<vector<int>> dp;
-    int dp[1001][1001]={};
+    vector<vector<int>> dp;
     int numOfWays(vector<int>& nums) {
-		int n = nums.size();
-        /*
-		// Pascal triangle        
-        dp.resize(n + 1);
-        for(int i = 0; i < n + 1; ++i){
-            dp[i] = vector<int>(i + 1, 1);
-            cout<< dp[i][0] << " ";
-            for(int j = 1; j < i; ++j){
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % mod;
-                cout<< dp[i][j] << " ";
-            }
-            cout<< endl;
-        } */       
-        return dfs(nums)-1;
+        int n  = nums.size();
+        dp.resize(n+1,vector<int>(n+1));
+        return dfs(nums) - 1;
     }
-    int dfs(vector<int> nums){
-        if (nums.size() <=1) return 1;
-        vector<int> l,r;
-        int  N = nums.size();
-        for ( int i=1; i<N; i++){
+    int dfs(vector<int> &nums){
+        int n = nums.size();
+        
+        if (n<=1) return 1;
+        vector<int> r,l;
+        for (int i=1; i<n; i++)
             if (nums[i] < nums[0]) l.push_back(nums[i]);
             else r.push_back(nums[i]);
-        }
-        // chose r = l.size or r.size from N-1 position
-        //return ((dp[N-1][r.size()] * (long)dfs(l))%mod * dfs(r))%mod;
-        return ((comb(N-1,r.size()) * (long)dfs(l))%mod * dfs(r))%mod;
+        return (((comb(n-1,r.size()) * (long)dfs(l))%mod)*dfs(r))%mod;
     }
-    int comb(int n, int m) {
-        return n == 0 || m == 0 || n==m ? 1 :
-            dp[n][m] ? dp[n][m] : 
-                dp[n][m] = (comb(n - 1, m) + comb(n-1, m - 1)) % 1000000007;
+    int comb(int m, int n){
+        if (n==0 || m==0 || n==m ) return dp[m][n] = 1;
+        return (dp[m][n] ? dp[m][n] : dp[m][n]=(comb(m-1,n)+comb(m-1,n-1))%mod); 
     }
 };
