55127f08e4e22aff671ee12d2b1bf301239bea1b
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-22 10:55:52 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Transaction Fee (cpp)

--- problems/best_time_to_buy_and_sell_stock_with_transaction_fee/solution.cpp
+++ problems/best_time_to_buy_and_sell_stock_with_transaction_fee/solution.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
-
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size(), buy[n], sell[n];
-        buy[0] = -prices[0], sell[0]=0;
-        for (int i=1; i<prices.size(); i++  ) 
-        {
-            buy[i] = max(buy[i-1], sell[i-1]-prices[i]);
-            sell[i] = max(sell[i-1], buy[i-1]+prices[i]-fee);
+        int ans = 0;
+        
+        int prevBuy = -prices[0], prevSell=0;
+        for (int i=1; i<prices.size(); i++){
+            int curBuy =max(-prices[i] + prevSell , prevBuy);
+            int curSell =max( prices[i] - fee + prevBuy , prevSell);
+            prevBuy= curBuy;
+            prevSell=curSell;
         }
-        return sell[n-1];
+        return prevSell;
     }
+    int maxProfit3(vector<int>& prices, int fee) {
+        int ans = 0;
+        vector<vector<int>> dp(prices.size(),vector<int>(2));
+        dp[0][0] = -prices[0];
+        for (int i=1; i<prices.size(); i++){
+            dp[i][0] =max(-prices[i] + dp[i-1][1] , dp[i-1][0]);
+            dp[i][1] =max( prices[i] - fee + dp[i-1][0] , dp[i-1][1]);
+        }
+        return dp[prices.size()-1][1];
+    }
+    int maxProfit2(vector<int>& prices, int fee) {
+        int ans = 0;
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+        function<int(int , bool)> dfs= [&](int i, bool buy){
+            if (i >=prices.size()) return 0;
+            if (dp[i][buy] != -1) return dp[i][buy];
+            
+            if (!buy)
+                return dp[i][buy] =max((-prices[i] + dfs(i+1,!buy)) , dfs(i+1,buy));
+            else 
+                return dp[i][buy] =max(( prices[i] - fee + dfs(i+1,!buy)) , dfs(i+1,buy));
 
+
+        };
+        return dfs(0,0);
+    }
 };
