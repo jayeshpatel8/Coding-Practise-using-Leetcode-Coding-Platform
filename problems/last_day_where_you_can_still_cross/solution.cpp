26ab371fbc86af90c0763202b41e1ec8e1493a78
jayeshpatel8 <jnpatel81@gmail.com>
2023-06-30 13:15:15 +0000

    Sync LeetCode submission - Last Day Where You Can Still Cross (cpp)

--- problems/last_day_where_you_can_still_cross/solution.cpp
+++ problems/last_day_where_you_can_still_cross/solution.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int uf[20001];
-    int find(int n){
-        if (uf[n] < 0 ) return n;
-        return uf[n] = find(uf[n]);
-    }
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        int dir[5] =  {0,1,0,-1,0};
-        for ( int i=0; i<20001; i++) uf[i] = INT_MIN;
-        //memset(uf,-1,sizeof(uf));
-        for (int d = cells.size()-1; d>=0; d--){
+        int n =  row * col, top= n, bottom=n+1;
+        vector<int> uf(n+2,-1), sz(n+2,1);
+
+        function<int(int)> find = [&](int i){
+            return uf[i] < 0 ? i : uf[i] = find(uf[i]);
+        };
+        int dirs[] ={-1,0,1,0,-1};
+        for (int d = n-1; d>=0; d--){
             int r = cells[d][0]-1, c = cells[d][1]-1;
-            int cur = r*col+c;            
-            uf[r*col+c] = -1;
-            for ( int i=0; i<4; i++){
-                int r1 = r + dir[i] , c1  = c + dir[i+1];
-                if ( r1 >=0 && r1<row && c1 >=0 && c1 <col && uf[r1*col + c1] >= -1){
-                    int n1 = find(r1*col+c1);
-                    if(n1 != cur){
-                        uf[max(cur,n1)] = min(cur,n1);
-                        cur = min(cur,n1);
-                    }
+            uf[r*col+c] = r==0? top : (r==row-1?bottom : -2);
+             
+            for (int i=0; i<4; i++){
+                int r1 = r + dirs[i], c1 = c + dirs[i+1];
+                if (r1<0 || c1 < 0 || r1 >= row || c1 >= col || uf[r1*col+c1]==-1) continue;
+                int p2 = find(r1*col+c1),p1 = find(r*col+c);;
+                if (p1!=p2){
+                    if (sz[p1] > sz[p2]) swap(p1,p2);
+                    uf[p2]=p1;
+                    sz[p1]+=sz[p2];
                 }
             }
-            
-            if (cur < col)
-                for(int j = 0; j<col; j++){
-                    if (find((row-1) * col +j) < col){
-                        return d;
-                    }
-                }
+            if (find(top) == find(bottom)) return d;
         }
+
         return -1;
     }
 };
