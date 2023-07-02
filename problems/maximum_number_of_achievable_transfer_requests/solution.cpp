b38d63ede700dcc2aa6e10d9831a811a3879960b
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-02 12:11:24 +0000

    Sync LeetCode submission - Maximum Number of Achievable Transfer Requests (cpp)

--- problems/maximum_number_of_achievable_transfer_requests/solution.cpp
+++ problems/maximum_number_of_achievable_transfer_requests/solution.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int degree[21]={}, N, ans = 0, n1 ;
-    int maximumRequests(int n, vector<vector<int>>& requests) {
-        N = requests.size();
-        n1 = n;
-        return dfs(requests);
-    }
-    int dfs(vector<vector<int>>& req, int i=0){
-        if(i>=N){
-            return (count(begin(degree), begin(degree) + n1, 0) == n1) ? 0 : INT_MIN;
-        }
-        int ans =  dfs(req,i+1);
-        degree[req[i][0]]++;
-        degree[req[i][1]]--;
-        ans = max(ans, 1 + dfs(req,i+1));
-        degree[req[i][1]]++;
-        degree[req[i][0]]--;
+int deg[21]={};
+    int maximumRequests(int n, vector<vector<int>>& req,int  i= 0) {
+        if (i >= req.size()){
+            return count(begin(deg),begin(deg)+n,0) ==n ? 0 : INT_MIN;
+        }    
+        int ans = maximumRequests(n,req,i+1);
+        int u = req[i][0] ,v = req[i][1];
+        deg[u]--;
+        deg[v]++;
+        ans= max(ans, 1 + maximumRequests(n,req,i+1));
+        deg[v]--;
+        deg[u]++;
         return ans;
     }
 };
