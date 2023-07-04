e0af6e4f371e58e96538cef54468cb5ff20ff35b
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-04 09:35:13 +0000

    Sync LeetCode submission - Single Number II (cpp)

--- problems/single_number_ii/solution.cpp
+++ problems/single_number_ii/solution.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones=0, twos=0;
-        for (auto i : nums){
-            ones = (ones ^ i) & ~twos;
-            twos = (twos ^ i) & ~ones;
+        int f[32]={};
+        for (auto  i : nums){
+            for (int j=0; j<32; j++)
+                f[j] += (i & (1<<j))!=0;
+        }       
+        int ans = 0; 
+        for (int i=0; i<32; i++){
+             if (f[i] %3 != 0 )
+                ans |= 1<<i;            
         }
-        return ones;
+        return ans;
     }
 };
