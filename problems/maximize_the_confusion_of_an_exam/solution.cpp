bdeb823c3b0ae0599478e8ba704af41d4d0a03db
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-07 09:33:29 +0000

    Sync LeetCode submission - Maximize the Confusion of an Exam (cpp)

--- problems/maximize_the_confusion_of_an_exam/solution.cpp
+++ problems/maximize_the_confusion_of_an_exam/solution.cpp
@@ -1,50 +1,17 @@
 class Solution {
 public:
-    int N;
-    unordered_map<string,int> dp;
-    int maxConsecutiveAnswers(string a, int k) {
-        N = a.size();
-        int cntT =0, cntF=0, ans= 0;
-        for ( int i=0 , j = 0; i<N; i++){
-            if (a[i] == 'T') cntT++;
-            else cntF++;
-            if (cntT>k && cntF>k){
-                ans =max (ans, cntT+cntF-1);
-                while(cntT>k && cntF>k){
-                    if (a[j++] == 'T') cntT--;
-                    else cntF--;                    
-                }
-            }
-            else  continue;            
+    int maxConsecutiveAnswers(string akey, int k) {
+        int ans = 0,f[2]={};
+        
+        for (int i=0,j=0; i<akey.size(); i++){
+            akey[i] = akey[i] =='T' ? 0 : 1;
+            f[akey[i]]++;
+            
+            while (min(f[0],f[1]) > k)
+                f[akey[j++]]--;
+            
+            ans=  max(ans, i-j+1);
         }
-        return ans =max (ans, (cntT+cntF));
-        //if(k==N) return N;
-        //return dfs(a, 0,k);
-    }
-    int dfs(string & a, int i, int k){
-        if ( k<1 || i>=N ) {
-            return cnt(a);
-        }
-        string key = to_string(k) + a;
-        if (dp.count(key)) return dp[key];
-        int ans = dfs(a, i+1, k);       
-        int t = a[i];
-         a[i] =  (t == 'F') ? 'T' : 'F';
-        ans =max(ans, dfs(a, i+1, k-1));
-        a[i] = t;
-        return dp[key] = ans;
-    }
-    int cnt(string & a){
-        int p = a[0], c = 1  ,ans = 1;
-        for ( int i=1; i<N; i++){
-            if (p == a[i]){
-                c++;
-            }
-            else{
-                ans= max(ans, c);
-                p = a[i]; c= 1;
-            }
-        }
-        return max(ans, c);
+        return ans;
     }
 };
