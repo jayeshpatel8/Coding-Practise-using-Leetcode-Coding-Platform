6767f73edfadebd4b6e1990fc9107e331da745c9
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-15 13:34:56 +0000

    Sync LeetCode submission - Maximum Number of Events That Can Be Attended II (cpp)

--- problems/maximum_number_of_events_that_can_be_attended_ii/solution.cpp
+++ problems/maximum_number_of_events_that_can_be_attended_ii/solution.cpp
@@ -1,39 +1,19 @@
-
 class Solution {
 public:
-    int n, K;
-    static bool compare(const vector<int> & a, const vector<int> & b){
-        return ( a[1] < b[1]);
+int  N ;
+    int maxValue(vector<vector<int>>& events, int k) {
+        N = events.size();
+        sort(events.begin(), events.end());
+        
+        vector<vector<int>>dp(N+1,vector<int>(k+1,-1));
+        return dfs(events,0,k,dp);
     }
-   
-    vector<vector<int>> dp;
-    int maxValue(vector<vector<int>>& e, int k) {
-        sort(e.begin(), e.end(), compare);
-        
-         n= e.size(); K=k;
-        dp = vector<vector<int>>(n+2, vector<int>(k + 1));
-        
-        for (int i=0; i<n; i++){
-            auto idx = lower_bound(rbegin(e) -(n- i) , rend(e),e[i],
-                   [](const vector<int>& vt , const vector<int>& v){return vt[1]>= v[0];}) - rbegin(e);
-
-    
-            for (int K=1; K<=k; K++){
-                dp[i+1][K] = max(e[i][2] +dp[n-idx][K-1], dp[i][K]);
-                            
-            }
-        }
-
-        return dp[n][k];
-    }
-    int maxV(vector<vector<int>>& e, int k, int i){
-        if (i>=n || k==0) return 0;
-        if (dp[i][k] != -1) 
-        return dp[i][k];
-        
-        
-        auto idx = upper_bound(begin(e) + i, end(e),e[i][1],
-                   [](int t , const vector<int>& v){return v[0]>t;}) - begin(e);               
-        return *std::max_element(dp[n].begin(), dp[n].end());
+    int dfs(vector<vector<int>>&e , int i, int k,vector<vector<int>>&dp){
+        if (i>=N || k==0) return 0;
+        if (dp[i][k] != -1) return dp[i][k];
+        int j =i+1;
+        for (; j<N; j++)
+            if (e[j][0] > e[i][1]) break;
+        return dp[i][k] = max(dfs(e,i+1,k,dp), e[i][2] + dfs(e,j,k-1,dp));
     }
 };
