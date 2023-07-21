1be32245e9922a66147504b3f9906ee88a94ce38
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-21 11:54:59 +0000

    Sync LeetCode submission - Number of Longest Increasing Subsequence (cpp)

--- problems/number_of_longest_increasing_subsequence/solution.cpp
+++ problems/number_of_longest_increasing_subsequence/solution.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int dp[n], cnt[n];
-        if (n==1) return 1;
-        dp[n-1]=1;cnt[n-1]=1;
-        int mlen=0;
-        for (int i=n-2;i>=0; i--){
-            dp[i]=1;cnt[i]=1;
-            for (int j=i+1; j<n; j++){
-                if (nums[i] < nums[j] ){
-                    if (dp[i] < dp[j]+1){
-                        dp[i] = dp[j]+1;
-                        cnt[i] = cnt[j];
-                    }
-                    else if (dp[i] == dp[j]+1){
-                        cnt[i] +=cnt[j];
-                    }
-                    
+        vector<int> dp(nums.size(),1) ,cnt(nums.size(),1);
+        for (int i = 0; i <nums.size(); i++){
+            
+            for (int j=i-1; j>=0; j--){
+                if (nums[j] >= nums[i]) continue;
+                if (dp[j] +1 > dp[i]){
+                    dp[i]=dp[j] +1;
+                    cnt[i]=cnt[j];
                 }
+                else if(dp[j] +1 == dp[i])
+                    cnt[i]+=cnt[j];
             }
-            mlen = max(mlen,dp[i]);
         }
-        int ans=0;
-        for (int i=0; i<n; i++) if (dp[i]==mlen) ans += cnt[i];
+        int longest = *max_element(begin(dp),end(dp)), ans = 0;
+        for (auto i=0; i<nums.size(); i++){
+            if (dp[i] == longest) ans += cnt[i];
+        }
         return ans;
     }
 };
-
-/*
- if ()
-*/
