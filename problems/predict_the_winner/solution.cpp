eb6338279b6d24ca1115da6345616dfc7b7d2a17
jayeshpatel8 <jnpatel81@gmail.com>
2023-07-28 10:28:51 +0000

    Sync LeetCode submission - Predict the Winner (cpp)

--- problems/predict_the_winner/solution.cpp
+++ problems/predict_the_winner/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+int dp[21][21], N ;
     bool PredictTheWinner(vector<int>& nums) {
-        int  N = nums.size();
-        if(N <= 1) return true;
-        int dp[N][N];
-        for ( int i=0; i<N; i++) dp[i][i] = nums[i];
+        memset(dp,-1,sizeof(dp));
+        N = nums.size();
+        if (N<=2) return true;
+        return dfs(nums,0,N-1)>=0;
+    }
+    int dfs(vector<int> &nums, int i,int j){
+        if (i>j)return 0;
+        if (dp[i][j] != -1) return dp[i][j];
         
-        for(int l=1; l<N; l++){
-            for (int i=0; i<N-l;i++){
-                int j=i+l;
-                dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
-            }
-        }
-        return  dp[0][N-1] >= 0 ;
+        int ans= max(nums[i] - dfs(nums,i+1,j),
+            nums[j] - dfs(nums,i,j-1));
+        
+        return dp[i][j] = ans;
     }
 };
