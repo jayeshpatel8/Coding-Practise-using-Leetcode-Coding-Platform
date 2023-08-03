8b1261bb29603e82101d5aeb7fed51d34cabebf2
jayeshpatel8 <jnpatel81@gmail.com>
2023-08-03 09:42:52 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- problems/letter_combinations_of_a_phone_number/solution.cpp
+++ problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
+    unordered_map<char,string> m;
+    vector<string> ans;
+    string c;
     vector<string> letterCombinations(string digits) {
-           vector<string> ch={"","","abc", "def", "ghi", 
-                                  "jkl", "mno","pqrs",
-                                  "tuv","wxyz"};
-        vector<string> ans;
-        
-        for (auto d : digits){
-            vector<string> t;
+        if (digits.empty()) return {};
+        m['2']="abc";
+        m['3']="def";
+        m['4']="ghi";
+        m['5']="jkl";
+        m['6']="mno";
+        m['7']="pqrs";
+        m['8']="tuv";
+        m['9']="wxyz";
+        // BFS        
+        for (auto &s : digits){
             if (ans.empty()){
-                   for (auto &s : ch[d-'0']){
-                        t.push_back(string(1,s));
-                    }
+                for (auto c : m[s])
+                    ans.push_back(string(1,c));
             }
             else{
-                for (auto &a : ans){
-                    for (auto &s : ch[d-'0']){
-                        t.push_back(a+s);
+                vector <string> t;
+                for (auto &s1 : ans){
+                    for (auto c : m[s]){
+                        s1.push_back(c);
+                        t.push_back(s1);
+                        s1.pop_back();
                     }
                 }
+                ans.swap(t);
             }
-            ans.swap(t);
         }
+        //dfs(digits,0);
         return ans;
+    }
+    void dfs(string & s, int i){
+        if (i== s.size()){
+            ans.push_back(c);
+            return;
+        }
+        for (auto ch : m[s[i]]){
+            c.push_back(ch);
+            dfs(s,i+1);
+            c.pop_back();
+        }
     }
 };
