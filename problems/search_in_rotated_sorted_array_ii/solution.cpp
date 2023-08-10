038d029c5fef520dca985b88ccf3fdee4d425280
jayeshpatel8 <jnpatel81@gmail.com>
2023-08-10 08:27:19 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-
-         return bs(nums,target,0,nums.size()-1);
-    }
-    bool bs(vector<int>& nums, int target, int l, int r){
-        if (l>=r) return nums[l] == target;
-        
-        int m = (l + r) / 2;
-        if (nums[m]==target) return true;
-        else if (nums[l] == nums[r]){
-            if (nums[l] == target) return true;
-            else
-                return bs(nums,target,l+1,r-1);
-        }
-        
-        else if (nums[m]  <= nums[r]){
-            if (target > nums[m] && target <= nums[r]){
-                return bs(nums,target,m+1,r);
+        int l =  0, r=  nums.size()-1;
+        while (l<=r){
+            int m= (l+r)/2;
+            if (nums[m]==target) return true;
+            if (nums[l]==nums[r]) r--;
+            else if (nums[l]<= nums[m] && nums[m]<nums[r]){
+                 if (target < nums[l] || target > nums[r] )                 
+                 return false;
+                if (target < nums[m] ) r=m-1;
+                else l=m+1;
             }
-            else
-                return bs(nums,target,l,m-1);
-        }
-        else{
-            if (target >= nums[l] && target < nums[m]){
-                return bs(nums,target,l,m-1);
+            else if (nums[l]<= nums[m] && nums[m]>nums[r]){
+                if (target >= nums[l] && target < nums[m])
+                    r=m-1;
+                else if (target > nums[m] || target <= nums[r])
+                    l=m+1;
+                else return false;
             }
-            else
-                return bs(nums,target,m+1,r);
+            else //if (nums[m] < nums[r] && nums[l] >= nums[m])
+            {
+                if (target > nums[m] && target <= nums[r])
+                    l=m+1;
+                else if (target >= nums[l] || target < nums[m])
+                    r=m-1;
+                else return false;
+            }
         }
         return false;
     }
