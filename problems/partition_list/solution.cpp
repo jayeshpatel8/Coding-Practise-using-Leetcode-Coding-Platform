f64bd5929082bc410b0adde6415fe675baee723d
jayeshpatel8 <jnpatel81@gmail.com>
2023-08-15 09:49:44 +0000

    Sync LeetCode submission - Partition List (cpp)

--- problems/partition_list/solution.cpp
+++ problems/partition_list/solution.cpp
@@ -11,22 +11,19 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode s,l,*s1,*l1;
-        s1=&s,l1=&l;
-        s.next=NULL; l.next==NULL;
-        while (head){
-            if (head->val < x){
-                s1->next = head;
-                s1=s1->next;                
-            }
-            else{
-                l1->next=head;
-                l1=l1->next;
-            }
-            head=head->next;
-        }
-        s1->next = l.next;
-        l1->next=NULL;
-        return s.next;
+       ListNode s,b,* small , * big; 
+       small = &s, big= &b;
+
+       while (head ){
+            if (head->val <  x) 
+               small->next=head,small=small->next;
+            else
+                big->next=head,big=big->next;
+           
+           head=head->next;
+       }
+       small->next=b.next;
+       big->next=nullptr;
+       return s.next;
     }
 };
