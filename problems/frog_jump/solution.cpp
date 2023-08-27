2d4fb5a11eb85be272fc7c3b5b7dfadc305d5f24
jayeshpatel8 <jnpatel81@gmail.com>
2023-08-27 10:13:20 +0000

    Sync LeetCode submission - Frog Jump (cpp)

--- problems/frog_jump/solution.cpp
+++ problems/frog_jump/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    unordered_map<long,bool> dp;
-    unordered_map<int,int> map;
+int dp[2001][2001];
+unordered_map<int,int> idx;
     bool canCross(vector<int>& stones) {
-        for (int i=0; i<stones.size(); i++) map[stones[i]] = i;
-        return dfs(stones,0,0);
+        if (stones[1] != 1) return false;
+        for (int i=0; i<stones.size(); i++) idx[stones[i]]=i;
+        memset(dp,-1, sizeof(dp));
+      
+        return dfs(stones, 1, 0);
     }
-    bool dfs(vector<int>& stones, int i, int k){
-        if (i >= stones.size()-1) return true;
-    
-        long idx = k<<20 | i ;
-        auto it = dp.find(idx);
-        if (it != end(dp)  ) return it->second;
-        
-        for (int s=max(stones[i]+1,stones[i]+k-1)  ;  s<= stones[i]+k+1; s++){
-            auto it = map.find(s);
-            if (it == end(map)) continue;
-            if (dfs(stones, it->second, s-stones[i]))
-                return dp[idx] = true;            
+    bool dfs(vector<int> & stones, int i, int prev){        
+        if (i>=stones.size()-1) return 1;
+        if (dp[i][prev] != -1) return dp[i][prev];
+        int ans= 0;
+        for (int j=0,next=stones[i] + stones[i]-stones[prev]-1; j<3; j++,next++){
+            if (next > stones[i] && idx.count(next) && dfs(stones,idx[next],i)){
+                ans=1;break;
+            }
         }
-        return dp[idx]=false;
+        return dp[i][prev]=ans;
     }
 };
