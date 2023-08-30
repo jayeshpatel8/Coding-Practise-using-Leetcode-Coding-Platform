1b5cc895b915811a490942186b5eada8861690e6
jayeshpatel8 <jnpatel81@gmail.com>
2023-08-30 22:51:15 +0000

    Sync LeetCode submission - Minimum Replacements to Sort the Array (cpp)

--- problems/minimum_replacements_to_sort_the_array/solution.cpp
+++ problems/minimum_replacements_to_sort_the_array/solution.cpp
@@ -1,20 +1,3 @@
-class Solution {
-public:
-    long long minimumReplacement(vector<int>& nums) {
-        long long  ans = 0;
-        int N = nums.size();
-
-        for (int i=N-2; i>=0; i--){
-            if (nums[i] <= nums[i+1]) continue;        
-                auto cur  = nums[i];
-                int next = nums[i+1];
-                 long parts = cur / next  + (cur % next  != 0);
-                ans += parts - 1;
-                if (cur % next == 0)
-                    nums[i] = next;
-                else
-                    nums[i] = cur  / parts;  // select the smallest of all sub part of cur
-        }
-        return ans;
-    }
-};
+class Solution { public: long long minimumReplacement(vector<int>& nums) { long long ans = 0; int N = nums.size(); for (int i=N-2; i>=0; i--){ if (nums[i] <= nums[i+1]) continue; auto cur = nums[i]; int next = nums[i+1]; long parts = cur / next + (cur % next != 0); ans += parts - 1; if (cur % next == 0) nums[i] = next; else nums[i] = cur / parts; 
+// select the smallest of all sub part of cur
+ } return ans; } };
