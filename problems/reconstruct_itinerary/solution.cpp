43b4ec2573c59e3c080c054d2ea76321d10c4b4f
jayeshpatel8 <jnpatel81@gmail.com>
2023-09-14 14:53:39 +0000

    Sync LeetCode submission - Reconstruct Itinerary (cpp)

--- problems/reconstruct_itinerary/solution.cpp
+++ problems/reconstruct_itinerary/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<string> ans;
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         unordered_map<string, multiset<string>> m;
-        for (auto &s : tickets)            
-                m[s[0]].insert(s[1]);
-        
-         vector<string> trace(1, "JFK");
-        while (!trace.empty()){
-            string u = trace.back();
+        for (auto & t : tickets){
+            m[t[0]].insert(t[1]);
+        }
+        vector<string> stack,ans;
+        stack.push_back("JFK");
+        while (!stack.empty()){
+            auto u = stack.back();
             if (m[u].empty()){
+                stack.pop_back();
                 ans.push_back(u);
-                trace.pop_back();
             }
             else{
-                auto it = m[u].begin();
-                trace.push_back(*it);
+                auto it = begin(m[u]);
+                stack.push_back(*it);
                 m[u].erase(it);
             }
         }
         reverse(begin(ans),end(ans));
         return ans;
     }
-
 };
