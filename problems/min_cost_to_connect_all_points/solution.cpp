79a61b4f487cdea24594d7370837d49748d72d61
jayeshpatel8 <jnpatel81@gmail.com>
2023-09-15 14:59:29 +0000

    Sync LeetCode submission - Min Cost to Connect All Points (cpp)

--- problems/min_cost_to_connect_all_points/solution.cpp
+++ problems/min_cost_to_connect_all_points/solution.cpp
@@ -28,43 +28,5 @@ public:
         }
         return cost;
     }
-    vector<int> uf;
-    vector<int> size;
-    int minCostConnectPoints2(vector<vector<int>>& points) {
-        int N = points.size();
-        uf.resize(N,-1);
-        size.resize(N);
-        vector<array<int,3>> e;
-        for (int i=0; i<N; i++){
-            auto x1 = points[i][0], y1 = points[i][1];
-            for (int j=i+1; j<N; j++){                
-                auto x2 = points[j][0], y2 = points[j][1];
-                auto d = abs(x1-x2) + abs(y1-y2);
-                e.push_back({d,i,j});
-            }
-        }
-        
-        sort(begin(e),end(e));
-        int ans = 0;
-        for (auto &i : e){
-            if (find(i[1]) == find(i[2])) continue;
-            ans +=i[0];
-            join(i[1],i[2]);
-        }
-        return ans;
-    }
-    int find(int i){
-        return uf[i]==-1 ? i : (uf[i] = find(uf[i]));
-    }
-    void join(int i, int j){
-        auto pi = find(i), pj = find(j);
-        if (pi != pj){
-            if (size[pi] < size[pj]){
-                uf[pj]= pi;
-                size[pi] += size[pj];
-            }
-            else
-                uf[pi]= pj, size[pj] += size[pi];
-        }
-    }
+
 };
