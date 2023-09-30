ffcb32e035f903104810900519bce7e9f2a8e7f7
jayeshpatel8 <jnpatel81@gmail.com>
2023-09-30 22:36:56 +0000

    Sync LeetCode submission - 132 Pattern (cpp)

--- problems/132_pattern/solution.cpp
+++ problems/132_pattern/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        
-        stack<int> st;
-        int s3=INT_MIN;
-        for (int i=nums.size()-1; i>=0; i--){
-            int n = nums[i];
-
-            if ( n < s3) return true;
-            while (!st.empty() && n > st.top() ) s3 = st.top(), st.pop();
-            st.push(n);
+        vector<int> st;
+        int j = INT_MIN ;
+        for (int i=nums.size()-1,sz=0; i>=0; i--){
+            auto n = nums[i];
+            if (j > n) return true;
+            while (sz && n > st.back()) j=st.back(), st.pop_back(),sz--;
+            st.push_back(n);
+            sz++;
         }
         return false;
     }
