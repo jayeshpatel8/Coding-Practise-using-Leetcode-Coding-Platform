81a1b3288edfd91d688856fd5937e49c861049f0
jayeshpatel8 <jnpatel81@gmail.com>
2023-10-07 22:09:17 +0000

    Sync LeetCode submission - Build Array Where You Can Find The Maximum Exactly K Comparisons (cpp)

--- problems/build_array_where_you_can_find_the_maximum_exactly_k_comparisons/solution.cpp
+++ problems/build_array_where_you_can_find_the_maximum_exactly_k_comparisons/solution.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
-    //       N,M,K
-    int dp[52][102][52] ={}, mod=1e9 + 7;
-    
-    int numOfArrays(int N, int M, int K) {
-        
-        for (int i=1; i<=M; i++) dp[1][i][1]=1; //basecase
-        
-        for (int i=2; i<=N; i++){
-            for (int maxv=1; maxv<=M; maxv++){
-                for(int k=1; k<=K; k++){
-                    for (int m1=1; m1<=M; m1++){
-                        if(m1 <= maxv)
-                            dp[i][maxv][k] = (dp[i][maxv][k] + dp[i-1][maxv][k]) % mod;
-                        else
-                            dp[i][maxv][k] = (dp[i][maxv][k] + dp[i-1][m1][k-1]) % mod; 
-                    }
-                }
-            }
-        }
-        int ans = 0;
-        for (int m=1; m<=M; m++)  ans = (ans + dp[N][m][K]) % mod; 
-            
-        return ans;
-    }    
-    /*
-    int dp[52][52][102], mod=1e9 + 7;
+int dp[52][52][102], mod=1e9 + 7;
     int numOfArrays(int n, int m, int k) {
         
         memset(dp,-1,sizeof(dp));
@@ -44,5 +19,5 @@ public:
             ans = (ans + dfs(n-1,m,k-1,i)) % mod;
         
         return  dp[n][k][maxv+1] = ans;
-    }*/
+    }
 };
