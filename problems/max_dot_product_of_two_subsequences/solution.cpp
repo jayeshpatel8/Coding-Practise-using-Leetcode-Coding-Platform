612ee7cd38eeb0dffb0c4280b056445628dd7cf4
jayeshpatel8 <jnpatel81@gmail.com>
2023-10-08 17:25:52 +0000

    Sync LeetCode submission - Max Dot Product of Two Subsequences (cpp)

--- problems/max_dot_product_of_two_subsequences/solution.cpp
+++ problems/max_dot_product_of_two_subsequences/solution.cpp
@@ -4,19 +4,15 @@ public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
           N = nums1.size(), M = nums2.size();
         //vector<vector<int>> dp(N+1,vector<int>(M+1, INT_MIN));
-       int dp[N+1];
-        dp[N-1]=nums1[N-1]*nums2[M-1];
-        for(int i=N-2;i>=0;i--)  dp[i]=max(nums1[i]*nums2[M-1],dp[i+1]);
-        for (int j=M-2; j>=0; j--){
-            int prev = dp[N-1];;
-            dp[N-1]= max ({prev,nums1[N-1]*nums2[j]});
-            for (int i=N-2; i>=0; i--){
-                int temp = dp[i];
-                
-                dp[i] = max({dp[i+1],dp[i],nums1[i]*nums2[j]+ max (0,prev)});
-                prev=temp;
+       int dp[N+1][M+1];
+        
+        for (int i=N; i>=0; i--){
+            for (int j=M; j>=0; j--){
+                if (i==N||j ==M) dp[i][j]=INT_MIN;
+                else
+                    dp[i][j] = max({dp[i+1][j],dp[i][j+1],nums1[i]*nums2[j]+ max (0,dp[i+1][j+1])});     
             }
         }
-        return dp[0];
+        return dp[0][0];
     }
 };
