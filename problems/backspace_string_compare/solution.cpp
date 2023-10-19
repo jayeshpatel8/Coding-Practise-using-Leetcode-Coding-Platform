c61791b90a00638801ff60b2d347c5812e0f10a2
jayeshpatel8 <jnpatel81@gmail.com>
2023-10-19 21:30:46 +0000

    Sync LeetCode submission - Backspace String Compare (cpp)

--- problems/backspace_string_compare/solution.cpp
+++ problems/backspace_string_compare/solution.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string s1,t1;
-        for (auto c : s){
-            if (c =='#'){
-                if (!s1.empty())
-                    s1.pop_back();
+        string s1,s2;
+        for (auto c : s) 
+            if (c == '#' ){
+                if ( !s1.empty())s1.pop_back();
             }
-            else
-                s1.push_back(c);
-        }
-        for (auto c : t){
-            if (c =='#'){
-                if (!t1.empty())
-                    t1.pop_back();
+            else s1+=c;
+        for (auto c : t) 
+            if (c == '#'){
+                if( !s2.empty())s2.pop_back();
             }
-            else
-                t1.push_back(c);
-        }
-        return s1==t1;
+            else s2+=c;
+        return s1==s2;
     }
 };
