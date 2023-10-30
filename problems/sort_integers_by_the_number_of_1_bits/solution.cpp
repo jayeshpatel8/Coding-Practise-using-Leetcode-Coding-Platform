005d327892ef05c33614480cca073733016761c7
jayeshpatel8 <jnpatel81@gmail.com>
2023-10-30 20:57:22 +0000

    Sync LeetCode submission - Sort Integers by The Number of 1 Bits (cpp)

--- problems/sort_integers_by_the_number_of_1_bits/solution.cpp
+++ problems/sort_integers_by_the_number_of_1_bits/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        auto check = [](int a, int b){
-                int ba = __builtin_popcount(a) , bb = __builtin_popcount(b);
-                return ba==bb ? a<b : ba<bb;
-            };
-        sort(begin(arr),end(arr),[&](auto a, auto b){return (a==0||b==0 )? a<b : (check(a,b) );});
+        sort(begin(arr), end(arr), [&](int a, int b){
+            int ca = __builtin_popcount(a) , cb = __builtin_popcount(b); 
+            return ca == cb ? a < b : ca < cb;
+        });
         return arr;
     }
 };
