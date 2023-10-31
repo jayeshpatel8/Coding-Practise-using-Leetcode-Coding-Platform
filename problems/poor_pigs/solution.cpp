fcf7a9df7b1cdf82b1ffff9d5f0b1efb7216791f
jayeshpatel8 <jnpatel81@gmail.com>
2023-10-31 00:20:10 +0000

    Sync LeetCode submission - Poor Pigs (cpp)

--- problems/poor_pigs/solution.cpp
+++ problems/poor_pigs/solution.cpp
@@ -3,6 +3,6 @@ public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         //  (minutesToTest / minutesToDie) ^ pigs >= buckets
         // pigs >= log (buckets) / log((minutesToTest / minutesToDie))
-        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+        return ceil(log2(buckets) / log2(minutesToTest / minutesToDie + 1));
     }
 };
