3815cbf4e8e9bec9417a01ae581df42635989540
jayeshpatel8 <jnpatel81@gmail.com>
2023-11-09 22:54:29 +0000

    Sync LeetCode submission - Count Number of Homogenous Substrings (cpp)

--- problems/count_number_of_homogenous_substrings/solution.cpp
+++ problems/count_number_of_homogenous_substrings/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int countHomogenous(string s) {
-        
-        int N = s.size();
+                int N = s.size();
         
         //"abbcccaa"
         int prev=0,cnt=0,i=0;
-        for (; i< N; i++){
-
-            if (s[prev]!=s[i]){
+        for (; i< N; ){
+            while(i<N && s[prev] == s[i]) i++;
+            if (prev < i-1){
                 uint64_t n=i-prev;
                 uint64_t c = (n * (n+1)/2 );
-                cnt =  (cnt + c) % 1000000007;
-                prev=i;                
+                cnt =  (cnt + c) % 1000000007;                                
             }
+            else cnt++;
+            prev=i;
         }
-        unsigned long n=i-prev;
-
-        cnt =  (cnt + (n * (n+1)/2)) % 1000000007;
         return cnt;
     }
-
 };
