864f8e2fe3c343e053e5ff702bd29b1aeb726971
jayeshpatel8 <jnpatel81@gmail.com>
2023-11-30 22:38:42 +0000

    Sync LeetCode submission Runtime - 3 ms (36.41%), Memory - 6.3 MB (100.00%)

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        // overservation  n = 2^k  then ans = 2 ^(k+1) - 1
+        if(n==0) return  0;
+        if(n==1) return 1;
+        if (n & n-1 == 0) return (n<<1)-1;
+        
+        int j = 1;
+        while(j<=n) j<<=1;
+        
+        // For 1010 => f(1000) - f(0010)
+        return j-1 - minimumOneBitOperations(n - (j>>1));
+    }
+};
