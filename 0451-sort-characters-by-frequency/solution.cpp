7c6dad8bc6ade2a964f3212181001a41127fe9fc
jayeshpatel8 <jnpatel81@gmail.com>
2024-02-07 22:01:37 +0000

    Sync LeetCode submission Runtime - 11 ms (55.28%), Memory - 10 MB (89.77%)

---
+++ 0451-sort-characters-by-frequency/solution.cpp
@@ -0,0 +1,11 @@
+/*?///lass Solution {
+public:
+    string frequencySort(string s) {
+        
+    }
+};*/
+
+
+
+
+class Solution { public: string frequencySort(string s) { vector<int> f(256), idx(256); iota(begin(idx),end(idx),0); for (auto c : s) f[c]++; sort(begin(idx),end(idx),[&](auto a, auto b){return f[a] >f[b];}); string ans; for (auto i : idx){ if (f[i]) ans += string(f[i],i); else break; } return ans; } };
