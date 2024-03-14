f04f35110b393ca3607560956c1280f89676a9a5
jayeshpatel8 <jnpatel81@gmail.com>
2024-03-14 23:01:14 +0000

    Sync LeetCode submission Runtime - 23 ms (76.35%), Memory - 33.2 MB (59.93%)

---
+++ 0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return maxsum(nums,goal)   - maxsum(nums,goal-1);
+    }
+    int maxsum(vector<int> nums, int goal){
+        int sum = 0, cnt = 0;
+        for (int i=0,j=0; i<nums.size(); i++){
+            sum += nums[i];
+            while (j<=i && sum > goal) {
+                sum-=nums[j++];
+            }
+            cnt += i-j+1;
+        }
+        return cnt;
+    }
+};
