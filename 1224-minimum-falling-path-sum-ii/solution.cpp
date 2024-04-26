66579df57646939fe27831a40fe2b0d3234e5af6
jayeshpatel8 <jnpatel81@gmail.com>
2024-04-26 21:56:54 +0000

    Sync LeetCode submission Runtime - 26 ms (80.19%), Memory - 17 MB (74.09%)

---
+++ 1224-minimum-falling-path-sum-ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        int N= arr.size();
+        
+        for (int i=N-2; i>=0; i--){
+            int m[N+1];m[0]=20000;
+            for (int j=0; j<N; j++)   m[j+1] = min(m[j],arr[i+1][j]);
+            m[N]=20000;
+            for (int j=N-1; j>=0; j--){
+                arr[i][j] +=min(m[j],m[j+1]);
+                m[j] =min(m[j+1] ,arr[i+1][j]);
+            }
+        }
+        
+        return *min_element(begin(arr[0]),end(arr[0]));
+    }
+};
