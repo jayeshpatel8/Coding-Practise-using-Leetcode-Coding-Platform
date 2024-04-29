d671105e500e0a1a3b4dbde0d9937df344f20954
jayeshpatel8 <jnpatel81@gmail.com>
2024-04-29 21:26:26 +0000

    Sync LeetCode submission Runtime - 120 ms (18.16%), Memory - 91.2 MB (83.92%)

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int _xor  = 0,ans = 0;
-        for (auto i : nums) _xor ^= i;
-        for (int i =0 , j = _xor  ^ k; i<32; i++){
-            ans += (j & (1<<i))!=0;
-        }
-        return ans;
+        
+     
+     for (auto  i : nums)    k ^= i;
+     int ans = 0;
+     while(k) k &=k-1, ans++;
+     return  ans;
+
     }
 };
