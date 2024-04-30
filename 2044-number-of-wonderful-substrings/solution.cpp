0070a22d006ae11ce0dd4740456b46aeeebf2ff3
jayeshpatel8 <jnpatel81@gmail.com>
2024-04-30 22:36:30 +0000

    Sync LeetCode submission Runtime - 44 ms (84.16%), Memory - 16 MB (82.35%)

---
+++ 2044-number-of-wonderful-substrings/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        long long ans = 0;
+        vector<int> cnt(1024);
+        cnt[0]=1;
+        int mask=0;
+        
+        for (auto c : word){
+            mask ^= 1 << (c-'a');
+            ans += cnt[mask];
+            for (int m=1; m<1024; m<<=1){
+                ans += cnt[mask ^ m];
+            }
+            cnt[mask]++;
+        }
+        return ans;
+    }
+};
