fcc0133411d39d4780c3fa1757d5b3b14f7034bc
jayeshpatel8 <jnpatel81@gmail.com>
2024-05-03 22:34:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.4 MB (69.58%)

---
+++ 0165-compare-version-numbers/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        
+        for (int i=0, j=0, N1 = v1.size(), N2 = v2.size(); i<N1 || j<N2;){
+            long long a=0,b=0;
+            while (i<N1 && v1[i] == '0'){
+                i++; continue;
+            }
+            while (i<N1 && v1[i] != '.')
+                a = a*10 + v1[i++]-'0';
+            
+            
+            while (j<N2 && v2[j] == '0'){
+                j++; continue;
+            }
+            while (j<N2 && v2[j] != '.')
+                b = b*10 + v2[j++]-'0';
+            
+                        
+            if (a != b){
+                return a < b? -1 : 1;
+            }
+            j++;i++;
+        }
+        return 0;
+    }
+};
