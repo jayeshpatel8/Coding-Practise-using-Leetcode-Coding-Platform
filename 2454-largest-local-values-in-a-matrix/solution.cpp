dc177f0bf08c79dc7227ad63a85861eabcd580db
jayeshpatel8 <jnpatel81@gmail.com>
2024-05-12 22:31:55 +0000

    Sync LeetCode submission Runtime - 12 ms (33.67%), Memory - 13.5 MB (41.65%)

---
+++ 2454-largest-local-values-in-a-matrix/solution.cpp
@@ -0,0 +1,15 @@
+ class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int N = grid.size();
+        vector<vector<int>> ans(N-2, vector<int>(N-2));
+        for (int i=0; i<N-2; i++){
+            for (int j=0; j<N-2; j++){
+                for (int r=i; r<i+3; r++)
+                    for (int c=j; c<j+3; c++)
+                        ans[i][j] = max(ans[i][j], grid[r][c]);
+            }
+        }
+        return ans;
+    }
+};
