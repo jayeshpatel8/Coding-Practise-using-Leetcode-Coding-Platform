5a88e6841622ccbbcb8c49c7e385ef7192a9f082
jayeshpatel8 <jnpatel81@gmail.com>
2024-08-21 23:08:05 +0000

    Sync LeetCode submission Runtime - 9 ms (85.36%), Memory - 8.9 MB (75.33%)

---
+++ 0664-strange-printer/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[101][101]={};
+    int strangePrinter(string s1) {
+        string s(1,s1[0]);
+        for ( int i=1;i<s1.size(); i++){
+            if (s1[i]==s1[i-1])continue;
+            s.push_back(s1[i]);
+        }
+        return dfs(s,0,s.size()-1);
+    }
+    int dfs(string & s, int start, int end){
+        if(start >= end) return start == end;
+        if(dp[start][end]) return dp[start][end];
+        int len=end-start,ans = dfs(s,start+1,end)+1;
+        
+        for ( int k=start+1; k<=end; k++){
+            if (s[start]==s[k])            
+                ans = min (ans, dfs(s,start,k-1)+dfs(s,k+1,end));            
+        }
+        return dp[start][end] = ans;
+    }
+}; 
