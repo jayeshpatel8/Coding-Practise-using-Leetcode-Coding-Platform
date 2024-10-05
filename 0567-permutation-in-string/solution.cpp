47874a3cf414df45057e8d55e9a250d7354540f9
jayeshpatel8 <jnpatel81@gmail.com>
2024-10-05 11:13:03 +0000

    Sync LeetCode submission Runtime - 4 ms (63.59%), Memory - 8.9 MB (48.42%)

---
+++ 0567-permutation-in-string/solution.cpp
@@ -0,0 +1,4 @@
+
+      
+class Solution { public: bool checkInclusion(string s1, string s2) { vector<int> freq(26,0); for (auto c : s1) freq[c-'a']++; for (int i = 0,N=s1.size()-1,cnt=N+1; i<s2.size(); i++){ if (freq[s2[i]-'a']-->0) cnt--; if (i >= N){ if (cnt==0) return true; if (++freq[s2[i-N]-'a'] > 0) cnt++; } } return false; } };
+
