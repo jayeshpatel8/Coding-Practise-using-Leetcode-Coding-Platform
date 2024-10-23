99b0d48019d105d54a532eed021e244588038934
jayeshpatel8 <jnpatel81@gmail.com>
2024-10-23 22:21:12 +0000

    Sync LeetCode submission Runtime - 4 ms (96.87%), Memory - 313 MB (94.72%)

---
+++ 2677-cousins-in-binary-tree-ii/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int sum[100000];
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        dfs(root,0);
+        dfs2(root,0);
+        root->val=0;
+        return root;
+    }
+    void dfs(TreeNode * r, int height){
+        if (!r) return;
+        sum[height] += r->val;
+        dfs(r->left,height+1);
+        dfs(r->right,height+1);
+    }
+    int dfs2(TreeNode *r, int h){
+        if (!r) return 0;
+        int v = dfs2(r->left,h+1) + dfs2(r->right, h+1);
+        if (r->left){
+            r->left->val =  sum[h+1]-v;
+        }
+        if (r->right){
+            r->right->val =  sum[h+1]-v;
+        }
+        return r->val;
+    }
+};
