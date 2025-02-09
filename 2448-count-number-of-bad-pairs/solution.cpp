dc5717dd76dfa46a5eb06990e24b101a110228a1
jayeshpatel8 <jnpatel81@gmail.com>
2025-02-09 23:53:00 +0000

    Sync LeetCode submission Runtime - 57 ms (92.23%), Memory - 92.7 MB (6.77%)

---
+++ 2448-count-number-of-bad-pairs/solution.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        unordered_map<int,long long> m;
+        long long N = nums.size();
+        long long ans= (N)*(N-1)/2;
+        for (int i =0; i<N; i++){
+            auto j = nums[i]-i;
+            auto it = m.find(j);
+            if (it == m.end())m[j]++;
+            else
+                ans -= it->second, it->second++;
+           
+        }
+        return ans;
+    }
+};
