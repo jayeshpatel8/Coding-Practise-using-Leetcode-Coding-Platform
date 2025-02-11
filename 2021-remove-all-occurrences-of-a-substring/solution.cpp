faae1f003b6f00d4eb9eff87caa143758c85a9bd
jayeshpatel8 <jnpatel81@gmail.com>
2025-02-11 22:40:00 +0000

    Sync LeetCode submission Runtime - 3 ms (44.69%), Memory - 13.9 MB (11.20%)

---
+++ 2021-remove-all-occurrences-of-a-substring/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        auto i = s.find(part);
+        if (i == string::npos) return s;
+        if (i==0)
+            return  removeOccurrences(s.substr(part.size()),part);
+        return removeOccurrences(s.substr(0,i) + s.substr(i+part.size()),part);
+    }
+};
