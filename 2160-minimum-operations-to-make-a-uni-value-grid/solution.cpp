79c2f7e1eb49fc233cca387f6c9fb00d03340475
jayeshpatel8 <jnpatel81@gmail.com>
2025-03-26 09:14:02 +0000

    Sync LeetCode submission Runtime - 75 ms (5.30%), Memory - 92.2 MB (13.18%)

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/solution.cpp
@@ -0,0 +1,1 @@
+class Solution { public: int minOperations(vector<vector<int>>& grid, int x) { int M = grid.size(), N = grid[0].size(); vector<int> A(M*N); int k=0; for (auto i : grid){ for (auto j : i) { A[k++]=j; } } sort(begin(A),end(A)); int m = A[(A.size()-1)/2],ans=0; for (auto i : A){ if (m != i){ int diff = abs(i-m); if (diff%x) return -1; ans +=diff/x; } } return ans; } };
