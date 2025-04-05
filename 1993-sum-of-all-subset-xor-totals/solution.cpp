18faeba55357ebb1b517a0141eb4dd90416cf339
jayeshpatel8 <jnpatel81@gmail.com>
2025-04-05 21:13:13 +0000

    Sync LeetCode submission Runtime - 3 ms (31.93%), Memory - 9.2 MB (34.74%)

--- 1993-sum-of-all-subset-xor-totals/solution.cpp
+++ 1993-sum-of-all-subset-xor-totals/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        int N =  nums.size();
-        int ans = 0;
-        for  (int i=1; i < (1<<N); i++){
-            int _xor= 0;
+        int N= nums.size(), ans = 0;
+        for (int i=1; i<(1<<N); i++){
+            int x=0;
             for (int j=0; j<N; j++){
-                if ( i & (1<<j))
-                    _xor ^= nums[j];
+                if ( i & (1 << j))
+                    x ^= nums[j];
             }
-            ans += _xor;
+            ans += x;
         }
         return ans;
     }
