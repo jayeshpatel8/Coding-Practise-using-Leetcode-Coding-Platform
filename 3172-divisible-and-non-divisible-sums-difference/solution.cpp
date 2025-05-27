0bbd725e0107f9cc1f9e7cd8cf384f9e7d5c3ac8
jayeshpatel8 <jnpatel81@gmail.com>
2025-05-27 21:09:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (94.71%)

---
+++ 3172-divisible-and-non-divisible-sums-difference/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int a=0,b=0;
+        for (auto i=1; i<=n; i++)
+            if ( i % m) a+=i;
+            else b+=i;
+        return a-b;
+    }
+};
