650ad8e6688dee2e0713869702e33defc0cd8f1d
jayeshpatel8 <jnpatel81@gmail.com>
2025-06-02 22:23:21 +0000

    Sync LeetCode submission Runtime - 2 ms (31.14%), Memory - 23 MB (33.80%)

---
+++ 0135-candy/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int ans = 0;
+        vector<int> can(ratings.size() , 1);
+        for (int i=1; i<ratings.size(); i++){
+            if (ratings[i-1]< ratings[i]){
+                can[i] = can[i-1] + 1;
+            }
+        }
+        for (int i=ratings.size()-2; i>=0; i--){
+            if (ratings[i+1] < ratings[i]){
+                can[i] =max(can[i+1]+1,can[i]);
+            }
+            ans +=can[i];
+        }
+        return ans + can[ratings.size()-1];
+    }
+};
+
