ec38e2c643c77a90d4f51a75fa7d2d8e03caf6de
jayeshpatel8 <jnpatel81@gmail.com>
2025-06-08 20:19:34 +0000

    Sync LeetCode submission Runtime - 3 ms (38.29%), Memory - 15 MB (25.20%)

--- 0386-lexicographical-numbers/solution.cpp
+++ 0386-lexicographical-numbers/solution.cpp
@@ -1,36 +1,22 @@
-
-        
-
 class Solution {
-    public:
-        vector<int> lexicalOrder(int n) {
-                vector<int> lexicographicalNumbers;
-                        // Start generating numbers from 1 to 9
-                                for (int start = 1; start <= 9; ++start) {
-                                            generateLexicalNumbers(start, n, lexicographicalNumbers);
-                                                    }
-                                                            return lexicographicalNumbers;
-                                                                }
+public:
+        vector<int> ans;
 
-                                                                private:
-                                                                    void generateLexicalNumbers(int currentNumber, int limit,
-                                                                                                    vector<int>& result) {
-                                                                                                            // If the current number exceeds the limit, stop recursion
-                                                                                                                    if (currentNumber > limit) return;
+    vector<int> lexicalOrder(int n) {
 
-                                                                                                                            // Add the current number to the result
-                                                                                                                                    result.push_back(currentNumber);
+        for (int i=1; i<10; i++){            
+                dfs(i,n);
+        }
+        return ans;
+    }
+    void dfs(int i, int n){
+        if (i> n)return;
+        ans.push_back(i);
+        for (int next =0; next<10; next++){
 
-                                                                                                                                            // Try to append digits from 0 to 9 to the current number
-                                                                                                                                                    for (int nextDigit = 0; nextDigit <= 9; ++nextDigit) {
-                                                                                                                                                                int nextNumber = currentNumber * 10 + nextDigit;
-                                                                                                                                                                            // If the next number is within the limit, continue recursion
-                                                                                                                                                                                        if (nextNumber <= limit) {
-                                                                                                                                                                                                        generateLexicalNumbers(nextNumber, limit, result);
-                                                                                                                                                                                                                    } else {
-                                                                                                                                                                                                                                    break;  // No need to continue if nextNumber exceeds limit
-                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                            };
-
+            int nn = i * 10 + next;
+            if (nn <= n) dfs(nn,n);
+            else break;
+        }
+    }
+};
