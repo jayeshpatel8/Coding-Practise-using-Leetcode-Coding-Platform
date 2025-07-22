d566a8862e0f8851d7467b815e661c2c482a3437
jayeshpatel8 <jnpatel81@gmail.com>
2025-07-22 21:45:56 +0000

    Sync LeetCode submission Runtime - 3 ms (98.30%), Memory - 93.7 MB (95.05%)

---
+++ 1813-maximum-erasure-value/solution.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int ans = 0;
+        int f[10001]={};
+        for (int l=0,r=0,sum=0; r<nums.size(); r++){
+            sum +=nums[r];
+            while (f[nums[r]])
+                sum -=nums[l], --f[nums[l++]];
+            f[nums[r]]=1;
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
