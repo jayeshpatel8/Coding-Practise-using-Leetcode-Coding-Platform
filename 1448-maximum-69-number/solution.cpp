d6fbad382c6330d64e778fc4b4e2c5daf49b537d
jayeshpatel8 <jnpatel81@gmail.com>
2025-08-16 23:10:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (40.07%)

---
+++ 1448-maximum-69-number/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        auto s = to_string(num);
+        for (auto &i : s) 
+            if (i == '6')   {
+                i = '9'; break;
+            }
+        return stoi(s);
+    }
+};
