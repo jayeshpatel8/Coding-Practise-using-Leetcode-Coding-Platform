862bb0acd20065f24fcf44ba754bdf84bf41d65a
jayeshpatel8 <jnpatel81@gmail.com>
2025-08-21 22:05:19 +0000

    Sync LeetCode submission Runtime - 12 ms (49.24%), Memory - 19.2 MB (73.44%)

---
+++ 1628-count-submatrices-with-all-ones/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size() , ans = 0;
+
+        
+        for ( int i=0; i<m; i++){
+            vector<int> row(n,1);
+            for ( int i1=i; i1<m; i1++){
+                for ( int j=0, len=0; j<n; j++){
+                    row[j] &= mat[i1][j];
+                    ans += row[j] ? ++len : len=0;
+                }                    
+            }
+        }
+
+        return ans;
+    }
+};
