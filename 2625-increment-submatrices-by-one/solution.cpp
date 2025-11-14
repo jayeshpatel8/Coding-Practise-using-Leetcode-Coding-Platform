689fa024ab5b23008df8e7dfe859c3b3ab7dd643
jayeshpatel8 <jnpatel81@gmail.com>
2025-11-14 21:48:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 86.4 MB (73.42%)

---
+++ 2625-increment-submatrices-by-one/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> ans(n,vector<int>(n));
+        for (auto &q : queries){
+             ans[q[0]][q[1]]++;
+            if ((q[2]+1 < n) && (q[3]+1 < n)){
+                ans[q[2]+1][q[3]+1]++;
+            }
+            if ((q[3]+1 < n)){
+                ans[q[0]][q[3]+1]--;
+            }
+            if ((q[2]+1 < n)){
+                ans[q[2]+1][q[1]]--;
+            }
+        }
+        for (int i=0; i<n; i++){
+            for (int j=1; j<n; j++){                
+                ans[i][j] += ans[i][j-1];
+            }
+        }
+        for (int i=0; i<n; i++){
+            for (int j=1; j<n; j++){                
+                ans[j][i] += ans[j-1][i];
+            }
+        }
+        return ans;
+    }
+};
