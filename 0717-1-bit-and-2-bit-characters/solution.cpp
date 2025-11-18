2192ba1968718192dfdcbb1314badf6da9a71e32
jayeshpatel8 <jnpatel81@gmail.com>
2025-11-18 22:34:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (43.20%)

---
+++ 0717-1-bit-and-2-bit-characters/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int i =0;
+        while ( i <bits.size()-1){
+            if (bits[i]) i+=2;
+            else
+                i+=1;
+        }
+        return ((i == bits.size()-1) && bits[i]==0);
+    }
+};
