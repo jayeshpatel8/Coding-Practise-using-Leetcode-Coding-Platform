d4d06e6cbf322e9685eb3e78eec7a5e7eb569612
jayeshpatel8 <jnpatel81@gmail.com>
2025-12-16 19:18:04 +0000

    Sync LeetCode submission Runtime - 98 ms (74.03%), Memory - 95.3 MB (55.75%)

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/solution.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/solution.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        vector<int> f(100001);
-        long long ans =0, sum = 0 ;
-        int dup = 0;
-        for (int i=0,j=-1; i<nums.size(); i++){
-            sum += nums[i];
-            if (++f[nums[i]] == 2) dup++;
-            
-            if (i-j>=k){
-                if (dup==0) ans = max(ans,sum);
-                sum -= nums[++j];
-                if (--f[nums[j]] == 1) dup--;
+        using ll = long long;
+        unordered_set<int> st;
+        ll ans = 0, sum=0;
+        for (int i=0,j=0; i<nums.size(); i++){
+            auto n = nums[i];
+            if (st.count(n)){
+                while ( j<i && (nums[j] != n)){
+                    st.erase(nums[j]);                
+                    sum -= nums[j++];
+                }
+                sum -= nums[j++];
             }
+            sum += n;
+            st.insert(n);
 
+            if (i-j + 1== k ){
+                ans=max(ans,sum);
+            }
+            else if (i - j >= k){
+                st.erase(nums[j]);                
+                sum-=nums[j++];                
+                ans=max(ans,sum);
+            }
         }
         return ans;
     }
